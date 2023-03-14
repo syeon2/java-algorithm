77f031f14f3cc7bd0304a47d61fde4d24091d3ee
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-03-15 07:15:33 +0900

    [Silver II] Title: 이동하기, Time: 552 ms, Memory: 80268 KB -BaekjoonHub

--- 백준/Silver/11048. 이동하기/이동하기.java
+++ 백준/Silver/11048. 이동하기/이동하기.java
@@ -14,8 +14,8 @@ public class Main {
 		int M = Integer.parseInt(st.nextToken());
 		int N = Integer.parseInt(st.nextToken());
 
-		int[][] list = new int[M][N];
-		int[][] dp = new int[M][N];
+		list = new int[M][N];
+		dp = new int[M][N];
 
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine(), " ");
@@ -27,21 +27,41 @@ public class Main {
 		}
 
 		// bottom-up
-		dp[0][0] = list[0][0];
-		for (int i = 0; i < M; i++) {
+		// dp[0][0] = list[0][0];
+		// for (int i = 0; i < M; i++) {
+		//
+		// 	for (int k = 0; k < N; k++) {
+		// 		if (i == 0 && k == 0) continue;
+		// 		else if (i == 0) dp[i][k] = dp[i][k - 1] + list[i][k];
+		// 		else if (k == 0) dp[i][k] = dp[i - 1][k] + list[i][k];
+		// 		else {
+		// 			dp[i][k] = Math.max(dp[i - 1][k], Math.max(dp[i][k - 1], dp[i - 1][k - 1])) + list[i][k];
+		// 		}
+		// 	}
+		// }
 
-			for (int k = 0; k < N; k++) {
-				if (i == 0 && k == 0) continue;
-				else if (i == 0) dp[i][k] = dp[i][k - 1] + list[i][k];
-				else if (k == 0) dp[i][k] = dp[i - 1][k] + list[i][k];
-				else {
-					dp[i][k] = Math.max(dp[i - 1][k], Math.max(dp[i][k - 1], dp[i - 1][k - 1])) + list[i][k];
-				}
+		// top-down
+		int answer = topDown(M - 1, N - 1);
+
+
+		// bw.write(String.valueOf(dp[M - 1][N - 1]));
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
+
+	public static int topDown(int M, int N) {
+		if (N == 0 && M == 0) return dp[M][N] = list[M][N];
+
+		if (dp[M][N] == -1) {
+			if (M == 0) dp[M][N] = topDown(M, N - 1) + list[M][N];
+			else if (N == 0) dp[M][N] = topDown(M - 1, N) + list[M][N];
+			else {
+				dp[M][N] = Math.max(topDown(M - 1, N - 1),
+					Math.max(topDown(M - 1, N), topDown(M, N - 1))) + list[M][N];
 			}
 		}
 
-		bw.write(String.valueOf(dp[M - 1][N - 1]));
-		bw.flush();
-		bw.close();
+		return dp[M][N];
 	}
 }
