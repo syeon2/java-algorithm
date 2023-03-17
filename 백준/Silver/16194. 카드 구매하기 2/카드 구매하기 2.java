3e969f4b2afe8f173d9429a9c3188e1152eac0bf
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-03-18 06:39:50 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 176 ms, Memory: 15320 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] list;
+	static int[] dp;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+		list = new int[N + 1];
+		dp = new int[N + 1];
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 1; i <= N; i++) {
+			list[i] = Integer.parseInt(st.nextToken());
+		}
+
+		// bottom-up
+		// for (int i = 1; i <= N; i++) {
+		// 	dp[i] = list[i];
+		//
+		// 	for (int k = 1; k <= i / 2; k++) {
+		// 		dp[i] = Math.min(dp[i], dp[k] + dp[i - k]);
+		// 	}
+		// }
+
+		int answer = topDown(N);
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
+
+	public static int topDown(int N) {
+		if (N == 1) return dp[N] = list[N];
+
+		if (dp[N] == 0) {
+			dp[N] = list[N];
+
+			for (int i = 1; i <= N / 2; i++) {
+				dp[N] = Math.min(dp[N], topDown(i) + topDown(N - i));
+			}
+		}
+
+		return dp[N];
+	}
+}
