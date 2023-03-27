37e84042a810196b580f3e0045f30dc94f8024b8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-03-28 07:58:01 +0900

    [Gold IV] Title: 타일 채우기, Time: 120 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Gold/2133. 타일 채우기/타일 채우기.java
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.java
@@ -12,18 +12,36 @@ public class Main {
 		dp = new long[N + 1];
 
 		// bottom-up
-		dp[0] = 1;
-		if (N > 1) dp[2] = 3;
-		for (int i = 4; i <= N; i += 2) {
-			dp[i] = dp[i - 2] * 3;
+		// dp[0] = 1;
+		// if (N > 1) dp[2] = 3;
+		// for (int i = 4; i <= N; i += 2) {
+		// 	dp[i] = dp[i - 2] * 3;
+		//
+		// 	for (int k = 4; k <= i; k += 2) {
+		// 		dp[i] += dp[i - k] * 2;
+		// 	}
+		// }
 
-			for (int k = 4; k <= i; k += 2) {
-				dp[i] += dp[i - k] * 2;
-			}
-		}
+		topDown(N);
 
 		bw.write(String.valueOf(dp[N]));
 		bw.flush();
 		bw.close();
+	}
+
+	public static long topDown(int i) {
+		if (i % 2 != 0) return 0;
+		if (i == 0) return dp[i] = 1;
+		else if (i == 2) return dp[i] = 3;
+
+		if (dp[i] == 0) {
+			dp[i] = topDown(i - 2) * 3;
+
+			for (int k = 4; k <= i; k += 2) {
+				dp[i] += topDown(i - k) * 2;
+			}
+		}
+
+		return dp[i];
 	}
 }
