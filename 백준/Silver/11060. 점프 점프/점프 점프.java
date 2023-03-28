8022117a63a3a0aca62a5c9b35c4ae902860bbcc
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-03-29 06:43:41 +0900

    [Silver II] Title: 점프 점프, Time: 144 ms, Memory: 14956 KB -BaekjoonHub

--- 백준/Silver/11060. 점프 점프/점프 점프.java
+++ 백준/Silver/11060. 점프 점프/점프 점프.java
@@ -1,35 +1,56 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+
+	static int[] list;
+	static Integer[] dp;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		int[] list = new int[N];
-		int[] dp = new int[N];
+		list = new int[N + 1];
+		dp = new Integer[N + 1];
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			list[i] = Integer.parseInt(st.nextToken());
-			dp[i] = 1001;
+		for (int n = 1; n <= N; n++) {
+			list[n] = Integer.parseInt(st.nextToken());
+			dp[n] = Integer.MAX_VALUE;
 		}
 
 		// bottom-up
-		dp[0] = 0;
-		for (int i = 0; i < N; i++) {
+		// dp[1] = 0;
+		// for (int i = 1; i <= N; i++) {
+		// 	for (int j = 1; j < i; j++) {
+		// 		if (list[j] + j >= i && dp[j] != Integer.MAX_VALUE) dp[i] = Math.min(dp[i], dp[j] + 1);
+		// 	}
+		// }
 
-			for (int k = 0; k < i; k++) {
-				if (k + list[k] >= i) dp[i] = Math.min(dp[i], dp[k] + 1);
+		for (int i = 1; i <= N; i++) {
+			topDown(i);
+		}
+
+		int answer = -1;
+		if (dp[N] != Integer.MAX_VALUE) answer = dp[N];
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
+
+	public static int topDown(int n) {
+		if (n == 1) return dp[n] = 0;
+
+		if (dp[n] == Integer.MAX_VALUE) {
+			for (int i = 1; i < n; i++) {
+				if (list[i] + i >= n && dp[i] != Integer.MAX_VALUE) {
+					dp[n] = Math.min(dp[n], topDown(i) + 1);
+				}
 			}
 		}
 
-		if (dp[N - 1] == 1001) bw.write(String.valueOf(-1));
-		else bw.write(String.valueOf(dp[N - 1]));
-
-		bw.flush();
-		bw.close();
+		return dp[n];
 	}
 }
