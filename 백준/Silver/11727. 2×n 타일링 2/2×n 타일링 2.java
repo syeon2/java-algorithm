f51734be169e1f22edab272177084154123737d9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-04-11 07:22:56 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 124 ms, Memory: 14340 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,39 +1,43 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main {
 
 	static int[] dp;
 	static int MOD = 10_007;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 		dp = new int[N + 1];
 
 		// bottom-up
-		for (int i = 1; i <= N; i++) {
-			if (i == 1) dp[i] = 1;
-			else if (i == 2) dp[i] = 3;
-			else dp[i] = (dp[i - 1] + (dp[i - 2] * 2)) % MOD;
-		}
+		// for (int i = 1; i <= N; i++) {
+		// 	if (i == 1) dp[i] = 1;
+		// 	else if (i == 2) dp[i] = 3;
+		// 	else dp[i] = (dp[i - 1] + (2 * dp[i - 2])) % MOD;
+		// }
 
-		// int answer = topDown(N);
+		int answer = topDown(N);
 
-		bw.write(String.valueOf(dp[N]));
+		bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
-	}
+    }
 
-	public static int topDown(int N) {
-		if (N == 1) return dp[N] = 1;
-		else if (N == 2) return dp[N] = 3;
+	public static int topDown(int n) {
+		if (n == 1) return dp[n] = 1;
+		else if (n == 2) return dp[n] = 3;
 
-		if (dp[N] == 0) {
-			dp[N] = (topDown(N - 1) + (topDown(N - 2) * 2)) % MOD;
+		if (dp[n] == 0) {
+			dp[n] = (topDown(n - 1) + (2 * topDown(n - 2))) % MOD;
 		}
 
-		return dp[N];
+		return dp[n];
 	}
 }
