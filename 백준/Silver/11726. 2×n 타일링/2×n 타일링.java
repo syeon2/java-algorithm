290fc06fd387e391f210a17243b4b491741f35b1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-04-11 07:11:31 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,38 +1,42 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main {
 
 	static int[] dp;
 	static int MOD = 10_007;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 		dp = new int[N + 1];
 
 		// bottom-up
 		for (int i = 1; i <= N; i++) {
-			if (i == 1) dp[1] = 1;
-			else if (i == 2) dp[2] = 2;
+			if (i == 1) dp[i] = 1;
+			else if (i == 2) dp[i] = 2;
 			else dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
 		}
 
-		// int answer = topDown(N);
+		int answer = topDown(N);
 
-		bw.write(String.valueOf(dp[N]));
+		bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
-	}
+    }
 
-	public static int topDown(int N) {
-		if (N < 3) return dp[N] = N;
+	public static int topDown(int n) {
+		if (n < 3) return dp[n] = n;
 
-		if (dp[N] == 0) {
-			dp[N] = (topDown(N - 1) + topDown(N - 2)) % MOD;
+		if (dp[n] == 0) {
+			dp[n] = (topDown(n - 1) + topDown(n - 2)) % MOD;
 		}
 
-		return dp[N];
+		return dp[n];
 	}
 }
