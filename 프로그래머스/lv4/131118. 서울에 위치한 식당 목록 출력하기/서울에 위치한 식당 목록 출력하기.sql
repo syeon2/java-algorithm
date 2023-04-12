b1a224366e1a2bf2651acd0799c0fc4035bb8d25
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-04-12 16:57:53 +0900

    [level 4] Title: 서울에 위치한 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
+++ 프로그래머스/lv4/131118. 서울에 위치한 식당 목록 출력하기/서울에 위치한 식당 목록 출력하기.sql
@@ -1,5 +1,5 @@
-select a.rest_id, a.rest_name, a.food_type, a.favorites, a.address, 
-round(avg(b.review_score), 2) as SCORE
+select a.rest_id, a.rest_name, a.food_type, a.favorites, a.address,
+round(avg(b.review_score), 2) as score
 from rest_info a
 join rest_review b on a.rest_id = b.rest_id
 where a.address like '서울%'
