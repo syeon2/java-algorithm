1f55af0f8ba0a31e99792bb194ed02361ab77cb9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-04-17 15:29:35 +0900

    [unrated] Title: 조건에 부합하는 중고거래 댓글 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ 프로그래머스/unrated/164673. 조건에 부합하는 중고거래 댓글 조회하기/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,6 +1,7 @@
-select a.title, a.board_id, b.reply_id, b.writer_id, b.contents, date_format(b.created_date, '%Y-%m-%d') as created_date
+select a.title, a.board_id, b.reply_id, b.writer_id, b.contents,
+date_format(b.created_date, '%Y-%m-%d') as CREATED_DATE
 from used_goods_board a
 join used_goods_reply b
     on a.board_id = b.board_id
 where year(a.created_date) = 2022 and month(a.created_date) = 10
-order by b.created_date asc, a.title asc;
+order by b.created_date, a.title;
