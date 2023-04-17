8158f7ca2743c10745da2fc98aea19c3e47998d5
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-04-17 15:52:32 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,8 +1,12 @@
-(SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") AS SALES_DATE, PRODUCT_ID, USER_ID, SALES_AMOUNT FROM ONLINE_SALE
-WHERE SALES_DATE BETWEEN DATE("2022-03-01") AND DATE("2022-03-31")
-
-UNION
-
-SELECT DATE_FORMAT(SALES_DATE, "%Y-%m-%d") AS SALES_DATE, PRODUCT_ID, NULL AS USER_ID, SALES_AMOUNT FROM OFFLINE_SALE
-WHERE SALES_DATE BETWEEN DATE("2022-03-01") AND DATE("2022-03-31"))
-ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
+(select date_format(sales_date, '%Y-%m-%d') as sales_date,
+ product_id, user_id, sales_amount
+ from online_sale
+ where year(sales_date) = 2022 and month(sales_date) = 3
+ 
+ union
+ 
+ select date_format(sales_date, '%Y-%m-%d') as sales_date,
+ product_id, NULL as user_id, sales_amount
+ from offline_sale
+ where year(sales_date) = 2022 and month(sales_date) = 3)
+order by sales_date, product_id, user_id;
