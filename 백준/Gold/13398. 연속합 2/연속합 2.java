3fd0ae60bf00556a87aa4b5048b5e94c47fcfd44
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-04-19 06:46:07 +0900

    [Gold V] Title: 연속합 2, Time: 276 ms, Memory: 23856 KB -BaekjoonHub

--- 백준/Gold/13398. 연속합 2/연속합 2.java
+++ 백준/Gold/13398. 연속합 2/연속합 2.java
@@ -1,47 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] list;
-	static int[] lDp;
+	static int[] nList;
 	static int[] rDp;
+	static int[] lDp;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		list = new int[N];
-		lDp = new int[N];
-		rDp = new int[N];
+        int N = Integer.parseInt(br.readLine());
+		nList = new int[N + 1];
+		rDp = new int[N + 1];
+		lDp = new int[N + 1];
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			list[i] = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			nList[i] = Integer.parseInt(st.nextToken());
 		}
 
 		// bottom-up
-		lDp[0] = list[0];
+		for (int i = 1; i <= N; i++) {
+			rDp[i] = Math.max(rDp[i - 1] + nList[i], nList[i]);
+		}
+
+		for (int i = N; i > 0; i--) {
+			if (i == N) lDp[i] = nList[i];
+			else lDp[i] = Math.max(lDp[i + 1] + nList[i], nList[i]);
+		}
+
+		int answer = rDp[1];
+		for (int i = 1; i <= N; i++) {
+			answer = Math.max(answer, rDp[i]);
+		}
+
 		for (int i = 1; i < N; i++) {
-			lDp[i] = Math.max(lDp[i - 1] + list[i], list[i]);
-		}
-
-		rDp[N - 1] = list[N - 1];
-		for (int i = N - 2; i >= 0; i--) {
-			rDp[i] = Math.max(rDp[i + 1] + list[i], list[i]);
-		}
-
-
-		int answer = lDp[0];
-		for (int i = 0; i < N; i++) {
-			answer = Math.max(answer, lDp[i]);
-
-			if (i > 0 && i < N - 1) answer = Math.max(answer, lDp[i - 1] + rDp[i + 1]);
+			if (nList[i] < 0) answer = Math.max(answer, rDp[i - 1] + lDp[i + 1]);
 		}
 
 		bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
-	}
+    }
 }
