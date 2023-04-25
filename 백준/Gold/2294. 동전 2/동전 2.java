eda2e0d425654df8bd5693387fd44c8f8ca57c53
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-04-26 07:54:29 +0900

    [Gold V] Title: 동전 2, Time: 156 ms, Memory: 18236 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
@@ -11,9 +11,9 @@ public class Main {
 	static int[] nList;
 	static int[][] dp;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
@@ -26,26 +26,22 @@ public class Main {
 			nList[i] = Integer.parseInt(br.readLine());
 		}
 
-		for (int i = 0; i <= N; i++) {
-			Arrays.fill(dp[i], 100_001);
-		}
+		Arrays.fill(dp[0], 100001);
+		dp[0][0] = 0;
 
-		// bottom-up
-		dp[0][0] = 1;
-		for (int i = 1; i <= N; i++) {
-			dp[i][0] = 0;
+		for (int n = 1; n <= N; n++) {
 
-			for (int k = 1; k <= K; k++) {
-				if (nList[i] > k) dp[i][k] = dp[i - 1][k];
-				else if (nList[i] <= k) dp[i][k] = Math.min(dp[i - 1][k], dp[i][k - nList[i]] + 1);
+			for (int k = 0; k <= K; k++) {
+				dp[n][k] = dp[n - 1][k];
+
+				if (k - nList[n] >= 0) dp[n][k] = Math.min(dp[n][k], dp[n][k - nList[n]] + 1);
 			}
 		}
 
-		int answer = -1;
-		if (dp[N][K] != 100_001) answer = dp[N][K];
-
+		int answer = dp[N][K];
+		if (answer == 100001) answer = -1;
 		bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
-	}
+    }
 }
