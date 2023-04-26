8f8d957f601171e48e8c9d0de883e31277dafeec
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-04-27 07:50:33 +0900

    [Gold V] Title: 크리보드, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Gold/11058. 크리보드/크리보드.java
+++ 백준/Gold/11058. 크리보드/크리보드.java
@@ -1,27 +1,30 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main {
 
 	static long[] dp;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 		dp = new long[N + 1];
 
-		// bottom-up
 		for (int i = 1; i <= N; i++) {
-			dp[i] = i;
 
-			for (int k = 3; k <= i; k++) {
-				dp[i] = Math.max(dp[i], dp[i - k] * (k - 1));
+			dp[i] = i;
+			for (int k = 1; k <= i - 3; k++) {
+				dp[i] = Math.max(dp[i], dp[k] * (i - k - 1));
 			}
 		}
 
 		bw.write(String.valueOf(dp[N]));
 		bw.flush();
 		bw.close();
-	}
+    }
 }
