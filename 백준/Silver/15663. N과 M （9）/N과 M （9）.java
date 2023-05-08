22e7f329ab69b5880a12008c98cbcba8f5e7b636
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-05-08 09:41:09 +0900

    [Silver II] Title: N과 M (9), Time: 200 ms, Memory: 20356 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] list = new int[10];
+	static int[] count = new int[10];
+	static int[] answer = new int[10];
+	static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int[] temp = new int[N];
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < N; i++) {
+			temp[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(temp);
+		int k = 0;
+		int x = temp[0];
+		int c = 1;
+		for (int i = 1; i < N; i++) {
+			if (x == temp[i]) c += 1;
+			else {
+				list[k] = x;
+				count[k] = c;
+				k += 1;
+				x = temp[i];
+				c = 1;
+			}
+		}
+		list[k] = x;
+		count[k] = c;
+		N = k + 1;
+		recur(0, N, M);
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+    }
+
+	static void recur(int index, int n, int m) {
+		if (index == m) {
+			for (int i = 0; i < m; i++) {
+				sb.append(list[answer[i]]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = 0; i < n; i++) {
+			if (count[i] > 0) {
+				count[i] -= 1;
+				answer[index] = i;
+				recur(index + 1, n, m);
+				count[i] += 1;
+			}
+		}
+	}
+}
