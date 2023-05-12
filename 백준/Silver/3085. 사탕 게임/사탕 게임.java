d53fb8fffb235ca8a6b32e5b442dec9cd491dc2a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-05-12 10:51:06 +0900

    [Silver II] Title: 사탕 게임, Time: 176 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -1,8 +1,8 @@
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -10,31 +10,38 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-
 		char[][] board = new char[N][N];
-		for (int i = 0; i < N; i++) {
 
+		for (int i = 0; i < N; i++) {
 			String str = br.readLine();
+
 			for (int k = 0; k < N; k++) {
 				board[i][k] = str.charAt(k);
 			}
 		}
 
-		int answer = 1;
+		int answer = 0;
+
 		for (int i = 0; i < N; i++) {
-
 			for (int k = 0; k < N - 1; k++) {
-				char temp = board[i][k]; board[i][k] = board[i][k + 1]; board[i][k + 1] = temp;
-				int check = check(board);
-				if (answer < check) answer = check;
-				temp = board[i][k]; board[i][k] = board[i][k + 1]; board[i][k + 1] = temp;
-			}
+				// row
+				char temp1 = board[i][k];
+				board[i][k] = board[i][k + 1];
+				board[i][k + 1] = temp1;
+				int rowCheck = check(board);
+				board[i][k + 1] = board[i][k];
+				board[i][k] = temp1;
 
-			for (int k = 0; k < N - 1; k++) {
-				char temp = board[k][i]; board[k][i] = board[k + 1][i]; board[k + 1][i] = temp;
-				int check = check(board);
-				if (answer < check) answer = check;
-				temp = board[k][i]; board[k][i] = board[k + 1][i]; board[k + 1][i] = temp;
+				// column
+				char temp2 = board[k][i];
+				board[k][i] = board[k + 1][i];
+				board[k + 1][i] = temp2;
+				int columnCheck = check(board);
+				board[k + 1][i] = board[k][i];
+				board[k][i] = temp2;
+
+				answer = Math.max(answer,
+					Math.max(rowCheck, columnCheck));
 			}
 		}
 
@@ -46,24 +53,20 @@ public class Main {
 	public static int check(char[][] board) {
 		int N = board.length;
 
-		int answer = 1;
-
+		int answer = 0;
 		for (int i = 0; i < N; i++) {
 
-			int count = 1;
+			int rowCount = 1;
+			int columnCount = 1;
 			for (int k = 0; k < N - 1; k++) {
-				if (board[i][k] == board[i][k + 1]) count++;
-				else count = 1;
+				if (board[i][k] == board[i][k + 1]) rowCount += 1;
+				else rowCount = 1;
 
-				if (answer < count) answer = count;
-			}
+				if (board[k][i] == board[k + 1][i]) columnCount += 1;
+				else columnCount = 1;
 
-			count = 1;
-			for (int k = 0; k < N - 1; k++) {
-				if (board[k][i] == board[k + 1][i]) count++;
-				else count = 1;
-
-				if (answer < count) answer = count;
+				answer = Math.max(answer,
+					Math.max(rowCount, columnCount));
 			}
 		}
 
