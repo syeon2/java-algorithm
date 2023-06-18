96496011901073c8779523bc98899ab2f9ddeac6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-18 14:40:28 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18112 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -6,41 +6,25 @@ import java.io.IOException;
 
 public class Main {
 
-	static int[] dp;
+	static int[] memo;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-		dp = new int[N + 1];
+		memo = new int[N + 1];
 
-		// bottom-up
-		for (int i = 2; i <= N; i++) {
-			dp[i] = dp[i - 1] + 1;
+		for (int i = 1; i <= N; i++) {
+			memo[i] = memo[i - 1] + 1;
 
-			if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-			if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+			if (i % 3 == 0) memo[i] = Math.min(memo[i], memo[i / 3] + 1);
+			if (i % 2 == 0) memo[i] = Math.min(memo[i], memo[i / 2] + 1);
 		}
-		// int answer = topDown(N);
 
-		bw.write(String.valueOf(dp[N]));
+		bw.write(String.valueOf(memo[N] - 1));
 		bw.flush();
 		bw.close();
     }
-
-	public static int topDown(int n) {
-		if (n == 1) return dp[n] = 0;
-
-		if (dp[n] == 0) {
-			dp[n] = n - 1;
-
-			if (n % 3 == 0) dp[n] = Math.min(dp[n], topDown(n / 3) + 1);
-			if (n % 2 == 0) dp[n] = Math.min(dp[n], topDown(n / 2) + 1);
-			dp[n] = Math.min(dp[n], topDown(n - 1) + 1);
-		}
-
-		return dp[n];
-	}
 }
