1a1ff86d05f874f72da44dc0f89290dfd8ca4312
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-22 08:49:00 +0900

    [Gold IV] Title: 가장 긴 증가하는 부분 수열 4, Time: 144 ms, Memory: 15500 KB -BaekjoonHub

--- 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
+++ 백준/Gold/14002. 가장 긴 증가하는 부분 수열 4/가장 긴 증가하는 부분 수열 4.java
@@ -1,84 +1,95 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int[] list;
 	static int[] dp;
-	static int[] bt;
+	static int[] tracking;
+	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		list = new int[N];
-		dp = new int[N];
-		bt = new int[N];
+
+        int N = Integer.parseInt(br.readLine());
+		list = new int[N + 1];
+		dp = new int[N + 1];
+		tracking = new int[N + 1];
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			list[i] = Integer.parseInt(st.nextToken());
 		}
 
-		// bottom-up
-		for (int i = 0; i < N; i++) {
-			dp[i] = 1;
-			bt[i] = -1;
+		// 점화식
+		// D[N] = D[I] + 1, I > 0, I < N, A[N] > A[I]
 
-			for (int k = 0; k < i; k++) {
-				if (list[i] > list[k] && dp[i] < dp[k] + 1) {
-					dp[i] = dp[k] + 1;
-					bt[i] = k;
-				}
-			}
+		// bottom-up
+		// for (int i = 1; i <= N; i++) {
+		//
+		// 	dp[i] = 1;
+		// 	for (int k = 1; k < i; k++) {
+		// 		if (list[i] > list[k] && dp[i] < dp[k] + 1) {
+		// 			dp[i] = dp[k] + 1;
+		// 			tracking[i] = k;
+		// 		}
+		// 	}
+		// }
+
+		for (int i = 1; i <= N; i++) {
+			topDown(i);
 		}
 
-		// topDown(N - 1);
 
 		int answer = 0;
 		int idx = 0;
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			if (answer < dp[i]) {
 				answer = dp[i];
 				idx = i;
 			}
 		}
 
-		sb.append(answer).append("\n");
-		backTracking(idx, sb);
+		sb.append(String.valueOf(answer)).append("\n");
+		backtracking(idx);
 
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
-	}
+    }
 
-	public static int topDown(int N) {
-		if (N == 0) return dp[N] = 1;
+	public static int topDown(int n) {
+		if (n == 1) return dp[n] = 1;
 
-		if (dp[N] == 0) {
-			dp[N] = 1;
+		if (dp[n] == 0) {
+			dp[n] = 1;
 
-			for (int i = 0; i < N; i++) {
-				if (list[N] > list[i] && dp[N] < topDown(i) + 1) {
-					dp[N] = topDown(i) + 1;
-					bt[N] = i;
+			for (int i = 1; i < n; i++) {
+				if (list[n] > list[i] && dp[n] < topDown(i) + 1) {
+					dp[n] = topDown(i) + 1;
+
+					tracking[n] = i;
 				}
 			}
 		}
 
-		return dp[N];
+		return dp[n];
 	}
 
-	public static void backTracking(int i, StringBuilder sb) {
-		if (bt[i] == -1) {
-			sb.append(list[i]).append(" ");
+	public static void backtracking(int n) {
+		if (tracking[n] == 0) {
+			sb.append(list[n]).append(" ");
 			return;
 		}
 
-		backTracking(bt[i], sb);
+		backtracking(tracking[n]);
 
-		sb.append(list[i]).append(" ");
+		sb.append(list[n]).append(" ");
 	}
 }
