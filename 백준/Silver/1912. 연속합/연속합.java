d83b492730c83f33a95eb545b884b0f664175530
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-22 09:05:11 +0900

    [Silver II] Title: 연속합, Time: 268 ms, Memory: 24128 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.java
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -1,51 +1,46 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int[] list;
-	static Integer[] dp;
+	static int[] dp;
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		list = new int[N];
-		dp = new Integer[N];
+
+		list = new int[N + 1];
+		dp = new int[N + 1];
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			list[i] = Integer.parseInt(st.nextToken());
+			dp[i] = list[i];
 		}
+
+		// 점화식
+		// D[N] = Max(A[N], D[I] + A[N]), I > 0, I < N
 
 		// bottom-up
-		for (int i = 0; i < N; i++) {
-			if (i == 0) dp[i] = list[i];
-			else {
-				dp[i] = Math.max(list[i], dp[i - 1] + list[i]);
-			}
+		for (int i = 1; i <= N; i++) {
+
+			dp[i] = Math.max(list[i], dp[i - 1] + list[i]);
 		}
 
-		// topDown(N - 1);
-
-		int answer = dp[0];
-		for (int i = 0; i < N; i++) {
+		int answer = Integer.MIN_VALUE;
+		for (int i = 1; i <= N; i++) {
 			answer = Math.max(answer, dp[i]);
 		}
 
 		bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
-	}
-
-	public static int topDown(int N) {
-		if (N == 0) return dp[N] = list[N];
-
-		if (dp[N] == null) {
-			dp[N] = Math.max(list[N], topDown(N - 1) + list[N]);
-		}
-
-		return dp[N];
 	}
 }
