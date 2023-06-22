0434e74317d6913bb4c1a943e014eff8360c51b3
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-23 06:55:13 +0900

    [Gold V] Title: 합분해, Time: 124 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.java
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -1,5 +1,8 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -7,9 +10,9 @@ public class Main {
 	static int[][] dp;
 	static int MOD = 1_000_000_000;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int N = Integer.parseInt(st.nextToken());
@@ -17,31 +20,22 @@ public class Main {
 
 		dp = new int[K + 1][N + 1];
 
-		// bottom-up
 		dp[0][0] = 1;
-		for (int k = 1; k <= K; k++) {
-			for (int n = 0; n <= N; n++) {
-				if (n == 0) dp[k][n] = 1;
-				else {
-					dp[k][n] = (dp[k - 1][n] + dp[k][n - 1]) % MOD;
-				}
-			}
+		for (int i = 1; i <= N; i++) {
+			dp[1][i] = 1;
 		}
 
-		// int answer = topDown(N, K);
+		// bottom-up
+		for (int k = 2; k <= K; k++) {
+
+			for (int n = 0; n <= N; n++) {
+				if (n == 0) dp[k][n] = 1;
+				else dp[k][n] = (dp[k - 1][n] + dp[k][n - 1]) % MOD;
+			}
+		}
 
 		bw.write(String.valueOf(dp[K][N]));
 		bw.flush();
 		bw.close();
-	}
-
-	public static int topDown(int N, int K) {
-		if (K == 1 || N == 0) return dp[K][N] = 1;
-
-		if (dp[K][N] == 0) {
-			dp[K][N] = (topDown(N - 1, K) + topDown(N, K - 1)) % MOD;
-		}
-
-		return dp[K][N];
-	}
+    }
 }
