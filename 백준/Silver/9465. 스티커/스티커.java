807adf68c9cdde7076b1fd82a3564a6e97a81125
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-23 12:34:56 +0900

    [Silver I] Title: 스티커, Time: 836 ms, Memory: 113560 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -7,7 +7,7 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[][] nList;
+	static int[][] list;
 	static int[][] dp;
 
     public static void main(String[] args) throws IOException {
@@ -15,41 +15,35 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 
-        int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 0; t < T; t++) {
 			int N = Integer.parseInt(br.readLine());
-			nList = new int[N + 1][2];
-			dp = new int[N + 1][2];
+
+			list = new int[2][N + 1];
+			dp = new int[3][N + 1];
 
 			for (int i = 0; i < 2; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 				for (int k = 1; k <= N; k++) {
-					nList[k][i] = Integer.parseInt(st.nextToken());
-					dp[k][i] = -1;
+					list[i][k] = Integer.parseInt(st.nextToken());
 				}
 			}
 
-			// // bottom-up
-			dp[1][0] = nList[1][0];
-			dp[1][1] = nList[1][1];
+			// bottom-up
+			dp[0][1] = list[0][1];
+			dp[1][1] = list[1][1];
 
 			for (int i = 2; i <= N; i++) {
-				dp[i][0] = Math.max(dp[i - 1][1],
-					Math.max(dp[i - 2][0], dp[i - 2][1])) + nList[i][0];
-
-				dp[i][1] = Math.max(dp[i - 1][0],
-					Math.max(dp[i - 2][0], dp[i - 2][1])) + nList[i][1];
+				dp[0][i] = Math.max(dp[1][i - 1], dp[2][i - 1]) + list[0][i];
+				dp[1][i] = Math.max(dp[0][i - 1], dp[2][i - 1]) + list[1][i];
+				dp[2][i] = Math.max(dp[0][i - 1], Math.max(dp[1][i - 1], dp[2][i - 1]));
 			}
 
-			// topDown(N, 0);
-			// topDown(N, 1);
-
 			int answer = 0;
-			for (int i = 1; i <= N; i++) {
-				answer = Math.max(answer,
-					Math.max(dp[i][0], dp[i][1]));
+			for (int i = 0; i < 3; i++) {
+				answer = Math.max(answer, dp[i][N]);
 			}
 
 			sb.append(answer).append("\n");
@@ -59,20 +53,4 @@ public class Main {
 		bw.flush();
 		bw.close();
     }
-
-	public static int topDown(int n, int i) {
-		if (n == 1) return dp[n][i] = nList[n][i];
-
-		if (dp[n][i] == -1) {
-			if (i == 0) {
-				dp[n][i] = Math.max(topDown(n - 1, 1),
-					Math.max(topDown(n - 2, 0), topDown(n - 2, 1))) + nList[n][i];
-			} else {
-				dp[n][i] = Math.max(topDown(n - 1, 0),
-					Math.max(topDown(n - 2, 0), topDown(n - 2, 1))) + nList[n][i];
-			}
-		}
-
-		return dp[n][i];
-	}
 }
