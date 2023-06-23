3e483eb6832eea881d6347aa663aa1f332d0a6d2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-24 08:24:10 +0900

    [Silver I] Title: 정수 삼각형, Time: 284 ms, Memory: 26652 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,4 +1,8 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -6,39 +10,37 @@ public class Main {
 	static int[][] list;
 	static int[][] dp;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 		list = new int[N + 1][N + 1];
 		dp = new int[N + 1][N + 1];
 
 		for (int i = 1; i <= N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
 
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 			for (int k = 1; k <= i; k++) {
 				list[i][k] = Integer.parseInt(st.nextToken());
-				dp[i][k] = -1;
 			}
 		}
 
-		// bottom-up
-		dp[1][1] = list[1][1];
-		for (int i = 2; i <= N; i++) {
+		// 점화식
+		// D[N][M] = max(D[N - 1][I - 1], D[N - 1][I]) + A[N][M], I > 0, I <= N
 
-			for (int k = 1; k <= i; k++) {
-				if (k == 1) dp[i][k] = dp[i - 1][k] + list[i][k];
-				else if (k == i) dp[i][k] = dp[i - 1][k - 1] + list[i][k];
-				else {
-					dp[i][k] = Math.max(dp[i - 1][k - 1], dp[i - 1][k]) + list[i][k];
+		// bottom-up
+		for (int i = 1; i <= N; i++) {
+
+			if (i == 1) dp[i][1] = list[i][1];
+			else {
+				for (int k = 1; k <= i; k++) {
+					if (k == 1) dp[i][k] = dp[i - 1][k] + list[i][k];
+					else if (k == i) dp[i][k] = dp[i - 1][k - 1] + list[i][k];
+					else dp[i][k] = Math.max(dp[i - 1][k - 1], dp[i - 1][k]) + list[i][k];
 				}
 			}
 		}
-
-		// for (int k = 1; k <= N; k++) {
-		// 	topDown(N, k);
-		// }
 
 		int answer = 0;
 		for (int i = 1; i <= N; i++) {
@@ -48,19 +50,5 @@ public class Main {
 		bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
-	}
-
-	public static int topDown(int n, int i) {
-		if (n == 1) return dp[n][i] = list[n][i];
-
-		if (dp[n][i] == -1) {
-			if (i == 1) dp[n][i] = topDown(n - 1, i) + list[n][i];
-			else if (i == n) dp[n][i] = topDown(n - 1, i - 1) + list[n][i];
-			else {
-				dp[n][i] = Math.max(topDown(n - 1, i - 1), topDown(n - 1, i)) + list[n][i];
-			}
-		}
-
-		return dp[n][i];
-	}
+    }
 }
