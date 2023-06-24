efa24141c846ed4fa502d2549fc1f0accb18265f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-25 07:41:06 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 156 ms, Memory: 15012 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -1,86 +1,102 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
 
 	static int[] list;
-	static int[] lDp;
 	static int[] rDp;
+	static int[] lDp;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		list = new int[N];
-		lDp = new int[N];
-		rDp = new int[N];
+        int N = Integer.parseInt(br.readLine());
+		list = new int[N + 1];
+		rDp = new int[N + 1];
+		lDp = new int[N + 1];
 
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
+		for (int i = 1; i <= N; i++) {
 			list[i] = Integer.parseInt(st.nextToken());
 		}
 
+		// 점화식
+		// rD[N] = max(rD[I]) + 1, I > 0, I < N, list[N] > list[I]
+		// lD[N] = max(lD[I]) + 1, I > 0, I < N, list[N] < list[I]
+
 		// bottom-up
-		// for (int i = 0; i < N; i++) {
-		// 	lDp[i] = 1;
+		// for (int i = 1; i <= N; i++) {
 		//
-		// 	for (int k = 0; k < i; k++) {
-		// 		if (list[i] > list[k]) lDp[i] = Math.max(lDp[i], lDp[k] + 1);
+		// 	rDp[i] = 1;
+		// 	for (int k = 1; k < i; k++) {
+		// 		if (list[i] > list[k]) {
+		// 			rDp[i] = Math.max(rDp[i], rDp[k] + 1);
+		// 		}
 		// 	}
 		// }
 		//
-		// for (int i = N - 1; i >= 0; i--) {
-		// 	rDp[i] = 1;
+		// for (int i = N; i > 0; i--) {
 		//
-		// 	for (int k = N - 1; k > i; k--) {
-		// 		if (list[i] > list[k]) rDp[i] = Math.max(rDp[i], rDp[k] + 1);
+		// 	lDp[i] = 1;
+		// 	for (int k = N; k > i; k--) {
+		// 		if (list[i] > list[k]) {
+		// 			lDp[i] = Math.max(lDp[i], lDp[k] + 1);
+		// 		}
 		// 	}
 		// }
 
-		for (int i = 0; i < N; i++) {
-			lDpTopDown(i);
+		for (int i = 1; i <= N; i++) {
+			rTopDown(i);
 		}
 
-		for (int i = N - 1; i >= 0; i--) {
-			rDpTopDown(i);
+		for (int i = N; i > 0; i--) {
+			lTopDown(i, N);
 		}
 
 		int answer = 0;
-		for (int i = 0; i < N; i++) {
-			answer = Math.max(answer, lDp[i] + rDp[i] - 1);
+		for (int i = 1; i <= N; i++) {
+			answer = Math.max(answer, rDp[i] + lDp[i] - 1);
 		}
 
 		bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
-	}
+    }
 
-	public static int lDpTopDown(int n) {
-		if (n == 0) return lDp[n] = 1;
-
-		if (lDp[n] == 0) {
-			lDp[n] = 1;
-			
-			for (int i = 0; i < n; i++) {
-				if (list[n] > list[i]) lDp[n] = Math.max(lDp[n], lDpTopDown(i) + 1);
-			}
-		}
-
-		return lDp[n];
-	}
-
-	public static int rDpTopDown(int n) {
-		if (n == list.length - 1) return rDp[n] = 1;
+	public static int rTopDown(int n) {
+		if (n == 1) return rDp[n] = 1;
 
 		if (rDp[n] == 0) {
 			rDp[n] = 1;
-			
-			for (int i = list.length - 1; i > n; i--) {
-				if (list[n] > list[i]) rDp[n] = Math.max(rDp[n], rDpTopDown(i) + 1);
+
+			for (int i = 1; i < n; i++) {
+				if (list[n] > list[i]) {
+					rDp[n] = Math.max(rDp[n], rTopDown(i) + 1);
+				}
 			}
 		}
 
 		return rDp[n];
+	}
+
+	public static int lTopDown(int n, int N) {
+		if (n == N) return lDp[n] = 1;
+
+		if (lDp[n] == 0) {
+			lDp[n] = 1;
+
+			for (int i = N; i > n; i--) {
+				if (list[n] > list[i]) {
+					lDp[n] = Math.max(lDp[n], lTopDown(i, N) + 1);
+				}
+			}
+		}
+
+		return lDp[n];
 	}
 }
