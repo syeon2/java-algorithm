1fd225a71c8d3d14183aecb35de35f3f15789f50
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-29 08:56:40 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double n = (double) num1 / (double) num2;
+        n = n * 1000;
+        
+        return (int) n;
+    }
+}
