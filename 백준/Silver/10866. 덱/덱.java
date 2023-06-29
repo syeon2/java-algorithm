13d7b410917b92ea716b07bc1572b6c0d7584307
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-29 15:05:39 +0900

    [Silver IV] Title: 덱, Time: 208 ms, Memory: 19048 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		Deque<Integer> queue = new LinkedList<>();
+
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			String cmd = st.nextToken();
+
+			if (cmd.equals("push_front")) {
+				Integer n = Integer.parseInt(st.nextToken());
+
+				queue.addFirst(n);
+			} else if (cmd.equals("push_back")) {
+				Integer n = Integer.parseInt(st.nextToken());
+
+				queue.addLast(n);
+			} else if (cmd.equals("front")) {
+				if (queue.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(queue.peekFirst()).append("\n");
+			} else if (cmd.equals("back")) {
+				if (queue.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(queue.peekLast()).append("\n");
+			} else if (cmd.equals("pop_front")) {
+				if (queue.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(queue.removeFirst()).append("\n");
+			} else if (cmd.equals("pop_back")) {
+				if (queue.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(queue.removeLast()).append("\n");
+			} else if (cmd.equals("size")) {
+				sb.append(queue.size()).append("\n");
+			} else if (cmd.equals("empty")) {
+				if (queue.isEmpty()) sb.append(1).append("\n");
+				else sb.append(0).append("\n");
+			}
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+    }
+}
