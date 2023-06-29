4793c065fb85c4319261be4f19e049ac8cb26e37
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-30 07:52:06 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 140 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		bw.write(String.valueOf(gcm(A, B)));
+		bw.newLine();
+		bw.write(String.valueOf(lcm(A, B)));
+
+		bw.flush();
+		bw.close();
+    }
+
+	public static int gcm(int a, int b) {
+		if (a % b == 0) return b;
+		else return gcm(b, a % b);
+	}
+
+	public static int lcm(int a, int b){
+		return a * b / gcm(a, b);
+	}
+}
