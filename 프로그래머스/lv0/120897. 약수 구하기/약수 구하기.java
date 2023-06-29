b263f3704820b06d65e7d03006339cdf583f4462
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-29 11:44:29 +0900

    [level 0] Title: 약수 구하기, Time: 2.14 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,9 @@
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n) {
+        return IntStream.range(1, n + 1)
+            .filter(i -> n % i == 0)
+            .toArray();
+    }
+}
