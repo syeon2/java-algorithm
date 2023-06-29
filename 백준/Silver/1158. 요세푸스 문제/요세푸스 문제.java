8a53436d1ec1d12dd001580e732c4d1825eaa21f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-06-29 14:56:03 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 512 ms, Memory: 294580 KB -BaekjoonHub

---
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		Queue<Integer> queue = new LinkedList<>();
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		for (int i = 1; i <= N; i++) {
+			queue.add(i);
+		}
+
+		sb.append("<");
+
+		int target = 1;
+		while (queue.size() != 1) {
+			Integer remove = queue.remove();
+
+			if (target != K) {
+				queue.add(remove);
+				target++;
+			}
+			else {
+				target = 1;
+				sb.append(remove).append(", ");
+			}
+		}
+
+		sb.append(queue.remove()).append(">");
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+    }
+}
