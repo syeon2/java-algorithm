05ae907be3408a8095a644485e702e6704cb4594
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-07-09 05:57:17 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -9,20 +9,33 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
+        // 시간복잡도와 풀이 방식을 고민하면서 풀기
+        // 시간초를 두고 문제를 이해하면서 풀기
 
-		sb.append((A + B) % C).append("\n")
-			.append(((A % C) + (B % C)) % C).append("\n")
-			.append((A * B) % C).append("\n")
-			.append(((A % C) * (B % C)) % C);
+        // Result : A, B, C, D를 구하는 문제
+        // How : 문제에서 주어진 연산을 그대로 하면 풀 수 있는 문제
 
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+        int caseA = (A + B) % C;
+        int caseB = ((A % C) + (B % C)) % C;
+        int caseC = (A * B) % C;
+        int caseD = ((A % C) * (B % C)) % C;
+
+        bw.write(String.valueOf(caseA));
+        bw.newLine();
+        bw.write(String.valueOf(caseB));
+        bw.newLine();
+        bw.write(String.valueOf(caseC));
+        bw.newLine();
+        bw.write(String.valueOf(caseD));
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
     }
 }
