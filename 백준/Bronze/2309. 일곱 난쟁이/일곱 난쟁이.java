2796dfbc9d855aed6aab4e613619df9bdc7e4e2c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-07-12 10:31:02 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -9,27 +9,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
+
+        // 아옵명 중 7명을 찾는 문제 -> 역으로 나머지 2명을 찾아내면 7명을 구할 수 있다.
+		// 전체 N은 100을 넘지 않고 2명을 찾으면 되는 문제이기 때문에 Brute Force 알고리즘으로 풀어낼 수 있다.
 
 		int total = 0;
-        int[] list = new int[9];
+		int[] list = new int[9];
 		for (int i = 0; i < 9; i++) {
-			int num = Integer.parseInt(br.readLine());
-			list[i] = num;
-			total += num;
+			list[i] = Integer.parseInt(br.readLine());
+			total += list[i];
 		}
+
 		Arrays.sort(list);
-
-		for (int i = 0; i < 9; i++) {
-			for (int k = i + 1; k < 9; k++) {
+		for (int i = 0; i < list.length; i++) {
+			for (int k = i + 1; k < list.length; k++) {
 				if (total - list[i] - list[k] == 100) {
-					for (int l = 0; l < 9; l++) {
-						if (l == i || l == k) continue;
 
-						sb.append(list[l]).append("\n");
+					for (int l = 0; l < list.length; l++) {
+						if (l == i || l == k) continue;
+						bw.write(String.valueOf(list[l]));
+						bw.newLine();
 					}
 
-					bw.write(sb.toString());
 					bw.flush();
 					bw.close();
 					return;
