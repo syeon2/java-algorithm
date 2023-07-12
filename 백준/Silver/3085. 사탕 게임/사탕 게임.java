4056cbb6dd7a10bdd5ebc35e4ae68b23c9131535
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-07-12 11:06:17 +0900

    [Silver II] Title: 사탕 게임, Time: 148 ms, Memory: 14488 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -3,45 +3,55 @@ import java.io.InputStreamReader;
 import java.io.BufferedWriter;
 import java.io.OutputStreamWriter;
 import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine());
+        // 주어진 보드 안에서 먼저 문자를 교환한 후 전체 체크 한다.
+		// 성능을 개선하고 싶으면 교환된 열, 행만 비교해서 Max값을 갱신시킨다.
+
+		int N = Integer.parseInt(br.readLine());
 		char[][] board = new char[N][N];
+		int answer = 0;
 
-		for (int i = 0; i < N; i++) {
-			String str = br.readLine();
+		for (int y = 0; y < N; y++) {
 
+			char[] list = br.readLine().toCharArray();
 			for (int k = 0; k < N; k++) {
-				board[i][k] = str.charAt(k);
+				board[y][k] = list[k];
 			}
 		}
 
-		int answer = 0;
+		for (int y = 0; y < N; y++) {
+			for (int x = 0; x < N - 1; x++) {
+				char temp = board[y][x];
 
-		for (int i = 0; i < N; i++) {
-			for (int k = 0; k < N - 1; k++) {
-				// row
-				char temp1 = board[i][k];
-				board[i][k] = board[i][k + 1];
-				board[i][k + 1] = temp1;
-				int rowCheck = check(board);
-				board[i][k + 1] = board[i][k];
-				board[i][k] = temp1;
+				board[y][x] = board[y][x + 1];
+				board[y][x + 1] = temp;
 
-				// column
-				char temp2 = board[k][i];
-				board[k][i] = board[k + 1][i];
-				board[k + 1][i] = temp2;
-				int columnCheck = check(board);
-				board[k + 1][i] = board[k][i];
-				board[k][i] = temp2;
+				answer = Math.max(answer, check(board, x, y));
+				answer = Math.max(answer, check(board, x + 1, y));
 
-				answer = Math.max(answer,
-					Math.max(rowCheck, columnCheck));
+				board[y][x + 1] = board[y][x];
+				board[y][x] = temp;
+			}
+		}
+
+		for (int y = 0; y < N - 1; y++) {
+			for (int x = 0; x < N; x++) {
+				char temp = board[y][x];
+
+				board[y][x] = board[y + 1][x];
+				board[y + 1][x] = temp;
+
+				answer = Math.max(answer, check(board, x, y));
+				answer = Math.max(answer, check(board, x, y + 1));
+
+				board[y + 1][x] = board[y][x];
+				board[y][x] = temp;
 			}
 		}
 
@@ -50,26 +60,29 @@ public class Main {
 		bw.close();
     }
 
-	public static int check(char[][] board) {
-		int N = board.length;
+	public static int check(char[][] board, int x, int y) {
+		int temp = 1;
 
-		int answer = 0;
-		for (int i = 0; i < N; i++) {
-
-			int rowCount = 1;
-			int columnCount = 1;
-			for (int k = 0; k < N - 1; k++) {
-				if (board[i][k] == board[i][k + 1]) rowCount += 1;
-				else rowCount = 1;
-
-				if (board[k][i] == board[k + 1][i]) columnCount += 1;
-				else columnCount = 1;
-
-				answer = Math.max(answer,
-					Math.max(rowCount, columnCount));
+		int xLine = 1;
+		for (int y2 = 0; y2 < board.length - 1; y2++) {
+			if (board[y2][x] == board[y2 + 1][x]) xLine++;
+			else {
+				temp = Math.max(temp, xLine);
+				xLine = 1;
 			}
 		}
+		temp = Math.max(temp, xLine);
 
-		return answer;
+		int yLine = 1;
+		for (int x2 = 0; x2 < board.length - 1; x2++) {
+			if (board[y][x2] == board[y][x2 + 1]) yLine++;
+			else {
+				temp = Math.max(temp, yLine);
+				yLine = 1;
+			}
+		}
+		temp = Math.max(temp, yLine);
+
+		return temp;
 	}
 }
