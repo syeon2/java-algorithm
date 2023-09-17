84b10878cb5a50edfa924c1f4caeaa16c54957c9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-09-18 08:22:11 +0900

    [Silver III] Title: 1, Time: 116 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/4375. 1/1.java
+++ 백준/Silver/4375. 1/1.java
@@ -1,37 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.Scanner;
+import java.io.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+class Main {
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-        // 나머지 연산의 특징을 이용한다.
-		// (n * m) % c 와 ((n % c) * (m % c)) % c는 같다는 성질을 이용하기
-		// ((1 % x) * (10 % x) + 1) % c
 
 		String str;
+
 		while ((str = br.readLine()) != null) {
-			int N = Integer.parseInt(str);
-			// 반복문으로 ((1 % N) * (10 % N) + 1) % N 을 반복한다.
+			int n = Integer.parseInt(str);
 
 			int num = 1;
-			int cnt = 1;
-			while (num % N != 0) {
-				num = (num * 10 + 1) % N;
-				cnt++;
-			}
+			int current = 1;
 
-			sb.append(cnt).append("\n");
+			while (num % n != 0) {
+				num = (num * 10 + 1) % n;
+				current += 1;
+			}
+			
+			bw.write(String.valueOf(current));
+			bw.newLine();
 		}
 
-		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
-    }
+	}
 }
