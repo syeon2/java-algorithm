e8208e90f8de7bb3e05255965c3994f3dc2deb98
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-09-18 07:56:46 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,40 +1,23 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
 
-public class Main {
+class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        // 시간복잡도와 풀이 방식을 고민하면서 풀기
-        // 시간초를 두고 문제를 이해하면서 풀기
-
-        // Result : A, B, C, D를 구하는 문제
-        // How : 문제에서 주어진 연산을 그대로 하면 풀 수 있는 문제
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int C = Integer.parseInt(st.nextToken());
-
-        int caseA = (A + B) % C;
-        int caseB = ((A % C) + (B % C)) % C;
-        int caseC = (A * B) % C;
-        int caseD = ((A % C) * (B % C)) % C;
-
-        bw.write(String.valueOf(caseA));
+        
+        String[] str = br.readLine().split(" ");
+        int a = Integer.parseInt(str[0]);
+        int b = Integer.parseInt(str[1]);
+        int c = Integer.parseInt(str[2]);
+        
+        bw.write(String.valueOf((a + b) % c));
         bw.newLine();
-        bw.write(String.valueOf(caseB));
+        bw.write(String.valueOf(((a % c) + (b % c)) % c));
         bw.newLine();
-        bw.write(String.valueOf(caseC));
+        bw.write(String.valueOf((a * b) % c));
         bw.newLine();
-        bw.write(String.valueOf(caseD));
-        bw.newLine();
-
+        bw.write(String.valueOf(((a % c) * (b % c)) % c));
+        
         bw.flush();
         bw.close();
     }
