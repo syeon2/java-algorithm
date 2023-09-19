23bd74c414d59afe24f3ef856f684bbe33ad0323
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-09-19 18:51:02 +0900

    [unrated] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -0,0 +1,14 @@
+select flavor
+from (
+    select * 
+    from july
+    
+    union all 
+    
+    select * 
+    from first_half
+) a
+group by flavor
+order by sum(total_order) desc
+limit 3;
+
