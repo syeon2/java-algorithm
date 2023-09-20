bc31d947ca25d893a7755e0c0299a5eb2902fd89
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-09-21 00:34:16 +0900

    [level 4] Title: 오프라인/온라인 판매 데이터 통합하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++ 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
@@ -1,12 +1,10 @@
-(select date_format(sales_date, '%Y-%m-%d') as sales_date,
- product_id, user_id, sales_amount
- from online_sale
- where year(sales_date) = 2022 and month(sales_date) = 3
+(select date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, user_id, sales_amount
+from online_sale
+where year(sales_date) = 2022 and month(sales_date) = 3
  
  union
  
- select date_format(sales_date, '%Y-%m-%d') as sales_date,
- product_id, NULL as user_id, sales_amount
+ select date_format(sales_date, '%Y-%m-%d') as sales_date, product_id, null as user_id, sales_amount
  from offline_sale
  where year(sales_date) = 2022 and month(sales_date) = 3)
-order by sales_date, product_id, user_id;
+ order by sales_date, product_id, user_id;
