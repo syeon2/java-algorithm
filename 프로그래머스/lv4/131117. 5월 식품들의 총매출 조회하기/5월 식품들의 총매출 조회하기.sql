bff78fb9fb57c17affa75b5ca68b36cf33430487
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-09-22 16:45:15 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -0,0 +1,7 @@
+select a.product_id, a.product_name, sum(a.price * b.amount) as total_sales
+from food_product a
+join food_order b
+    on a.product_id = b.product_id
+where year(b.produce_date) = 2022 and month(b.produce_date) = 5
+group by a.product_id
+order by total_sales desc, product_id;
