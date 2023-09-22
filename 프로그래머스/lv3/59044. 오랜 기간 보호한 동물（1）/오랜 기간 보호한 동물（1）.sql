f70747320f1314c7198f62fb000e1c32673f41f8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-09-22 16:54:09 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/lv3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,8 +1,7 @@
--- 코드를 입력하세요
-SELECT a.name, a.datetime
+select a.name, a.datetime
 from animal_ins a
 left join animal_outs b
-on a.animal_id = b.animal_id
-where b.datetime is null
+    on a.animal_id = b.animal_id
+where b.animal_id is null
 order by a.datetime
 limit 3;
