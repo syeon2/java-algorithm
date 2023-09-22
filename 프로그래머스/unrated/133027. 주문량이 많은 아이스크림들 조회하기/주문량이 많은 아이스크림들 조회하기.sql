57b348d9f83b814bda4fe0950c51e8d2eb0ce0ee
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-09-22 16:36:03 +0900

    [unrated] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/unrated/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,14 +1,8 @@
-select flavor
-from (
-    select * 
-    from july
-    
-    union all 
-    
-    select * 
-    from first_half
-) a
-group by flavor
-order by sum(total_order) desc
+select temp.flavor
+from (select a.flavor, sum(a.total_order + b.total_order) as orders
+        from first_half a
+        join july b
+            on a.flavor = b.flavor
+        group by a.flavor) temp
+order by temp.orders desc
 limit 3;
-
