427e7103467776bd76f9dba7485283564937ad75
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-09-30 09:33:31 +0900

    [Bronze II] Title: 나누기, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+		int F = Integer.parseInt(br.readLine());
+
+		int init = N - (N % 100);
+
+		for (int i = init; i < init + 100; i++) {
+			if (i % F == 0) {
+				String str = String.valueOf(i);
+
+				System.out.println(str.substring(str.length() - 2));
+				return;
+			}
+		}
+	}
+}
