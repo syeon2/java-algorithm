a70e47232fc406dfe5c7ece2545b5a9e32833b32
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-02 08:38:41 +0900

    [Bronze III] Title: 세 수, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+        int c = Integer.parseInt(list[2]);
+        
+        int[] answer = {a, b, c};
+        
+        Arrays.sort(answer);
+        
+        System.out.println(answer[1]);
+    }
+}
