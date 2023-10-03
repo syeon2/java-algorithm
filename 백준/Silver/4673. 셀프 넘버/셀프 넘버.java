5dd1300654ba2afcdfe475975d763ec4763eff9c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-03 12:18:54 +0900

    [Silver V] Title: 셀프 넘버, Time: 116 ms, Memory: 13912 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -1,32 +1,41 @@
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class Main {
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        boolean[] isExist = new boolean[10001];
-        
-        for (int i = 1; i <= 10000; i++) {
-            String[] numStr = String.valueOf(i).split("");
-            
-            int num = i;
-            for (int k = 0; k < numStr.length; k++) {
-                num += Integer.parseInt(numStr[k]);
-            }
-            
-            if (num <= 10000) isExist[num] = true;
-        }
-        
-        for (int i = 1; i < isExist.length; i++) {
-            if (!isExist[i]) {
-                bw.write(String.valueOf(i));
-                bw.newLine();
-            }
-        }
-        
-        bw.flush();
-        bw.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		boolean[] cache = new boolean[10001];
+
+		for (int i = 1; i <= 10000; i++) {
+			int num = d(i);
+
+			if (num <= 10000) {
+				cache[num] = true;
+			}
+		}
+
+		for (int i = 1; i <= 10000; i++) {
+			if (!cache[i]) {
+				sb.append(i).append("\n");
+			}
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+
+	public static int d(int num) {
+		int temp = num;
+
+		while (temp > 0) {
+			num += (temp % 10);
+			temp /= 10;
+		}
+		
+		return num;
+	}
 }
