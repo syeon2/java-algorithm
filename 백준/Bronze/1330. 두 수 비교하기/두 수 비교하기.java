c908a2b23a88ed8ff489ef2881bc2c01032036d1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-04 07:33:58 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,16 +1,15 @@
 import java.io.*;
 
 public class Main {
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         String[] str = br.readLine().split(" ");
-        int a = Integer.parseInt(str[0]);
-        int b = Integer.parseInt(str[1]);
+        int A = Integer.parseInt(str[0]);
+        int B = Integer.parseInt(str[1]);
         
-        if (a > b) System.out.println(">");
-        else if (a < b) System.out.println("<");
+        if (A > B) System.out.println(">");
+        else if (A < B) System.out.println("<");
         else System.out.println("==");
     }
 }
