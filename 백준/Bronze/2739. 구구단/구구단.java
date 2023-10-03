93cda9fd58682278b6efab3213795ba32c2858f8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-03 10:18:24 +0900

    [Bronze V] Title: 구구단, Time: 152 ms, Memory: 16012 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -3,18 +3,17 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int a = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 1; i <= 9; i++) {
-            sb.append(a).append(" * ").append(i).append(" = ").append(a * i).append("\n");
-        }
-        
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
+        System.out.println(N + " * 1 = " + (N * 1));
+        System.out.println(N + " * 2 = " + (N * 2));
+        System.out.println(N + " * 3 = " + (N * 3));
+        System.out.println(N + " * 4 = " + (N * 4));
+        System.out.println(N + " * 5 = " + (N * 5));
+        System.out.println(N + " * 6 = " + (N * 6));
+        System.out.println(N + " * 7 = " + (N * 7));
+        System.out.println(N + " * 8 = " + (N * 8));
+        System.out.println(N + " * 9 = " + (N * 9));
     }
 }
