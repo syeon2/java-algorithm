f72eeb79a8ad170a13b065edab7ad29fbacc09d4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-03 10:00:20 +0900

    [Bronze V] Title: Hello World, Time: 116 ms, Memory: 13856 KB -BaekjoonHub

--- 백준/Bronze/2557. Hello World/Hello World.java
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -1,14 +1,5 @@
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
-
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		bw.write("Hello World!");
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
