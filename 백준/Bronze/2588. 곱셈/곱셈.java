824b65abf56839f1d91ab46e0aaa3c448406b2be
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-04 07:59:18 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -1,23 +1,28 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int a = Integer.parseInt(br.readLine());
-        String[] b = br.readLine().split("");
-        
-        int answer = 0;
-        int pos = 1;
-        for (int i = b.length - 1; i >= 0; i--) {
-            int partB = Integer.parseInt(b[i]);
-            
-            int multiply = a * partB;
-            answer += (multiply * pos);
-            pos *= 10;
-            System.out.println(multiply);
-        }
-        
-        System.out.println(answer);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+
+		char[] list = String.valueOf(B).toCharArray();
+
+		int pos = 1;
+		int total = 0;
+
+		for (int i = list.length - 1; i >= 0; i--) {
+			int num = A * (list[i] - '0');
+
+			System.out.println(num);
+
+			total += num * pos;
+			pos *= 10;
+		}
+
+		System.out.println(total);
+	}
 }
