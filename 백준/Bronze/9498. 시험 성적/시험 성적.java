937f2a593ae1add87d615bd62e3ff44117167946
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-03 10:15:32 +0900

    [Bronze V] Title: 시험 성적, Time: 124 ms, Memory: 14352 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -4,22 +4,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        int num = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         
-        if (num >= 90) {
-            System.out.println("A");
-            return;
-        } else if (num >= 80) {
-            System.out.println("B");
-            return;
-        } else if (num >= 70) {
-            System.out.println("C");
-            return;
-        } else if (num >= 60) {
-            System.out.println("D");
-            return;
-        } else {
-            System.out.println("F");
-        }
+        if (N >= 90) System.out.println("A");
+        else if (N >= 80) System.out.println("B");
+        else if (N >= 70) System.out.println("C");
+        else if (N >= 60) System.out.println("D");
+        else System.out.println("F");
     }
 }
