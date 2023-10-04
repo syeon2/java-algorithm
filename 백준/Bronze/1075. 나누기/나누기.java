5192bba941be983d63c020c5628b48f432d8c9e8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-05 08:47:35 +0900

    [Bronze II] Title: 나누기, Time: 128 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/1075. 나누기/나누기.java
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -7,13 +7,13 @@ public class Main {
 		int N = Integer.parseInt(br.readLine());
 		int F = Integer.parseInt(br.readLine());
 
-		int init = N - (N % 100);
+		int init = (N / 100) * 100;
 
 		for (int i = init; i < init + 100; i++) {
 			if (i % F == 0) {
 				String str = String.valueOf(i);
 
-				System.out.println(str.substring(str.length() - 2));
+				System.out.println(str.substring(str.length()- 2));
 				return;
 			}
 		}
