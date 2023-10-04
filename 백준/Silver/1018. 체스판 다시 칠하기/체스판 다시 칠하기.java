97545f0ebb096d07d6b475ca1e7dd8b799e33994
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-04 09:01:54 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 132 ms, Memory: 14752 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -6,49 +6,56 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		String[] pos = br.readLine().split(" ");
-		int N = Integer.parseInt(pos[0]);
-		int M = Integer.parseInt(pos[1]);
+		String[] info = br.readLine().split(" ");
+		int N = Integer.parseInt(info[0]);
+		int M = Integer.parseInt(info[1]);
 
 		char[][] board = new char[N][M];
-
-		for (int i = 0; i < N; i++) {
+		for (int n = 0; n < N; n++) {
 			char[] list = br.readLine().toCharArray();
 
-			for (int k = 0; k < M; k++) {
-				board[i][k] = list[k];
+			for (int m = 0; m < M; m++) {
+				board[n][m] = list[m];
 			}
 		}
 
 		int answer = Integer.MAX_VALUE;
 
 		for (int i = 0; i <= N - 8; i++) {
+
 			for (int k = 0; k <= M - 8; k++) {
 				char target = board[i][k];
 
-				int count1 = 0;
-				int count2 = 0;
-				for (int z = i; z < i + 8; z++) {
-					for (int q = k; q < k + 8; q++) {
-						if (((z - i) % 2 == 0 && (q - k) % 2 == 0) ||
-							((z - i) % 2 == 1 && (q - k) % 2 == 1)) {
-							if (target != board[z][q]) {
-								count1++;
-							} else
-								count2++;
-						} else {
-							if (target == board[z][q]) {
-								count1++;
-							} else
-								count2++;
-						}
-					}
-				}
+				int num = check(board, k, i, target);
 
-				answer = Math.min(answer, Math.min(count1, count2));
+				answer = Math.min(answer, num);
 			}
 		}
 
 		System.out.println(answer);
+	}
+
+	public static int check(char[][] board, int x, int y, char target) {
+		int diff1 = 0;
+		int diff2 = 0;
+
+		for (int i = y; i < y + 8; i++) {
+
+			for (int k = x; k < x + 8; k++) {
+				if (((i - y) % 2 == 0 && (k - x) % 2 == 0) || ((i - y) % 2 == 1 && (k - x) % 2 == 1)) {
+					if (board[i][k] != target)
+						diff1++;
+					else
+						diff2++;
+				} else {
+					if (board[i][k] == target)
+						diff1++;
+					else
+						diff2++;
+				}
+			}
+		}
+
+		return Math.min(diff1, diff2);
 	}
 }
