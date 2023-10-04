8c5545b91988db6d3ed5ebb96322f50ac5058eba
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-05 08:12:19 +0900

    [Bronze III] Title: 약수 구하기, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,24 +1,24 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        
-        int a = Integer.parseInt(list[0]);
-        int b = Integer.parseInt(list[1]);
-        
-        int count = 0;
-        for (int i = 1; i <= a; i++) {
-            if (a % i == 0) count += 1;
-            
-            if (count == b) {
-                System.out.println(i);
-                return;
-            }
-        }
-        
-        System.out.println(0);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] list = br.readLine().split(" ");
+		int N = Integer.parseInt(list[0]);
+		int K = Integer.parseInt(list[1]);
+
+		for (int i = 1; i <= N; i++) {
+			if (N % i == 0) {
+				K--;
+			}
+
+			if (K == 0) {
+				System.out.println(i);
+				return;
+			}
+		}
+
+		System.out.println(0);
+	}
 }
