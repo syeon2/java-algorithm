1e36d919c99d59dca9b90e1d693a349962ec93fe
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-05 08:08:05 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -1,52 +1,34 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		List<Integer> list = new ArrayList<>();
-        int[] nList = new int[9];
-		int total = 0;
-
-		for (int i = 0; i < 9; i++) {
-			nList[i] = Integer.parseInt(br.readLine());
-			total += nList[i];
-		}
-
-		for (int i = 0; i < 9; i++) {
-			boolean find = false;
-
-			for (int k = i + 1; k < 9; k++) {
-				if (total - nList[i] - nList[k] == 100) {
-					find = true;
-
-					for (int l = 0; l < 9; l++) {
-						if (l != i && l != k) {
-							list.add(nList[l]);
-						}
-					}
-				}
-			}
-
-			if (find) break;
-		}
-
-		Collections.sort(list);
-
-		for (int i = 0; i < list.size(); i++) {
-			bw.write(String.valueOf(list.get(i)));
-			bw.newLine();
-		}
-
-		bw.flush();
-		bw.close();
+        
+        int[] list = new int[9];
+        int total = 0;
+        
+        for (int i = 0; i < 9; i++) {
+            int num = Integer.parseInt(br.readLine());
+                
+            list[i] = num;
+            total += num;
+        }
+        
+        Arrays.sort(list);
+        
+        for (int i = 0; i < 9; i++) {
+            for (int k = i + 1; k < 9; k++) {
+                if (total - list[i] - list[k] == 100) {
+                    for (int j = 0; j < 9; j++) {
+                        if (j == i || j == k) continue;
+                        
+                        System.out.println(list[j]);
+                    }
+                    
+                    return;
+                }
+            }
+        }
     }
 }
