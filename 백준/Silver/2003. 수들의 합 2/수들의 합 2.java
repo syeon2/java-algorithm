29868351b402a1b5aa2a147d901d25151f6ee13c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-05 08:27:44 +0900

    [Silver IV] Title: 수들의 합 2, Time: 244 ms, Memory: 19100 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,33 +1,36 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] strs = br.readLine().split(" ");
-        int N = Integer.parseInt(strs[0]);
-        int M = Integer.parseInt(strs[1]);
-        
-        String[] strs2 = br.readLine().split(" ");
-        int[] list = new int[N];
-        for (int i = 0; i < list.length; i++) {
-            list[i] = Integer.parseInt(strs2[i]);
-        }
-        
-        int count = 0;
-        for (int i = 0; i < list.length; i++) {
-            
-            int total = 0;
-            for (int k = i; k < list.length; k++) {
-                total += list[k];
-                
-                if (total == M) {
-                    count++;
-                    break;
-                } else if (total > M) break;
-            }
-        }
-        
-        System.out.println(count);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		String[] info = br.readLine().split(" ");
+		int N = Integer.parseInt(info[0]);
+		int M = Integer.parseInt(info[1]);
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] list = new int[N];
+		for (int i = 0; i < list.length; i++) {
+			list[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int count = 0;
+		for (int i = 0; i < N; i++) {
+			int sum = 0;
+
+			for (int k = i; k < N; k++) {
+				sum += list[k];
+
+				if (sum == M) {
+					count++;
+					break;
+				}
+
+				if (sum > M) break;
+			}
+		}
+
+		System.out.println(count);
+	}
 }
