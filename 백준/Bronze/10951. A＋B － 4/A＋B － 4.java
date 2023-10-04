fe368c1554aa1cd7fb3afd5426d69d8d102d4088
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-05 08:00:00 +0900

    [Bronze V] Title: A+B - 4, Time: 120 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -1,17 +1,21 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String str;
-        while ((str = br.readLine()) != null) {
-            String[] list = str.split(" ");
-            
-            int a = Integer.parseInt(list[0]);
-            int b = Integer.parseInt(list[1]);
-            
-            System.out.println(a + b);
-        }
-    }
+	public static void main(String args[]) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		String str;
+
+		while( (str=br.readLine()) != null ){
+
+			int a = str.charAt(0) - 48;
+			int b = str.charAt(2) - 48;
+			sb.append(a+b).append("\n");
+
+		}
+		System.out.print(sb);
+	}
 }
