d29064f61e0bcff82511f1f5b75b2c35f386af0c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-06 08:01:32 +0900

    [Bronze II] Title: 숫자의 개수, Time: 128 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,24 +1,30 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int total = 1;
-        
-        for (int i = 0; i < 3; i++) {
-            total *= Integer.parseInt(br.readLine());
-        }
-        
-        int[] cache = new int[10];
-        String value = String.valueOf(total);
-        
-        for (int i = 0; i < value.length(); i++) {
-            cache[value.charAt(i) - '0']++;
-        }
-        
-        for (int i = 0; i < cache.length; i++) {
-            System.out.println(cache[i]);
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int A = Integer.parseInt(br.readLine());
+		int B = Integer.parseInt(br.readLine());
+		int C = Integer.parseInt(br.readLine());
+
+		int total = A * B * C;
+
+		char[] list = String.valueOf(total).toCharArray();
+
+		int[] cache = new int[10];
+		for (int i = 0; i < list.length; i++) {
+			cache[list[i] - '0']++;
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < cache.length; i++) {
+			sb.append(cache[i]).append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
