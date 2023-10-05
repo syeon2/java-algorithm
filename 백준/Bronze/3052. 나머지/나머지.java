18480635c900d6ac0cc482c6a223bf31109f2446
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-06 07:53:00 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,22 +1,25 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        boolean[] list = new boolean[42];
-        
-        for (int i = 0; i < 10; i++) {
-            int num = Integer.parseInt(br.readLine());
-            
-            list[num % 42] = true;
-        }
-        
-        int count = 0;
-        for (int i = 0; i < list.length; i++) {
-            if (list[i]) count++;
-        }
-        
-        System.out.println(count);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		boolean[] cache = new boolean[43];
+		for (int i = 0; i < 10; i++) {
+			int num = Integer.parseInt(br.readLine());
+
+			int remain = num % 42;
+			cache[remain] = true;
+		}
+
+		int count = 0;
+		for (int i = 0; i < cache.length; i++) {
+			if (cache[i]) count++;
+		}
+
+		bw.write(String.valueOf(count));
+		bw.flush();
+		bw.close();
+	}
 }
