3670d1ef3acee78ad805d417d8b771722db93657
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 07:49:24 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,23 +1,33 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] str = br.readLine().split(" ");
-        String[] a = str[0].split("");
-        String[] b = str[1].split("");
-        
-        for (int i = 2; i >= 0; i--) {
-            if (Integer.parseInt(a[i]) > Integer.parseInt(b[i])) {
-                StringBuilder sb = new StringBuilder(str[0]);
-                System.out.println(sb.reverse().toString());
-                return;
-            } else if (Integer.parseInt(a[i]) < Integer.parseInt(b[i])) {
-                StringBuilder sb = new StringBuilder(str[1]);
-                System.out.println(sb.reverse().toString());
-                return;
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		int rA = reverse(A);
+		int rB = reverse(B);
+
+		bw.write(String.valueOf(Math.max(rA, rB)));
+		bw.flush();
+		bw.close();
+	}
+
+	public static int reverse(int num) {
+		char[] list = String.valueOf(num).toCharArray();
+
+		for (int i = 0; i < list.length / 2; i++) {
+			char temp = list[i];
+
+			list[i] = list[list.length - 1 - i];
+			list[list.length - 1 - i] = temp;
+		}
+
+		return Integer.parseInt(String.valueOf(list));
+	}
 }
