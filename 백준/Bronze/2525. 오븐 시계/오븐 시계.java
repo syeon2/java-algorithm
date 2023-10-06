99dbf000c2510ee95f761e8bfada1caa04c5f88b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 07:54:54 +0900

    [Bronze III] Title: 오븐 시계, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,27 +1,25 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        int hour = Integer.parseInt(list[0]);
-        int minute = Integer.parseInt(list[1]);
-        
-        int addM = Integer.parseInt(br.readLine());
-        
-        int addedM = minute + addM;
-        
-        if (addedM >= 60) {
-            int hourToM = addedM / 60;
-            addedM = addedM % 60;
-            
-            hour += hourToM;
-            hour = hour % 24;
-            
-            System.out.println(hour + " " + addedM);
-        } else {
-            System.out.println(hour + " " + addedM);
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int H = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+
+		int m = Integer.parseInt(br.readLine());
+
+		int answerM = (M + m) % 60;
+		int answerH = (H + ((M + m) / 60)) % 24;
+
+		bw.write(String.valueOf(answerH));
+		bw.write(" ");
+		bw.write(String.valueOf(answerM));
+
+		bw.flush();
+		bw.close();
+	}
 }
