01f980124df113c40a4584073665b3e268c2f991
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 08:09:29 +0900

    [Bronze IV] Title: 주사위 세개, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,40 +1,43 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        
-        int[] cache = new int[7];
-        
-        int count = 0;
-        
-        for (int i = 0; i < 3; i++) {
-            int pos = Integer.parseInt(list[i]);
-            
-            cache[pos] += 1;
-            
-            count = Math.max(count, cache[pos]);
-        }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        for (int i = 6; i > 0; i--) {
-            if (cache[i] == count) {
-                if (count == 3) {
-                    int answer = 10000 + (1000 * i);
-                    
-                    System.out.println(answer);
-                    return;
-                } else if (count == 2) {
-                    int answer = 1000 + (100 * i);
-                    
-                    System.out.println(answer);
-                    return;
-                } else if (count == 1) {
-                    System.out.println(i * 100);
-                    return;
-                }
-            }
-        }
-    }
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] list = new int[7];
+
+		int count = 0;
+		for (int i = 0; i < 3; i++) {
+			int num = Integer.parseInt(st.nextToken());
+
+			list[num]++;
+
+			if (count < list[num]) count = list[num];
+		}
+
+		int idx = 0;
+		int big = 0;
+
+		for (int i = 1; i < 7; i++) {
+			if (list[i] == count) {
+				idx = i;
+			}
+
+			if (list[i] > 0) big = i;
+		}
+
+		if (count == 3) {
+			bw.write(String.valueOf(10000 + (idx * 1000)));
+		} else if (count == 2) {
+			bw.write(String.valueOf(1000 + (idx * 100)));
+		} else {
+			bw.write(String.valueOf(100 * big));
+		}
+
+		bw.flush();
+		bw.close();
+	}
 }
