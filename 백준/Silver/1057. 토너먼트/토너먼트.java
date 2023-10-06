2f0491704ed824cc12a803d8604e954bcf133218
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-06 09:37:28 +0900

    [Silver IV] Title: 토너먼트, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -1,36 +1,46 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        int a = Integer.parseInt(list[1]);
-        int b = Integer.parseInt(list[2]);
-        
-        int count = 1;
-        while (true) {
-            if (a % 2 == 0 && b % 2 == 1) {
-                if (a / 2 == b / 2 + 1) break;
-                else count++;
-            } else if (a % 2 == 1 && b % 2 == 0) {
-                if (a / 2 + 1 == b / 2) break;
-                else count++;
-            } else count++;
-            
-            if (a % 2 == 1) {
-                a = (a / 2) + 1;
-            } else {
-                a = a / 2;
-            }
-            
-            if (b % 2 == 1) {
-                b = (b / 2) + 1;
-            } else {
-                b = b / 2;
-            }
-        }
-        
-        System.out.println(count);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+
+		int na = A;
+		if (na % 2 == 1) na += 1;
+
+		int nb = B;
+		if (nb % 2 == 1) nb += 1;
+
+		if (na == nb) bw.write(String.valueOf(1));
+		else {
+			int count = 1;
+
+			while (true) {
+				if (A % 2 == 1) {
+					A += 1;
+					A /= 2;
+				} else A /= 2;
+
+				if (B % 2 == 1) {
+					B += 1;
+					B /= 2;
+				} else B /= 2;
+
+				if (A == B) break;
+
+				count++;
+			}
+
+			bw.write(String.valueOf(count));
+		}
+
+		bw.flush();
+		bw.close();
+	}
 }
