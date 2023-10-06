1e5a6d7859bb1b30c595944ea8e116181a80e019
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 06:31:39 +0900

    [Bronze II] Title: 문자열 반복, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -1,30 +1,31 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringBuilder sb = new StringBuilder();
-        
-        String str;
-        
-        while ((str = br.readLine()) != null) {
-            String[] sentence = str.split(" ");
-            
-            if (sentence.length == 1) continue;
-            
-            int num = Integer.parseInt(sentence[0]);
-            
-            String[] list = sentence[1].split("");
-            for (int i = 0; i < list.length; i++) {
-                for (int k = 0; k < num; k++) {
-                    sb.append(list[i]);
-                }
-            }
-            sb.append("\n");
-        }
-        
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int n = 0; n < N; n++) {
+			String[] list = br.readLine().split(" ");
+			int count = Integer.parseInt(list[0]);
+			String[] str = list[1].split("");
+
+			for (int i = 0; i < str.length; i++) {
+
+				String s = str[i];
+				for (int k = 0; k < count; k++) {
+					sb.append(s);
+				}
+			}
+
+			sb.append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
