a80209515dcb31550365b739e4716bdf89a63984
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-06 09:44:42 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 116 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,33 +1,36 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
 
-        int[] list = new int[9];
-        
-        int total = 0;
-        for (int i = 0; i < 9; i++) {
-            int num = Integer.parseInt(br.readLine());
-            list[i] = num;
-            total += num;
-        }
-        
-        Arrays.sort(list);
-        
-        for (int i = 0; i < 9; i++) {
-            for (int k = i + 1; k < 9; k++) {
-                if (total - list[i] - list[k] == 100) {
-                    for (int j = 0; j < 9; j++) {
-                        if (j == i || j == k) continue;
-                        
-                        System.out.println(list[j]);
-                    }
-                    
-                    return;
-                }
-            }
-        }
-    }
+		int total = 0;
+		int[] list = new int[9];
+		for (int i = 0; i < 9; i++) {
+			int num = Integer.parseInt(br.readLine());
+
+			list[i] = num;
+			total += num;
+		}
+
+		for (int i = 0; i < 9; i++) {
+			for (int k = i + 1; k < 9; k++) {
+				if (total - list[i] - list[k] == 100) {
+					for (int j = 0; j < 9; j++) {
+						if (j == i || j == k) continue;
+
+						sb.append(list[j]).append("\n");
+					}
+
+					bw.write(sb.toString());
+					bw.flush();
+					bw.close();
+
+					return;
+				}
+			}
+		}
+	}
 }
