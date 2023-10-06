322c949d4a2a055b9bb25f864c92e1179672df2d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 07:19:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,28 +1,31 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        int count = 0;
-        int answer = Integer.MAX_VALUE;
-        
-        while (5 * count <= N) {
-            int temp = N - (count * 5);
-            
-            if (temp % 3 == 0) {
-                answer = Math.min(answer, count + (temp / 3));
-            }
-            
-            count++;
-        }
-        
-        if (answer == Integer.MAX_VALUE) {
-            System.out.println(-1);
-        } else {
-            System.out.println(answer);   
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		if (N % 5 == 0) {
+			bw.write(String.valueOf(N / 5));
+		} else {
+			int three = 0;
+			int five = 0;
+
+			int count = 0;
+
+			while ((N - (5 * count)) / 3 > 0) {
+				if ((N - (5 * count)) % 3 == 0) {
+					three = (N - (5 * count)) / 3;
+					five = count++;
+				} else count++;
+			}
+			if (three == 0) bw.write(String.valueOf(-1));
+			else bw.write(String.valueOf(three + five));
+		}
+
+		bw.flush();
+		bw.close();
+	}
 }
