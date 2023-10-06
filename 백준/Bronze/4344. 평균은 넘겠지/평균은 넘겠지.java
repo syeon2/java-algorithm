4c3fd19817da36d09be4254d18f4093e03f0827b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 07:43:03 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 168 ms, Memory: 15548 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.java
@@ -1,31 +1,36 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 
 		for (int n = 0; n < N; n++) {
-			String[] sen = br.readLine().split(" ");
+			st = new StringTokenizer(br.readLine(), " ");
 
-			int num = Integer.parseInt(sen[0]);
+			int C = Integer.parseInt(st.nextToken());
 
+			int[] list = new int[C];
 			int total = 0;
-			for (int i = 1; i <= num; i++) {
-				total += Integer.parseInt(sen[i]);
+			for (int c = 0; c < C; c++) {
+				list[c] = Integer.parseInt(st.nextToken());
+				total += list[c];
 			}
 
-			double avg = (double) total / (double) num;
+			double avg = (double) total / (double) (100 * C) * 100;
 
 			int count = 0;
-			for (int i = 1; i <= num; i++) {
-				if (Integer.parseInt(sen[i]) > avg) {
-					count++;
-				}
+			for (int i = 0; i < list.length; i++) {
+				if (avg < list[i]) count++;
 			}
 
-			System.out.printf("%.3f", (double) count / (double) num * 100);
+			double answer = (double) count / (double) C * 100;
+
+			System.out.printf("%.3f", answer);
 			System.out.println("%");
 		}
 	}
