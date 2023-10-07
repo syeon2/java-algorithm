dec71711742c67cacd7e36455048c9876098c615
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-08 08:36:58 +0900

    [Silver III] Title: 1로 만들기, Time: 172 ms, Memory: 18240 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,41 +1,25 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	static int[] dp;
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
-		dp = new int[N + 1];
 
-		// bottom-up
-		// for (int i = 2; i <= N; i++) {
-		// 	dp[i] = dp[i - 1] + 1;
-		// 	if (i % 3 == 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
-		// 	if (i % 2 == 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
-		// }
+		int[] cache = new int[N + 1];
+		Arrays.fill(cache, Integer.MAX_VALUE);
+		cache[1] = 0;
 
-		int answer = topDown(N);
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
-	}
-
-	public static int topDown(int N) {
-		if (N <= 1) return dp[N];
-
-		if (dp[N] == 0) {
-			dp[N] = N;
-
-			if (N % 3 == 0) dp[N] = Math.min(dp[N], topDown(N / 3) + 1);
-			if (N % 2 == 0) dp[N] = Math.min(dp[N], topDown(N / 2) + 1);
-			dp[N] = Math.min(dp[N], topDown(N - 1) + 1);
+		for (int i = 1; i <= N; i++) {
+			if (i + 1 <= N) cache[i + 1] = Math.min(cache[i + 1], cache[i] + 1);
+			if (i * 2 <= N) cache[i * 2] = Math.min(cache[i * 2], cache[i] + 1);
+			if (i * 3 <= N) cache[i * 3] = Math.min(cache[i * 3], cache[i] + 1);
 		}
 
-		return dp[N];
+		bw.write(String.valueOf(cache[N]));
+		bw.flush();
+		bw.close();
 	}
 }
