b438defc97f93a1a793a5096a48fda7677828f16
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 16:00:04 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,40 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		int max = 1001;
-		boolean[] list = new boolean[max];
-
-		list[0] = list[1] = true;
-		for (int i = 2; i < (int) Math.sqrt(max); i++) {
-
-			if (list[i]) continue;
-
-			for (int k = i * i; k < max; k += i) {
-				list[k] = true;
-			}
-		}
-
-		int count = 0;
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int t = 0; t < T; t++) {
-			int N = Integer.parseInt(st.nextToken());
 
-			if (!list[N]) count++;
+		int answer = 0;
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+
+			int count = 0;
+			for (int k = 1; k <= num; k++) {
+				if (num % k == 0) count++;
+			}
+
+			if (count == 2) answer++;
 		}
 
-		bw.write(String.valueOf(count));
+		bw.write(String.valueOf(answer));
 		bw.flush();
 		bw.close();
-    }
+	}
 }
