c629eccbf3da5d778f1c7c8c7975fdab35cba359
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 16:03:55 +0900

    [Bronze V] Title: A+B - 2, Time: 128 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -1,8 +1,4 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
