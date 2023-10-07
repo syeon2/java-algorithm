b536dd24904fe68ed6c744170180c47330c39682
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-08 08:12:05 +0900

    [Bronze II] Title: 다이얼, Time: 128 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -3,32 +3,30 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		char[] list = br.readLine().toCharArray();
+		int ans = 0;
 
-		int total = 0;
 		for (int i = 0; i < list.length; i++) {
-			int c = list[i] - 'A';
+			int num = convert(list[i]);
 
-			if (c < 3) {
-				total += 3;
-			} else if (c < 6) {
-				total += 4;
-			} else if (c < 9) {
-				total += 5;
-			} else if (c < 12) {
-				total += 6;
-			} else if (c < 15) {
-				total += 7;
-			} else if (c < 19) {
-				total += 8;
-			} else if (c < 22) {
-				total += 9;
-			} else {
-				total += 10;
-			}
+			ans += num;
 		}
 
-		System.out.println(total);
+		bw.write(String.valueOf(ans));
+		bw.flush();
+		bw.close();
+	}
+
+	public static int convert(char c) {
+		if (c >= 'A' && c <= 'C') return 3;
+		else if (c >= 'D' && c <= 'F') return 4;
+		else if (c >= 'G' && c <= 'I') return 5;
+		else if (c >= 'J' && c <= 'L') return 6;
+		else if (c >= 'M' && c <= 'O') return 7;
+		else if (c >= 'P' && c <= 'S') return 8;
+		else if (c >= 'T' && c <= 'V') return 9;
+		else return 10;
 	}
 }
