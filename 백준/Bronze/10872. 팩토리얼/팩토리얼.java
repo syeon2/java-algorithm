c16a551799a572bb4447d4ab4feb0958137f9ab1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 15:55:55 +0900

    [Bronze V] Title: 팩토리얼, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/10872. 팩토리얼/팩토리얼.java
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -1,23 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        long answer = 1;
-
-		int N = Integer.parseInt(br.readLine());
-		for (int i = 1; i <= N; i++) {
-			answer *= i;
-		}
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int answer = 1;
+        
+        for (int i = 1; i <= N; i++) {
+            answer *= i;
+        }
+        
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
     }
 }
