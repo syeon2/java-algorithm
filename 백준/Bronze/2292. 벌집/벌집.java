75e91fde9781bb0e90145ef3fa9a8edafe185249
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-07 16:11:32 +0900

    [Bronze II] Title: 벌집, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,19 +1,24 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int count = 1;
-
-		int current = 1;
-
-		while (current < N) {
-			current = current + (count++ * 6);
-		}
-
-		System.out.println(count);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int count = 0;
+        int total = 1;
+        
+        while (true) {
+            if (total + (6 * count) < N) {
+                total += 6 * count;
+                count++;
+            } else break;
+        }
+        
+        bw.write(String.valueOf(count + 1));
+        bw.flush();
+        bw.close();
+    }
 }
