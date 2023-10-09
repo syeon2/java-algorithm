af72628a876bbc8a01bf5ddb7563c3f4158d42ca
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-09 21:43:02 +0900

    [Bronze III] Title: 세 수, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -2,18 +2,35 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        int a = Integer.parseInt(list[0]);
-        int b = Integer.parseInt(list[1]);
-        int c = Integer.parseInt(list[2]);
-        
-        int[] answer = {a, b, c};
-        
-        Arrays.sort(answer);
-        
-        System.out.println(answer[1]);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int C = Integer.parseInt(st.nextToken());
+
+		int[] list = new int[101];
+
+		list[A]++;
+		list[B]++;
+		list[C]++;
+
+		int answer = 0;
+		int count = 0;
+
+		for (int i = 0; i < list.length; i++) {
+			if (list[i] > 0) count += list[i];
+
+			if (count >= 2) {
+				answer = i;
+				break;
+			}
+		}
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
 }
