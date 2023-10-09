21b75ccb4ce96cc6490171263699b392f5b21485
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-09 21:14:42 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -1,19 +1,25 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        int A = Integer.parseInt(list[0]);
-        int B = Integer.parseInt(list[1]);
-        int V = Integer.parseInt(list[2]);
-        
-        V -= A;
-        
-        int count = V / (A - B);
-        if (V % (A - B) > 0) count++;
-        
-        System.out.println(count + 1);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+
+		int temp = V - A;
+		int answer = 1;
+
+		answer += temp / (A - B);
+
+		if (temp % (A - B) > 0) answer++;
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
 }
