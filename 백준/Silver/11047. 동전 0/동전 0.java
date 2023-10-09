7b1f47d5e0add9a9291f4b5d1f8876ab5dba41c6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-10 07:04:01 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,30 +1,32 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] str = br.readLine().split(" ");
-        int N = Integer.parseInt(str[0]);
-        int K = Integer.parseInt(str[1]);
-        
-        int[] list = new int[N];
-        
-        for (int i = 0; i < N; i++) {
-            list[i] = Integer.parseInt(br.readLine());
-        }
-        
-        int count = 0;
-        
-        for (int i = N - 1; i >= 0; i--) {
-            if (K / list[i] > 0) {
-                count += K / list[i];
-                K = K % list[i];
-            }
-            
-            if (K == 0) break;
-        }
-        
-        System.out.println(count);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+
+		int[] list = new int[N];
+		for (int i = 0; i < N; i++) {
+			list[i] = Integer.parseInt(br.readLine());
+		}
+
+		int answer = 0;
+		for (int i = list.length - 1; i >= 0; i--) {
+			if (K == 0) break;
+
+			if (K % list[i] == K) continue;
+
+			answer += K / list[i];
+			K = K % list[i];
+		}
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
 }
