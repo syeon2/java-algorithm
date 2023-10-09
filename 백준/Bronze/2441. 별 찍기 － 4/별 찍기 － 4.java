7b8b2f7104a085c931c54d63f59556a727963712
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-10 07:51:41 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 128 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -1,26 +1,29 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < N; i++) {
-            
-            for (int k = 0; k < i; k++) {
-                sb.append(" ");
-            }
-            
-            for (int k = 0; k < N - i; k++) {
-                sb.append("*");
-            }
-            
-            sb.append("\n");
-        }
-        
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N; i++) {
+
+			for (int k = 0; k < i; k++) {
+				sb.append(" ");
+			}
+
+			for (int k = 0; k < N - i; k++) {
+				sb.append("*");
+			}
+
+			sb.append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
