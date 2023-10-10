9b00322a48d151fd2f1f6ae70f0caadd21df9b1d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-11 07:12:38 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,33 +1,29 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-
-		bw.write(String.valueOf(gcd(Math.max(A, B), Math.min(A, B))));
-		bw.newLine();
-		bw.write(String.valueOf(lcm(Math.max(A, B), Math.min(A, B))));
-		bw.flush();
-		bw.close();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        int max = gcd(Math.min(A, B), Math.max(A, B));
+        int min = A * B / max;
+        
+        bw.write(String.valueOf(max));
+        bw.newLine();
+        bw.write(String.valueOf(min));
+        
+        bw.flush();
+        bw.close();
     }
-
-	public static int gcd(int a, int b) {
-		if (b == 0) return a;
-		else return gcd(b, a % b);
-	}
-
-	public static int lcm(int a, int b) {
-		int gcd = gcd(a, b);
-		return (a / gcd) * (b / gcd) * gcd;
-	}
+    
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        
+        return gcd(b, a % b);
+    }
 }
