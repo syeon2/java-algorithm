376dfd83f410df41b18f9fa64e376a6ee565485f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-13 05:40:23 +0900

    [Silver V] Title: 영화감독 숌, Time: 168 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,30 +1,54 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
 
-		int answer = 666;
-
-		if (N == 1)
-			System.out.println(answer);
+		if (N == 1) bw.write("666");
 		else {
-			for (int i = 2; i <= N; i++) {
+			int count = 1;
+			int total = 666;
 
-				int temp = answer + 1;
+			while (count != N) {
+				count += 1;
 
-				while (!String.valueOf(temp).contains("666")) {
-					temp++;
-				}
-
-				answer = temp;
+				total = nextNum(total);
 			}
-            
-            System.out.println(answer);
+
+			bw.write(String.valueOf(total));
 		}
+
+		bw.flush();
+		bw.close();
+	}
+
+	public static int nextNum(int num) {
+		int temp = num + 1;
+
+		while (true) {
+			int count = 0;
+			boolean flag = false;
+
+			int temp2 = temp;
+
+			while (temp2 != 0) {
+				if (temp2 % 10 == 6) {
+					count++;
+
+					if (count >= 3) flag = true;
+				}
+				else count = 0;
+
+				temp2 /= 10;
+			}
+
+			if (flag) break;
+			else temp++;
+		}
+
+		return temp;
 	}
 }
