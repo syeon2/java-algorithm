7aa29286162d25200e03325a4be63a5a66885e39
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-13 06:35:25 +0900

    [Silver IV] Title: 덱, Time: 204 ms, Memory: 18496 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,48 +1,50 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
 		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
 		Deque<Integer> queue = new LinkedList<>();
 
-		int T = Integer.parseInt(br.readLine());
-		for (int t = 0; t < T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			String cmd = st.nextToken();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
 
-			if (cmd.equals("push_front")) {
-				Integer n = Integer.parseInt(st.nextToken());
+			String command = st.nextToken();
 
-				queue.addFirst(n);
-			} else if (cmd.equals("push_back")) {
-				Integer n = Integer.parseInt(st.nextToken());
+			if (command.equals("push_front")) {
+				int num = Integer.parseInt(st.nextToken());
 
-				queue.addLast(n);
-			} else if (cmd.equals("front")) {
+				queue.addFirst(num);
+			} else if (command.equals("push_back")) {
+				int num = Integer.parseInt(st.nextToken());
+
+				queue.addLast(num);
+			} else if (command.equals("pop_front")) {
 				if (queue.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(queue.peekFirst()).append("\n");
-			} else if (cmd.equals("back")) {
+				else {
+					sb.append(queue.removeFirst()).append("\n");
+				}
+			} else if (command.equals("pop_back")) {
 				if (queue.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(queue.peekLast()).append("\n");
-			} else if (cmd.equals("pop_front")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(queue.removeFirst()).append("\n");
-			} else if (cmd.equals("pop_back")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(queue.removeLast()).append("\n");
-			} else if (cmd.equals("size")) {
+				else {
+					sb.append(queue.removeLast()).append("\n");
+				}
+			} else if (command.equals("size")) {
 				sb.append(queue.size()).append("\n");
-			} else if (cmd.equals("empty")) {
+			} else if (command.equals("front")) {
+				if (queue.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(queue.getFirst()).append("\n");
+			} else if (command.equals("back")) {
+				if (queue.isEmpty()) sb.append(-1).append("\n");
+				else sb.append(queue.getLast()).append("\n");
+			} else if (command.equals("empty")) {
 				if (queue.isEmpty()) sb.append(1).append("\n");
 				else sb.append(0).append("\n");
 			}
@@ -51,5 +53,5 @@ public class Main {
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
-    }
+	}
 }
