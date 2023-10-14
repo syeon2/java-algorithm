187ba149ff6d890e6f167fdb588248108b534175
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-14 13:56:19 +0900

    [Bronze I] Title: 최소공배수, Time: 144 ms, Memory: 14692 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,36 +1,31 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st;
 
-		for (int t = 0; t < T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
+		for (int n = 0; n < N; n++) {
+			st = new StringTokenizer(br.readLine(), " ");
+
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
 
-			bw.write(String.valueOf(lcm(A, B)));
-			bw.newLine();
+			sb.append(A * B / gcd(Math.min(A, B), Math.max(A, B))).append("\n");
 		}
 
+		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
-    }
-
-	public static int gcm(int a, int b) {
-		if (a % b == 0) return b;
-		else return gcm(b, a % b);
 	}
 
-	public static int lcm(int a, int b) {
-		return a * b / gcm(a, b);
+	public static int gcd(int a, int b) {
+		if (b == 0) return a;
+		else return gcd(b, a % b);
 	}
 }
