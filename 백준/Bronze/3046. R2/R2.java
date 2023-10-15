641d5962f0d5135d8d6eb4360c74cd94eddf79d6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-15 14:23:47 +0900

    [Bronze IV] Title: R2, Time: 128 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        
+        int r1 = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        
+        bw.write(String.valueOf(2 * s - r1));
+        bw.flush();
+        bw.close();
+    }
+}
