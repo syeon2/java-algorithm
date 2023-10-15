a6583600b476d6704223e9f5a3f3c9808b1fff44
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-15 13:09:41 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N / 4; i++) {
+			sb.append("long ");
+		}
+
+		sb.append("int");
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
