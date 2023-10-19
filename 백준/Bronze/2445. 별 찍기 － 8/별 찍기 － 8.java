0cee5fb49b2c69d8ddce45aa229cd75fff52007e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-10-19 17:56:04 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 140 ms, Memory: 14452 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,49 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= N; i++) {
+
+			for (int k = 1; k <= i; k++) {
+				sb.append("*");
+			}
+
+			for (int k = 1; k <= (N - i) * 2; k++) {
+				sb.append(" ");
+			}
+
+			for (int k = 1; k <= i; k++) {
+				sb.append("*");
+			}
+
+			sb.append("\n");
+		}
+
+		for (int i = N - 1; i >= 1; i--) {
+
+			for (int k = 1; k <= i; k++) {
+				sb.append("*");
+			}
+
+			for (int k = 1; k <= (N - i) * 2; k++) {
+				sb.append(" ");
+			}
+
+			for (int k = 1; k <= i; k++) {
+				sb.append("*");
+			}
+
+			sb.append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+}
