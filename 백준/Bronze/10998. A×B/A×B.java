22c9c603cfd0054f31d047429738ba7627c85b2a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-06 20:55:51 +0900

    [Bronze V] Title: A×B, Time: 128 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Bronze/10998. A×B/A×B.java
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -1,10 +1,16 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        System.out.println(Integer.parseInt(list[0]) * Integer.parseInt(list[1]));
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] list = br.readLine().split(" ");
+
+		int answer = Integer.parseInt(list[0]) * Integer.parseInt(list[1]);
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
 }
