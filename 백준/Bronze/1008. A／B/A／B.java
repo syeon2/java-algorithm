3cd899ee9452840a9b5160bf85784838d8b0fb73
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-06 20:59:11 +0900

    [Bronze V] Title: A/B, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,10 +1,16 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] str = br.readLine().split(" ");
-        System.out.println(Double.parseDouble(str[0]) / Double.parseDouble(str[1]));
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] list = br.readLine().split(" ");
+
+		double answer = Double.parseDouble(list[0]) / Double.parseDouble(list[1]);
+
+		bw.write(String.valueOf(answer));
+		bw.flush();
+		bw.close();
+	}
 }
