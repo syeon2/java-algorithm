e03625d91f6f23f39fad9620418788dfc50b8481
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-06 21:32:40 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13924 KB -BaekjoonHub

--- 백준/Bronze/10718. We love kriii/We love kriii.java
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -1,6 +1,14 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("강한친구 대한육군");
-        System.out.println("강한친구 대한육군");
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("강한친구 대한육군");
+        bw.newLine();
+        bw.write("강한친구 대한육군");
+        
+        bw.flush();
+        bw.close();
     }
 }
