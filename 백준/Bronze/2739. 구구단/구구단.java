f61f247e75f9b71e738c97cf8ea3d8ea2b7aa175
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-06 21:41:59 +0900

    [Bronze V] Title: 구구단, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2739. 구구단/구구단.java
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -1,19 +1,20 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        System.out.println(N + " * 1 = " + (N * 1));
-        System.out.println(N + " * 2 = " + (N * 2));
-        System.out.println(N + " * 3 = " + (N * 3));
-        System.out.println(N + " * 4 = " + (N * 4));
-        System.out.println(N + " * 5 = " + (N * 5));
-        System.out.println(N + " * 6 = " + (N * 6));
-        System.out.println(N + " * 7 = " + (N * 7));
-        System.out.println(N + " * 8 = " + (N * 8));
-        System.out.println(N + " * 9 = " + (N * 9));
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringBuilder sb = new StringBuilder();
+
+		int num = Integer.parseInt(br.readLine());
+
+		for (int i = 1; i < 10; i++) {
+			sb.append(num).append(" * ").append(i).append(" = ").append(num * i).append('\n');
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
