c9795177345e0732e3483fdcb349de852ba75943
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-07 21:53:10 +0900

    [Bronze V] Title: A+B - 3, Time: 132 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -1,18 +1,23 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        for (int n = 0; n < N; n++) {
-            String[] str = br.readLine().split(" ");
-            
-            int A = Integer.parseInt(str[0]);
-            int B = Integer.parseInt(str[1]);
-            
-            System.out.println(A + B);
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < N; i++) {
+			String[] list = br.readLine().split(" ");
+
+			int a = Integer.parseInt(list[0]);
+			int b = Integer.parseInt(list[1]);
+
+			bw.write(String.valueOf(a + b));
+			bw.newLine();
+		}
+
+		bw.flush();
+		bw.close();
+	}
 }
