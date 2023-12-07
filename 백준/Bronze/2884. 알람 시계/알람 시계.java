96a0e6183aeb6ace868cbc0fa1fe82759c8ce49f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-07 22:10:13 +0900

    [Bronze III] Title: 알람 시계, Time: 128 ms, Memory: 14296 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,21 +1,31 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        int H = Integer.parseInt(list[0]);
-        int M = Integer.parseInt(list[1]);
-        
-        if (M >= 45) {
-            System.out.println(H + " " + (M - 45));
-        } else {
-            if (H == 0) {
-                System.out.println(23 + " " + (60 - (45 - M)));
-            } else {
-                System.out.println((H - 1) + " " + (60 - (45 - M)));
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] list = br.readLine().split(" ");
+
+		int h = Integer.parseInt(list[0]);
+		int m = Integer.parseInt(list[1]);
+
+		if (m < 45) {
+			if (h == 0) {
+				bw.write("23 ");
+				bw.write(String.valueOf(60 + (m - 45)));
+			} else {
+				bw.write(String.valueOf(h - 1));
+				bw.write(" ");
+				bw.write(String.valueOf(60 + (m - 45)));
+			}
+		} else {
+			bw.write(String.valueOf(h));
+			bw.write(" ");
+			bw.write(String.valueOf(m - 45));
+		}
+
+		bw.flush();
+		bw.close();
+	}
 }
