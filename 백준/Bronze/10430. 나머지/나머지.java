ebfb08074e8ee88bb9279d30ab6f5bf419e5a384
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-07 21:22:05 +0900

    [Bronze V] Title: 나머지, Time: 124 ms, Memory: 14244 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,17 +1,28 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        String[] list = br.readLine().split(" ");
-        int A = Integer.parseInt(list[0]);
-        int B = Integer.parseInt(list[1]);
-        int C = Integer.parseInt(list[2]);
-        
-        System.out.println((A + B) % C);
-        System.out.println(((A % C) + (B % C)) % C);
-        System.out.println((A * B) % C);
-        System.out.println(((A % C) * (B % C)) % C);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] list = br.readLine().split(" ");
+
+		int A = Integer.parseInt(list[0]);
+		int B = Integer.parseInt(list[1]);
+		int C = Integer.parseInt(list[2]);
+
+		bw.write(String.valueOf((A + B) % C));
+		bw.newLine();
+
+		bw.write(String.valueOf(((A % C) + (B % C)) % C));
+		bw.newLine();
+
+		bw.write(String.valueOf((A * B) % C));
+		bw.newLine();
+
+		bw.write(String.valueOf(((A % C) * (B % C)) % C));
+
+		bw.flush();
+		bw.close();
+	}
 }
