31b3926f51900541ff15aae83d994d6cf30698e3
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-08 22:38:22 +0900

    [Bronze V] Title: X보다 작은 수, Time: 172 ms, Memory: 15820 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -3,16 +3,25 @@ import java.io.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] info = br.readLine().split(" ");
-		int N = Integer.parseInt(info[0]);
-		int X = Integer.parseInt(info[1]);
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		String[] list = br.readLine().split(" ");
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(list[i]);
 
-			if (X > num) System.out.print(num + " ");
+		int N = Integer.parseInt(list[0]);
+		int X = Integer.parseInt(list[1]);
+
+		String[] list2 = br.readLine().split(" ");
+
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(list2[i]);
+
+			if (num < X) {
+				bw.write(list2[i]);
+				bw.write(" ");
+			}
 		}
+
+		bw.flush();
+		bw.close();
 	}
 }
