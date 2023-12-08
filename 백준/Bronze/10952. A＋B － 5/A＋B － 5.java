98da91ea1ebf27055c72fda130b21f62b70ec5ab
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-08 22:48:04 +0900

    [Bronze V] Title: A+B - 5, Time: 132 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,20 +1,23 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        StringBuilder sb = new StringBuilder();
-        while (true) {
-            String[] list = br.readLine().split(" ");
-            int A = Integer.parseInt(list[0]);
-            int B = Integer.parseInt(list[1]);
-            
-            if (A == 0 && B == 0) break;
-            
-            sb.append(A + B).append("\n");
-        }
-        
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		while (true) {
+			String[] list = br.readLine().split(" ");
+
+			int A = Integer.parseInt(list[0]);
+			int B = Integer.parseInt(list[1]);
+
+			if (A == 0 && B == 0) break;
+
+			bw.write(String.valueOf(A + B));
+			bw.newLine();
+		}
+
+		bw.flush();
+		bw.close();
+	}
 }
