99c25c144b028e5cdeea58f40220964ec3ae3fdd
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-08 23:08:45 +0900

    [Bronze III] Title: 최댓값, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.java
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -1,22 +1,27 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int max = 0;
-        int idx = 0;
-        
-        for (int i = 1; i <= 9; i++) {
-            int num = Integer.parseInt(br.readLine());
-            
-            if (num > max) {
-                max = num;
-                idx = i;
-            }
-        }
-        
-        System.out.println(max);
-        System.out.println(idx);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int max = 0;
+		int index = -1;
+
+		for (int i = 1; i <= 9; i++) {
+			int num = Integer.parseInt(br.readLine());
+
+			if (num > max) {
+				max = num;
+				index = i;
+			}
+		}
+
+		bw.write(String.valueOf(max));
+		bw.write(" ");
+		bw.write(String.valueOf(index));
+
+		bw.flush();
+		bw.close();
+	}
 }
