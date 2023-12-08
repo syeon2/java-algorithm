32adc59d03aa638268f4c1081ded5ba90e567b02
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-08 23:05:05 +0900

    [Bronze V] Title: A+B - 8, Time: 132 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -1,28 +1,29 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        StringBuilder sb = new StringBuilder();
-        for (int i = 1; i <= N; i++) {
-            String[] list = br.readLine().split(" ");
-            int A = Integer.parseInt(list[0]);
-            int B = Integer.parseInt(list[1]);
-            
-            sb.append("Case #")
-                .append(i)
-                .append(": ")
-                .append(A)
-                .append(" + ")
-                .append(B)
-                .append(" = ")
-                .append(A + B)
-                .append("\n");
-        }
-        
-        System.out.println(sb.toString());
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < N; i++) {
+			String[] list = br.readLine().split(" ");
+
+			int A = Integer.parseInt(list[0]);
+			int B = Integer.parseInt(list[1]);
+
+			sb.append("Case #").append(i + 1)
+				.append(": ")
+				.append(A).append(" + ").append(B)
+				.append(" = ").append(A + B)
+				.append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
