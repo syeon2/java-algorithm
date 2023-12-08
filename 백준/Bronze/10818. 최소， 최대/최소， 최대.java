58279ba97a0bb1c3a70ba30a502108ee8757170e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-08 22:55:07 +0900

    [Bronze III] Title: 최소, 최대, Time: 496 ms, Memory: 104688 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,22 +1,28 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        
-        String[] list = br.readLine().split(" ");
-        for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(list[i]);
-            
-            if (max < num) max = num;
-            if (min > num) min = num;
-        }
-        
-        System.out.println(min + " " + max);
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		int min = 1000001;
+		int max = -1000001;
+
+		String[] list = br.readLine().split(" ");
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(list[i]);
+
+			if (num < min) min = num;
+			if (num > max) max = num;
+		}
+
+		bw.write(String.valueOf(min));
+		bw.write(" ");
+		bw.write(String.valueOf(max));
+
+		bw.flush();
+		bw.close();
+	}
 }
