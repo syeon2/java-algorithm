65f5c5730436d9953d7e5a7222e03025c898a556
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 21:42:46 +0900

    [Bronze III] Title: 최소, 최대, Time: 480 ms, Memory: 87784 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -7,15 +8,15 @@ public class Main {
 
 		int N = Integer.parseInt(br.readLine());
 
-		int min = 1000001;
 		int max = -1000001;
+		int min = 1000001;
 
-		String[] list = br.readLine().split(" ");
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(list[i]);
+			int num = Integer.parseInt(st.nextToken());
 
-			if (num < min) min = num;
-			if (num > max) max = num;
+			if (max < num) max = num;
+			if (min > num) min = num;
 		}
 
 		bw.write(String.valueOf(min));
