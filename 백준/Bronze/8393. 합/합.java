7c91769e0fdf209b9d61457ec7d186cc0d9b486e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 20:47:18 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -5,15 +5,15 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int n = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine());
 
-		int answer = 0;
+		int ans = 0;
 
-		for (int i = 1; i <= n; i++) {
-			answer += i;
+		for (int i = 1; i <= N; i++) {
+			ans += i;
 		}
 
-		bw.write(String.valueOf(answer));
+		bw.write(String.valueOf(ans));
 		bw.flush();
 		bw.close();
 	}
