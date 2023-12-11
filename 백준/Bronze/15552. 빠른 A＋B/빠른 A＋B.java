7ba173b994ff2dd1d54a4b60f432926b5a8c1b6c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 22:06:46 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 836 ms, Memory: 237472 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,25 +1,23 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 
 		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
 
 		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
+			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 			int A = Integer.parseInt(st.nextToken());
 			int B = Integer.parseInt(st.nextToken());
 
-			sb.append(A + B).append("\n");
+			bw.write(String.valueOf(A + B));
+			bw.newLine();
 		}
 
-		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
