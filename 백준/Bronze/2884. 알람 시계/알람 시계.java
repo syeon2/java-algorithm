0f1cc37cf377255f33c6218e2aeda77529f95f2c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 21:06:23 +0900

    [Bronze III] Title: 알람 시계, Time: 132 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -1,28 +1,29 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		String[] list = br.readLine().split(" ");
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int h = Integer.parseInt(list[0]);
-		int m = Integer.parseInt(list[1]);
+		int H = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
 
-		if (m < 45) {
-			if (h == 0) {
-				bw.write("23 ");
-				bw.write(String.valueOf(60 + (m - 45)));
-			} else {
-				bw.write(String.valueOf(h - 1));
-				bw.write(" ");
-				bw.write(String.valueOf(60 + (m - 45)));
-			}
-		} else {
-			bw.write(String.valueOf(h));
+		if (M >= 45) {
+			bw.write(String.valueOf(H));
 			bw.write(" ");
-			bw.write(String.valueOf(m - 45));
+			bw.write(String.valueOf(M - 45));
+		} else {
+			if (H == 0) {
+				bw.write("23 ");
+				bw.write(String.valueOf(60 + (M - 45)));
+			} else {
+				bw.write(String.valueOf(H - 1));
+				bw.write(" ");
+				bw.write(String.valueOf(60 + (M - 45)));
+			}
 		}
 
 		bw.flush();
