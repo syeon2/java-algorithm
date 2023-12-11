7f65aa5af1f7f3a71af1975a2d849d7baa4c6273
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 20:06:25 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -7,9 +7,10 @@ public class Main {
 
 		String[] list = br.readLine().split(" ");
 
-		int answer = Integer.parseInt(list[0]) - Integer.parseInt(list[1]);
+		int a = Integer.parseInt(list[0]);
+		int b = Integer.parseInt(list[1]);
 
-		bw.write(String.valueOf(answer));
+		bw.write(String.valueOf(a - b));
 		bw.flush();
 		bw.close();
 	}
