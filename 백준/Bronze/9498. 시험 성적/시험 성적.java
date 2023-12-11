874d23de1521eb1572ae8eea3d831bca69f4a8d4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 20:19:08 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.java
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -1,25 +1,19 @@
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int num = Integer.parseInt(br.readLine());
-        
-        if (num >= 90) {
-            bw.write("A");
-        } else if (num >= 80) {
-            bw.write("B");
-        } else if (num >= 70) {
-            bw.write("C");
-        } else if (num >= 60) {
-            bw.write("D");
-        } else {
-            bw.write("F");
-        }
-        
-        bw.flush();
-        bw.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		int N = Integer.parseInt(br.readLine());
+
+		if (N >= 90) bw.write("A");
+		else if (N >= 80) bw.write("B");
+		else if (N >= 70) bw.write("C");
+		else if (N >= 60) bw.write("D");
+		else bw.write("F");
+
+		bw.flush();
+		bw.close();
+	}
 }
