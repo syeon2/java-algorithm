6e8bcd74a026002285e06cdbfdd0af7c60c18798
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 20:02:21 +0900

    [Bronze V] Title: A+B, Time: 128 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,16 +1,17 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] list = br.readLine().split(" ");
-
-		int answer = Integer.parseInt(list[0]) + Integer.parseInt(list[1]);
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] list = br.readLine().split(" ");
+        
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+        
+        bw.write(String.valueOf(a + b));
+        bw.flush();
+        bw.close();
+    }
 }
