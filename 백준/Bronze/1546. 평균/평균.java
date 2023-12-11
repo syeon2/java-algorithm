11a9a28d90d6f4d637fa983ac118702df719c5d4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 22:19:19 +0900

    [Bronze I] Title: 평균, Time: 132 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,5 +1,5 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -7,20 +7,26 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 		int N = Integer.parseInt(br.readLine());
+
+		int[] list = new int[N];
+
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
 		int max = 0;
-		int total = 0;
 		for (int i = 0; i < N; i++) {
 			int num = Integer.parseInt(st.nextToken());
-			total += num;
+			list[i] = num;
 
 			if (max < num) max = num;
 		}
 
-		double answer = (double) total / (double) max * 100 / N;
+		double ans = 0;
 
-		bw.write(String.valueOf(answer));
+		for (int i = 0; i < N; i++) {
+			ans += (double) list[i] / (double) (max * 100);
+		}
+
+		bw.write(String.valueOf(ans / N * 10000));
 		bw.flush();
 		bw.close();
 	}
