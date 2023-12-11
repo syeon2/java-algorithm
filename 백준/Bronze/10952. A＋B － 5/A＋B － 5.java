3bcb8ae411e8fbe157168ea4a3b34b0d383c7e10
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-11 21:38:25 +0900

    [Bronze V] Title: A+B - 5, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.java
@@ -1,15 +1,18 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		while (true) {
-			String[] list = br.readLine().split(" ");
+		StringTokenizer st;
 
-			int A = Integer.parseInt(list[0]);
-			int B = Integer.parseInt(list[1]);
+		while (true) {
+			st = new StringTokenizer(br.readLine(), " ");
+
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
 
 			if (A == 0 && B == 0) break;
 
