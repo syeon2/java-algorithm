97fde08f9658e54eb07b7a86ea2cb5240b45eca2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2023-12-12 21:39:26 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 128 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -1,29 +1,30 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
 
 		char[] list = br.readLine().toCharArray();
-		int[] cache = new int[26];
 
-		Arrays.fill(cache, -1);
+		int[] ans = new int[26];
+		Arrays.fill(ans, -1);
+
 		for (int i = 0; i < list.length; i++) {
-			int index = list[i] - 'a';
+			int idx = list[i] - 'a';
 
-			if (cache[index] == -1) {
-				cache[index] = i;
+			if (ans[idx] == -1) ans[idx] = i;
+		}
+
+		for (int i = 0; i < 26; i++) {
+			if (ans[i] == -1) bw.write("-1 ");
+			else {
+				bw.write(String.valueOf(ans[i]));
+				bw.write(" ");
 			}
 		}
 
-		for (int i = 0; i < cache.length; i++) {
-			sb.append(cache[i]).append(" ");
-		}
-
-		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 	}
