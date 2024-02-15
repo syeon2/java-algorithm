4d9bc34fc5cbab67929f33c4374792b1fdedcdf2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-15 23:19:22 +0900

    [Bronze V] Title: A×B, Time: 120 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze V/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+        
+        bw.write(String.valueOf(a * b));
+        bw.flush();
+        bw.close();
+    }
+}
