ef6f6aea524ab17e78787a332df30e6608c54c24
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 19:30:49 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -2,30 +2,27 @@ import java.io.*;
 import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		char[] list = br.readLine().toCharArray();
-
-		int[] ans = new int[26];
-		Arrays.fill(ans, -1);
-
-		for (int i = 0; i < list.length; i++) {
-			int idx = list[i] - 'a';
-
-			if (ans[idx] == -1) ans[idx] = i;
-		}
-
-		for (int i = 0; i < 26; i++) {
-			if (ans[i] == -1) bw.write("-1 ");
-			else {
-				bw.write(String.valueOf(ans[i]));
-				bw.write(" ");
-			}
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        char[] list = br.readLine().toCharArray();
+        int[] alp = new int[26];
+        Arrays.fill(alp, -1);
+        
+        for (int i = 0; i < list.length; i++) {
+            int idx = list[i] - 'a';
+            
+            if (alp[idx] == -1) alp[idx] = i;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < alp.length; i++) {
+            sb.append(alp[i]).append(" ");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
