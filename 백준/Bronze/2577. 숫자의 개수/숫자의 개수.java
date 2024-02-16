3ccf8cf3d67e7e0dd2a048e4560abab7f43d32cd
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 19:35:33 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -1,29 +1,30 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
-		int C = Integer.parseInt(br.readLine());
-
-		String x = String.valueOf(A * B * C);
-		int[] list = new int[10];
-
-		for (int i = 0; i < x.length(); i++) {
-			int idx = x.charAt(i) - '0';
-
-			list[idx]++;
-		}
-
-		for (int i = 0; i < 10; i++) {
-			bw.write(String.valueOf(list[i]));
-			bw.newLine();
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+       
+        char[] list = String.valueOf(a * b * c).toCharArray();
+        
+        int[] ans = new int[10];
+        for (int i = 0; i < list.length; i++) {
+            int idx = list[i] - '0';
+            
+            ans[idx]++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ans.length; i++) {
+            sb.append(ans[i]).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
