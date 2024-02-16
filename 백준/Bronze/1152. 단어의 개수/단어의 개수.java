9083d8e20c515056a2d68cc5a0b654feb17db4df
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 19:26:21 +0900

    [Bronze II] Title: 단어의 개수, Time: 264 ms, Memory: 26332 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,15 +1,15 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] list = br.readLine().trim().split(" ");
-
-		if (list[0].isBlank()) bw.write("0");
-		else bw.write(String.valueOf(list.length));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] list = br.readLine().trim().split(" ");
+        
+        if (list[0].equals("")) bw.write("0");
+        else bw.write(String.valueOf(list.length));
+        bw.flush();
+        bw.close();
+    }
 }
