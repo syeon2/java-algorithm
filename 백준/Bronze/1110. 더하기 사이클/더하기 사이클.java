a7c323d1ae9d06ef5a45afaf8b4e31b83bc56457
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 19:13:17 +0900

    [Bronze I] Title: 더하기 사이클, Time: 132 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -1,50 +1,38 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb;
-
-		int N = Integer.parseInt(br.readLine());
-		int temp = N;
-
-		int count = 0;
-
-		do {
-			sb = new StringBuilder();
-			char[] list = String.valueOf(temp).toCharArray();
-			int sum = 0;
-
-			if (list.length == 1) {
-				sum = list[0] - '0';
-
-				sb.append(sum).append(sum);
-
-				temp = Integer.parseInt(sb.toString());
-				count++;
-			} else {
-				sum = (list[0] - '0') + (list[1] - '0');
-				
-				char[] list2 = String.valueOf(sum).toCharArray();
-
-				if (list2.length == 1) {
-					sb.append(list[1] - '0').append(list2[0] - '0');
-
-					temp = Integer.parseInt(sb.toString());
-					count++;
-				} else {
-					sb.append(list[1] - '0').append(list2[1] - '0');
-
-					temp = Integer.parseInt(sb.toString());
-					count++;
-				}
-
-			}
-		} while (temp != N);
-
-		bw.write(String.valueOf(count));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int M = N;
+        
+        int cnt = 0;
+        do {
+            String temp = String.valueOf(N);
+            if (temp.length() == 1) temp = "0".concat(temp);
+            
+            int sum = 0;
+            for (int i = 0; i < temp.length(); i++) {
+                sum += (temp.charAt(i) - '0');
+            }
+            
+            String strSum = String.valueOf(sum);
+            
+            StringBuilder sb = new StringBuilder();
+            sb.append(temp.charAt(temp.length() - 1))
+                .append(strSum.charAt(strSum.length() - 1));
+            
+            int newNum = Integer.parseInt(sb.toString());
+            
+            N = newNum;
+            cnt++;
+        } while (N != M);
+        
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
 }
