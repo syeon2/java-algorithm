6946ff9be8300d04769dfef6c6bd99528480fc66
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-16 19:17:54 +0900

    [Bronze II] Title: 나머지, Time: 132 ms, Memory: 14036 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,27 +1,25 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int[] list = new int[42];
-
-		for (int i = 0; i < 10; i++) {
-			int N = Integer.parseInt(br.readLine());
-
-			int idx = N % 42;
-
-			list[idx] = 1;
-		}
-
-		int ans = 0;
-		for (int i = 0; i < list.length; i++) {
-			if (list[i] == 1) ans++;
-		}
-
-		bw.write(String.valueOf(ans));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int[] list = new int[42];
+        
+        for (int i = 0; i < 10; i++) {
+            int num = Integer.parseInt(br.readLine());
+            
+            list[num % 42]++;
+        }
+        
+        int cnt = 0;
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] > 0) cnt++;
+        }
+        
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
 }
