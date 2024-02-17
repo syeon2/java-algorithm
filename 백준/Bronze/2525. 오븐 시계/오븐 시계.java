dd55315d8bb2eef796eb8f545492fb1fecf68884
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-17 17:58:24 +0900

    [Bronze III] Title: 오븐 시계, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,25 +1,31 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int H = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		int m = Integer.parseInt(br.readLine());
-
-		int answerM = (M + m) % 60;
-		int answerH = (H + ((M + m) / 60)) % 24;
-
-		bw.write(String.valueOf(answerH));
-		bw.write(" ");
-		bw.write(String.valueOf(answerM));
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int H = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int m = Integer.parseInt(br.readLine());
+        
+        // 분 처리
+        M += m;
+        
+        // 시간 더하기
+        H += M / 60;
+        M %= 60;
+        
+        H %= 24;
+        
+        bw.write(String.valueOf(H));
+        bw.write(" ");
+        bw.write(String.valueOf(M));
+        
+        bw.flush();
+        bw.close();
+    }
 }
