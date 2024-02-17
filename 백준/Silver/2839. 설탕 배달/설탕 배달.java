dcf2b7b3c990d0cac6d86cf69162bb3239cbe79a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-17 17:39:59 +0900

    [Silver IV] Title: 설탕 배달, Time: 128 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,31 +1,27 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		if (N % 5 == 0) {
-			bw.write(String.valueOf(N / 5));
-		} else {
-			int three = 0;
-			int five = 0;
-
-			int count = 0;
-
-			while ((N - (5 * count)) / 3 > 0) {
-				if ((N - (5 * count)) % 3 == 0) {
-					three = (N - (5 * count)) / 3;
-					five = count++;
-				} else count++;
-			}
-			if (three == 0) bw.write(String.valueOf(-1));
-			else bw.write(String.valueOf(three + five));
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int five = 0;
+        
+        int ans = -1;
+        while (true) {
+            if (five * 5 > N) break;
+            
+            int temp = N - (five * 5);
+            
+            if (temp % 3 == 0) ans = five + (temp / 3);
+            
+            five++;
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
