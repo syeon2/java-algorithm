552b06b4ba1b9d1d306e66f71dfd3877356e3a28
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-17 15:26:41 +0900

    [Bronze II] Title: 문자열 반복, Time: 124 ms, Memory: 14056 KB -BaekjoonHub

--- 백준/Bronze/2675. 문자열 반복/문자열 반복.java
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.java
@@ -2,31 +2,28 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		for (int i = 0; i < N; i++) {
-			StringBuilder sb = new StringBuilder();
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-			int count = Integer.parseInt(st.nextToken());
-			String[] word = st.nextToken().split("");
-
-			for (int k = 0; k < word.length; k++) {
-
-				for (int l = 0; l < count; l++) {
-					sb.append(word[k]);
-				}
-			}
-
-			bw.write(sb.toString());
-			bw.newLine();
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int K = Integer.parseInt(st.nextToken());
+            char[] list = st.nextToken().toCharArray();
+            
+            for (int i = 0; i < list.length; i++) {
+                for (int k = 0; k < K; k++) {
+                    bw.write(String.valueOf(list[i]));
+                }
+            }
+            
+            bw.newLine();
+        }
+        
+        bw.flush();
+        bw.close();
+    }
 }
