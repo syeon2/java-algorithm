a84d3cc6c5ffa39a4784d278e9e272ce0e7de3d3
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-17 17:51:56 +0900

    [Bronze I] Title: 단어 공부, Time: 284 ms, Memory: 22080 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,46 +1,41 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		char[] list = br.readLine().toCharArray();
-
-		int[] cache = new int[26];
-		int max = 0;
-
-		for (int i = 0; i < list.length; i++) {
-			if (list[i] >= 'A' && list[i] <= 'Z') {
-				int index = list[i] - 'A';
-
-				cache[index]++;
-
-				if (cache[index] > max) max = cache[index];
-			} else {
-				int index = list[i] - 'a';
-
-				cache[index]++;
-
-				if (cache[index] > max) max = cache[index];
-			}
-		}
-
-		int count = 0;
-		int idx = 0;
-		for (int i = 0; i < cache.length; i++) {
-			if (cache[i] == max) {
-				idx = i;
-				count++;
-			}
-
-			if (count > 1) break;
-		}
-
-		if (count > 1) bw.write(String.valueOf("?"));
-		else bw.write(String.valueOf((char) (idx + 'A')));
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        char[] list = br.readLine().toCharArray();
+        
+        int[] alp = new int[26];
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] >= 'a' && list[i] <= 'z') {
+                int idx = list[i] - 'a';
+                
+                alp[idx]++;
+            } else {
+                int idx = list[i] - 'A';
+                
+                alp[idx]++;
+            }
+        }
+        
+        int max = 0;
+        int idx = -1;
+        int cnt = 0;
+        
+        for (int i = 0; i < alp.length; i++) {
+            if (max < alp[i]) {
+                max = alp[i];
+                idx = i;
+                cnt = 1;
+            } else if (max == alp[i]) cnt++;
+        }
+        
+        if (cnt >= 2) bw.write("?");
+        else bw.write(String.valueOf((char) (idx + 'A')));
+        
+        bw.flush();
+        bw.close();
+    }
 }
