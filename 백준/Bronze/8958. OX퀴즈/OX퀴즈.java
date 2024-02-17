7e8406fef549471d26c2917ba7f8d16774a9bc7c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-17 15:29:57 +0900

    [Bronze II] Title: OX퀴즈, Time: 128 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
+++ 백준/Bronze/8958. OX퀴즈/OX퀴즈.java
@@ -1,32 +1,31 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		for (int i = 0; i < N; i++) {
-			String[] list = br.readLine().split("");
-
-			int score = 0;
-			int count = 1;
-			for (int k = 0; k < list.length; k++) {
-				if (list[k].equals("O")) {
-					score += count;
-
-					count++;
-				} else {
-					count = 1;
-				}
-			}
-
-			bw.write(String.valueOf(score));
-			bw.newLine();
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int n = 0; n < N; n++) {
+            char[] list = br.readLine().toCharArray();
+            
+            int score = 0;
+            int repeat = 0;
+            
+            for (int i = 0; i < list.length; i++) {
+                if (list[i] == 'X') repeat = 0;
+                else {
+                    repeat += 1;
+                    score += repeat;
+                }
+            }
+            
+            bw.write(String.valueOf(score));
+            bw.newLine();
+        }
+        
+        bw.flush();
+        bw.close();
+    }
 }
