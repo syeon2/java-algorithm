acd63dd757f3bfdd3219505af7500b915c7e216f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-18 16:17:14 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 132 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,40 +1,44 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int ans = 0;
-
-		for (int n = 0; n < N; n++) {
-			int[] cache = new int[26];
-			Arrays.fill(cache, -1);
-
-			char[] list = br.readLine().toCharArray();
-			boolean flag = true;
-
-			for (int i = 0; i < list.length; i++) {
-				int index = list[i] - 'a';
-
-				if (cache[index] == -1) cache[index] = i;
-				else {
-					if (cache[index] + 1 == i) cache[index]++;
-					else {
-						flag = false;
-						break;
-					}
-				}
-			}
-
-			if (flag) ans++;
-		}
-
-		bw.write(String.valueOf(ans));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int cnt = 0;
+        for (int n = 0; n < N; n++) {
+            String str = br.readLine();
+            
+            if (check(str)) cnt++;
+        }
+        
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
+    
+    public static boolean check(String str) {
+        boolean[] cList = new boolean[26];
+        char[] sList = str.toCharArray();
+        
+        char curC = sList[0];
+        cList[curC - 'a'] = true;
+        
+        for (int i = 1; i < sList.length; i++) {
+            if (sList[i] == curC) continue;
+            else {
+                int idx = sList[i] - 'a';
+                
+                if (cList[idx]) return false;
+                else {
+                    cList[idx] = true;
+                    curC = sList[i];
+                }
+            }
+        }
+        
+        return true;
+    }
 }
