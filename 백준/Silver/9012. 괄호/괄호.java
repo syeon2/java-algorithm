a594a0430599484eae3e1acddbfa4b4be39f6f8f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-18 16:05:00 +0900

    [Silver IV] Title: 괄호, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,35 +1,35 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		int N = Integer.parseInt(br.readLine());
-
-		for (int n = 0; n < N; n++) {
-			char[] list = br.readLine().toCharArray();
-
-			int ans = 0;
-			boolean flag = true;
-
-			for (int i = 0; i < list.length; i++) {
-				if (list[i] == '(') ans++;
-				else {
-					if (ans == 0) {
-						flag = false;
-						break;
-					} else ans--;
-				}
-			}
-
-			if (!flag || ans != 0) sb.append("NO").append("\n");
-			else sb.append("YES").append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int n = 0; n < N; n++) {
+            char[] list = br.readLine().toCharArray();
+            
+            int cnt = 0;
+            boolean flag = true;
+            
+            for (int i = 0; i < list.length; i++) {
+                if (list[i] == '(') cnt++;
+                else cnt--;
+                
+                if (cnt < 0) {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (cnt == 0 && flag) sb.append("YES\n");
+            else sb.append("NO\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
