0170e912ceb6828fbd9f4733433d4c3e1eeb62a9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-18 16:45:21 +0900

    [Bronze II] Title: 다이얼, Time: 128 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -1,32 +1,31 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		char[] list = br.readLine().toCharArray();
-		int ans = 0;
-
-		for (int i = 0; i < list.length; i++) {
-			int num = convert(list[i]);
-
-			ans += num;
-		}
-
-		bw.write(String.valueOf(ans));
-		bw.flush();
-		bw.close();
-	}
-
-	public static int convert(char c) {
-		if (c >= 'A' && c <= 'C') return 3;
-		else if (c >= 'D' && c <= 'F') return 4;
-		else if (c >= 'G' && c <= 'I') return 5;
-		else if (c >= 'J' && c <= 'L') return 6;
-		else if (c >= 'M' && c <= 'O') return 7;
-		else if (c >= 'P' && c <= 'S') return 8;
-		else if (c >= 'T' && c <= 'V') return 9;
-		else return 10;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        char[] list = br.readLine().toCharArray();
+        
+        int ans = 0;
+        
+        for (int i = 0; i < list.length; i++) {
+            ans += getSecond(list[i]);
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+    
+    public static int getSecond(char c) {
+        if (c <= 'C') return 3;
+        else if (c <= 'F') return 4;
+        else if (c <= 'I') return 5;
+        else if (c <= 'L') return 6;
+        else if (c <= 'O') return 7;
+        else if (c <= 'S') return 8;
+        else if (c <= 'V') return 9;
+        else return 10;
+    }
 }
