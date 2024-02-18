754984283c8826f39e55f8ef11108b89632e6800
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-18 15:55:46 +0900

    [Silver IV] Title: 한수, Time: 128 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -1,36 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int count = 0;
-		for (int i = 1; i <= N; i++) {
-
-			if (i < 100)
-				count++;
-			else {
-				char[] list = String.valueOf(i).toCharArray();
-
-				int gap = list[0] - list[1];
-
-				boolean flag = true;
-				for (int k = 1; k < list.length - 1; k++) {
-					if (list[k] - list[k + 1] != gap) {
-						flag = false;
-						break;
-					}
-				}
-
-				if (flag)
-					count++;
-			}
-		}
-
-		System.out.println(count);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        if (N < 100) bw.write(String.valueOf(N));
+        else {
+            int cnt = 99;
+            
+            for (int i = 100; i <= N; i++) {
+                if (check(i)) cnt++;
+            }
+            
+            bw.write(String.valueOf(cnt));
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+    
+    public static boolean check(int n) {
+        int sub = ((n % 100) / 10) - (n % 10);
+        
+        String temp = String.valueOf(n);
+        for (int i = 0; i < temp.length() - 1; i++) {
+            if (temp.charAt(i) - temp.charAt(i + 1) != sub) return false;
+        }
+        
+        return true;
+    }
 }
