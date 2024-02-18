ae7e92618931c1974b611963e9c6bdd493826142
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-18 15:26:41 +0900

    [Bronze II] Title: 소수 찾기, Time: 124 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -1,29 +1,31 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int answer = 0;
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-
-			int count = 0;
-			for (int k = 1; k <= num; k++) {
-				if (num % k == 0) count++;
-			}
-
-			if (count == 2) answer++;
-		}
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int cnt = 0;
+        for (int n = 0; n < N; n++) {
+            int num = Integer.parseInt(st.nextToken());
+            
+            boolean isPrime = true;
+            for (int i = 2; i < num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            
+            if (isPrime && num != 1) cnt++;
+        }
+        
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
 }
