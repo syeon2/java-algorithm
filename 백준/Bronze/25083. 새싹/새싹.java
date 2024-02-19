b8b48303c9bcc28410ca9f3d7a9db7782122c329
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 07:15:56 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Bronze/25083. 새싹/새싹.java
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -1,10 +1,19 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("         ,r\'\"7");
-        System.out.println("r`-_   ,\'  ,/");
-        System.out.println(" \\. \". L_r\'");
-        System.out.println("   `~\\/");
-        System.out.println("      |");
-        System.out.println("      |");
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7\n")
+            .append("r`-_   ,'  ,/\n")
+            .append(" \\. \". L_r'\n")
+            .append("   `~\\/\n")
+            .append("      |\n")
+            .append("      |\n");
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
