7530a0ef9eec9c4d774eebd0ec56d600fe5b0ef2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-19 09:16:27 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 816 ms, Memory: 100696 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,27 +1,33 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		int N = Integer.parseInt(br.readLine());
-
-		int[] list = new int[N];
-		for (int i = 0; i < N; i++) {
-			list[i] = Integer.parseInt(br.readLine());
-		}
-
-		Arrays.sort(list);
-
-		for (int i = 0; i < N; i++) {
-			sb.append(list[i]).append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] minusList = new int[1000001];
+        int[] plusList = new int[1000001];
+        
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            
+            if (num < 0) minusList[num * -1]++;
+            else plusList[num]++;
+        }
+       
+        for (int i = 1000000; i >= 1; i--) {
+            if (minusList[i] == 1) sb.append(i * -1).append("\n");
+        }
+        
+        for (int i = 0; i <= 1000000; i++) {
+            if (plusList[i] == 1) sb.append(i).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
