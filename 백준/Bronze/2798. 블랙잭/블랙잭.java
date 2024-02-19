9ff1d4be12a137c97d5adf24def18760c17d6627
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-19 09:04:11 +0900

    [Bronze II] Title: 블랙잭, Time: 124 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,32 +1,43 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] str = br.readLine().split(" ");
-		int N = Integer.parseInt(str[0]);
-		int M = Integer.parseInt(str[1]);
-
-		String[] strs = br.readLine().split(" ");
-		int[] list = new int[N];
-		for (int i = 0; i < N; i++) {
-			list[i] = Integer.parseInt(strs[i]);
-		}
-
-		int answer = 0;
-		for (int i = 0; i < N; i++) {
-			for (int k = i + 1; k < N; k++) {
-				for (int l = k + 1; l < N; l++) {
-					if (list[i] + list[k] + list[l] <= M) {
-						answer = Math.max(answer, list[i] + list[k] + list[l]);
-					}
-				}
-			}
-		}
-
-		System.out.println(answer);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        int[] list = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int min = Integer.MAX_VALUE;
+        int ans = 0;
+        
+        for (int i = 0; i < N; i++) {
+            
+            for (int k = i + 1; k < N; k++) {
+                
+                for (int j = k + 1; j < N; j++) {
+                    int sum = list[i] + list[k] + list[j];
+                    
+                    int absSub = Math.abs(M - sum);
+                    
+                    if (min > absSub && sum <= M) {
+                        min = absSub;
+                        ans = sum;
+                    }
+                }
+            }
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
