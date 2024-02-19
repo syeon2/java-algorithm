da61b526e0d8d77dd9cc697a0ef88005aa4b4c88
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-19 10:27:14 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 120 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -4,21 +4,18 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
         
         int N = Integer.parseInt(br.readLine());
         
+        StringBuilder sb = new StringBuilder();
+        
         for (int i = N; i >= 1; i--) {
-            
-            for (int k = 0; k < i; k++) {
-                sb.append("*");
-            }
-            
-            sb.append("\n");
+            sb.append("*".repeat(i)).append("\n");
         }
         
         bw.write(sb.toString());
         bw.flush();
         bw.close();
+            
     }
 }
