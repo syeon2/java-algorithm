3da1ed172d41da0cfb8b9e9a3735f0d318df9e61
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-19 09:40:29 +0900

    [Bronze V] Title: 꼬마 정민, Time: 120 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -1,17 +1,19 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        long A = Long.parseLong(st.nextToken());
-        long B = Long.parseLong(st.nextToken());
-        long C = Long.parseLong(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        bw.write(String.valueOf(A + B + C));
+        long ans = 0;
+        for (int i = 0; i < 3; i++) {
+            ans += Long.parseLong(st.nextToken());
+        }
+        
+        bw.write(String.valueOf(ans));
         bw.flush();
         bw.close();
     }
