60dfefa22a7399e236700822e250b67b3e527321
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 07:12:20 +0900

    [Bronze IV] Title: 영수증, Time: 124 ms, Memory: 14188 KB -BaekjoonHub

--- 백준/Bronze/25304. 영수증/영수증.java
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -1,31 +1,27 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int total = Integer.parseInt(br.readLine());
-
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-
-		for (int n = 0; n < N; n++) {
-			st = new StringTokenizer(br.readLine(), " ");
-
-			int price = Integer.parseInt(st.nextToken());
-			int count = Integer.parseInt(st.nextToken());
-
-			total -= (price * count);
-
-			if (total < 0) break;
-		}
-
-		if (total == 0) bw.write("Yes");
-		else bw.write("No");
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int total = Integer.parseInt(br.readLine());
+        
+        int N = Integer.parseInt(br.readLine());
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int cost = Integer.parseInt(st.nextToken());
+            int cnt = Integer.parseInt(st.nextToken());
+            
+            total -= (cost * cnt);
+        }
+        
+        if (total == 0) bw.write("Yes");
+        else bw.write("No");
+        
+        bw.flush();
+        bw.close();
+    }
 }
