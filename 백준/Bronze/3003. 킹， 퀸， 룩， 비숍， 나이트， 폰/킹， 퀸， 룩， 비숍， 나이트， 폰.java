a68d972b646ec8b6006303c51b65b3e996a09367
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 07:19:49 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -1,25 +1,20 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int k = 1 - Integer.parseInt(st.nextToken());
-		int q = 1 - Integer.parseInt(st.nextToken());
-		int l = 2 - Integer.parseInt(st.nextToken());
-		int b = 2 - Integer.parseInt(st.nextToken());
-		int kn = 2 - Integer.parseInt(st.nextToken());
-		int p = 8 - Integer.parseInt(st.nextToken());
-
-		StringBuilder sb = new StringBuilder();
-		sb.append(k).append(" ").append(q).append(" ").append(l).append(" ")
-			.append(b).append(" ").append(kn).append(" ").append(p);
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int[] basic = {1, 1, 2, 2, 2, 8};
+        for (int i = 0; i < 6; i++) {
+            sb.append(basic[i] - Integer.parseInt(st.nextToken())).append(" ");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
