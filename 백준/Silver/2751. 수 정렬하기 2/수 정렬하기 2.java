fa39e731781b929d170080eb03450ce86271a514
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-19 09:10:01 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1468 ms, Memory: 113984 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,28 +1,27 @@
 import java.io.*;
 import java.util.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = Integer.parseInt(br.readLine());
-        
-        int[] list = new int[N];
-        for (int i = 0; i < N; i++) {
-            list[i] = Integer.parseInt(br.readLine());
-        }
-        
-        Arrays.sort(list);
-        
-        StringBuilder sb = new StringBuilder();
-        
-        for (int i = 0; i < N; i++) {
-            sb.append(list[i]).append("\n");
-        }
-        
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-    }
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+
+		int N = Integer.parseInt(br.readLine());
+
+		int[] list = new int[N];
+		for (int i = 0; i < N; i++) {
+			list[i] = Integer.parseInt(br.readLine());
+		}
+
+		Arrays.sort(list);
+
+		for (int i = 0; i < N; i++) {
+			sb.append(list[i]).append("\n");
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
 }
