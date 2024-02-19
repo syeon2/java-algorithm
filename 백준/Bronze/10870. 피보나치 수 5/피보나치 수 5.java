42c07612a50f52238ab3c57d14e5c8778bad878e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 07:33:46 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 120 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,26 +1,29 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int prev = 0;
-		int next = 1;
-
-		int count = 0;
-
-		int N = Integer.parseInt(br.readLine());
-
-		while (count++ != N) {
-			int temp = prev;
-
-			prev = next;
-			next += temp;
-		}
-
-		bw.write(String.valueOf(prev));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        if (N == 0) {
+            bw.write("0");
+        } else {
+            int n0 = 0;
+            int n1 = 1;
+            
+            while (N-- > 1) {
+                int temp = n0;
+            
+                n0 = n1;
+                n1 = temp + n1;
+            }
+        
+            bw.write(String.valueOf(n1));
+        }
+        
+        bw.flush();
+        bw.close();
+    }
 }
