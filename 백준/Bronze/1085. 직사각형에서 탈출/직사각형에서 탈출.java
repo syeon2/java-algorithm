3f9ee14e92837b296f523456b7bccff8bbd9c7c7
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 08:07:49 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 124 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.java
@@ -1,23 +1,22 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int x = Integer.parseInt(st.nextToken());
-		int y = Integer.parseInt(st.nextToken());
-		int w = Integer.parseInt(st.nextToken());
-		int h = Integer.parseInt(st.nextToken());
-
-		int minX = Math.min(Math.abs(x), Math.abs(x - w));
-		int minY = Math.min(Math.abs(y), Math.abs(y - h));
-
-		int ans = Math.min(minX, minY);
-		bw.write(String.valueOf(ans));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        int minX = Math.min(Math.abs(x), Math.abs(a - x));
+        int minY = Math.min(Math.abs(y), Math.abs(b - y));
+        
+        bw.write(String.valueOf(Math.min(minX, minY)));
+        bw.flush();
+        bw.close();
+    }
 }
