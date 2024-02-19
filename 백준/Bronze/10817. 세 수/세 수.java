ec6c935f021379363f3eba2d20055cc5680328ed
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 07:47:46 +0900

    [Bronze III] Title: 세 수, Time: 128 ms, Memory: 14412 KB -BaekjoonHub

--- 백준/Bronze/10817. 세 수/세 수.java
+++ 백준/Bronze/10817. 세 수/세 수.java
@@ -1,36 +1,19 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-
-		int[] list = new int[101];
-
-		list[A]++;
-		list[B]++;
-		list[C]++;
-
-		int answer = 0;
-		int count = 0;
-
-		for (int i = 0; i < list.length; i++) {
-			if (list[i] > 0) count += list[i];
-
-			if (count >= 2) {
-				answer = i;
-				break;
-			}
-		}
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        Integer[] list = Arrays.stream(br.readLine().split(" "))
+            .map(Integer::parseInt)
+            .toArray(Integer[]::new);
+        
+        Arrays.sort(list);
+        
+        bw.write(String.valueOf(list[1]));
+        bw.flush();
+        bw.close();
+    }
 }
