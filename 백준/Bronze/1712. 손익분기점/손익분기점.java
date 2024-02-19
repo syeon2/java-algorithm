b6bb0bb55e11f83129ed2ad12f5a22f98c4bacae
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 07:40:20 +0900

    [Bronze II] Title: 손익분기점, Time: 120 ms, Memory: 14172 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.java
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -1,26 +1,26 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		long A = Long.parseLong(st.nextToken());
-		long B = Long.parseLong(st.nextToken());
-		long C = Long.parseLong(st.nextToken());
-
-		long answer = 1;
-
-		if (B >= C) {
-			answer = -1;
-		} else {
-			answer = A / (C - B) + 1;
-		}
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        if (B >= C) bw.write("-1");
+        else {
+            int cnt = A / (C - B);
+            
+            if (A % (C - B) >= 0) cnt++;
+            
+            bw.write(String.valueOf(cnt));
+        }
+        
+        bw.flush();
+        bw.close();
+    }
 }
