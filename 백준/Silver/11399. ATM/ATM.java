acc9ccfc67a48eb8c02a9a225e67a28213453cdc
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-19 10:19:45 +0900

    [Silver IV] Title: ATM, Time: 128 ms, Memory: 14408 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -2,28 +2,29 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-		int[] list = new int[N];
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < list.length; i++) {
-			list[i] = Integer.parseInt(st.nextToken());
-		}
-
-		Arrays.sort(list);
-
-		int ans = 0;
-
-		for (int i = 0; i < list.length; i++) {
-			ans += (list[i] * (N - i));
-		}
-
-		bw.write(String.valueOf(ans));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] list = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(list);
+        
+        int ans = list[0];
+        for (int i = 1; i < N; i++) {
+            list[i] = list[i - 1] + list[i];
+            
+            ans += list[i];
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
