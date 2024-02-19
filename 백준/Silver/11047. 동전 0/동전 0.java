360370d547c830585f5811169bada7e62a63023f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 07:08:01 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,32 +1,28 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-
-		int[] list = new int[N];
-		for (int i = 0; i < N; i++) {
-			list[i] = Integer.parseInt(br.readLine());
-		}
-
-		int answer = 0;
-		for (int i = list.length - 1; i >= 0; i--) {
-			if (K == 0) break;
-
-			if (K % list[i] == K) continue;
-
-			answer += K / list[i];
-			K = K % list[i];
-		}
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(br.readLine());
+        }
+        
+        int cnt = 0;
+        for (int i = N - 1; i >= 0; i--) {
+            int coin = list[i];
+            
+            cnt += K / coin;
+            K %= coin;
+        }
+        
+        System.out.print(cnt);
+    }
 }
