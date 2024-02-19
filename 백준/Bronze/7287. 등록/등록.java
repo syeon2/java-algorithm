4060693f4f2609c59641354844cab3fb5efcf8a1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-20 08:03:33 +0900

    [Bronze V] Title: 등록, Time: 120 ms, Memory: 13892 KB -BaekjoonHub

--- 백준/Bronze/7287. 등록/등록.java
+++ 백준/Bronze/7287. 등록/등록.java
@@ -1,6 +1,14 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("90");
-        System.out.println("waterkite94");
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("78");
+        bw.newLine();
+        bw.write("syeon94");
+        
+        bw.flush();
+        bw.close();
     }
 }
