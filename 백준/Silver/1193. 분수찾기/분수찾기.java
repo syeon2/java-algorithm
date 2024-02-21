ed22fa3819a605311751097fbad6c3a89cee9c0e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-21 13:14:08 +0900

    [Silver V] Title: 분수찾기, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -7,46 +7,16 @@ public class Main {
 
 		int X = Integer.parseInt(br.readLine());
 
-		int total = 0;
-		int count = 0;
-
-		while (total < X) {
-			count++;
-			total += count;
+		int cnt = 1;
+		while (X - cnt > 0) {
+			X -= cnt;
+			cnt++;
 		}
 
 		StringBuilder sb = new StringBuilder();
 
-		if (total == X) {
-			if (count % 2 == 0) {
-				sb.append(count).append("/").append(1);
-			} else {
-				sb.append(1).append("/").append(count);
-			}
-		} else {
-			if (count % 2 == 0) {
-				int bottom = 1;
-				while (total != X) {
-					total--;
-
-					count--;
-					bottom++;
-				}
-
-				sb.append(count).append("/").append(bottom);
-			} else {
-				int top = 1;
-
-				while (total != X) {
-					total--;
-
-					count--;
-					top++;
-				}
-
-				sb.append(top).append("/").append(count);
-			}
-		}
+		if (cnt % 2 == 1) sb.append(cnt - X + 1).append("/").append(X);
+		else sb.append(X).append("/").append(cnt - X + 1);
 
 		bw.write(sb.toString());
 		bw.flush();
