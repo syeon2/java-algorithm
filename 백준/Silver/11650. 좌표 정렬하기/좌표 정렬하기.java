c8ab52a2abd33f1bb158244cc4ecd4eff570985b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-21 12:48:16 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 704 ms, Memory: 48220 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.java
@@ -2,40 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int[][] list = new int[N][2];
-
-		StringTokenizer st;
-		for (int i = 0; i < list.length; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-
-			int x = Integer.parseInt(st.nextToken());
-			int y = Integer.parseInt(st.nextToken());
-
-			list[i][0] = x;
-			list[i][1] = y;
-		}
-
-		Arrays.sort(list, (a, b) -> {
-			if (a[0] == b[0]) {
-				return a[1] - b[1];
-			} else {
-				return a[0] - b[0];
-			}
-		});
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < list.length; i++) {
-			sb.append(list[i][0]).append(" ").append(list[i][1]).append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int[][] list = new int[N][2];
+        
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            
+            list[i][0] = x;
+            list[i][1] = y;
+        }
+        
+        Arrays.sort(list, new Comparator<>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                if (a[0] == b[0]) {
+                    return a[1] - b[1];
+                }
+                
+                return a[0] - b[0];
+            }
+        });
+        
+        for (int i = 0; i < N; i++) {
+            sb.append(list[i][0]).append(" ").append(list[i][1]).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
