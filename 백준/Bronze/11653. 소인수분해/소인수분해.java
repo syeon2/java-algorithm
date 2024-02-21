713216675771d1a58000043e83b606d806404e91
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-21 21:55:41 +0900

    [Bronze I] Title: 소인수분해, Time: 236 ms, Memory: 17752 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -1,27 +1,25 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
-
-		while (N != 1) {
-
-			for (int i = 2; i <= N; i++) {
-				if (N % i == 0) {
-					sb.append(i).append("\n");
-					N /= i;
-					break;
-				}
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        while (N != 0) {
+            boolean flag = true;
+            
+            for (int i = 2; i <= N; i++) {
+                if (N % i == 0) {
+                    System.out.println(i);
+                    
+                    N /= i;
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if (flag) break;
+        }
+    }
 }
