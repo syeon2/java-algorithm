805d0486b7699c7e2ffe6354b22fc81f69ae710b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-21 12:36:59 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 156 ms, Memory: 16268 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -1,29 +1,25 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
         
-        int max = gcd(Math.min(A, B), Math.max(A, B));
-        int min = A * B / max;
-        
-        bw.write(String.valueOf(max));
-        bw.newLine();
-        bw.write(String.valueOf(min));
-        
+        bw.write(String.valueOf(gcd(a, b)) + " " + String.valueOf(lcm(a, b)));
         bw.flush();
         bw.close();
     }
     
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+    
     public static int gcd(int a, int b) {
         if (b == 0) return a;
-        
         return gcd(b, a % b);
     }
 }
