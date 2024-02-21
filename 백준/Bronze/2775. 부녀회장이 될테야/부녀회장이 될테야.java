e188f7d97e6becbe55df6cee3c6afc7ac319e721
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-21 20:44:25 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 228 ms, Memory: 17584 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,39 +1,27 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		for (int n = 0; n < N; n++) {
-			int floor = Integer.parseInt(br.readLine());
-			int ho = Integer.parseInt(br.readLine());
-
-			int[][] list = new int[floor + 1][ho + 1];
-
-			for (int i = 0; i <= floor; i++) {
-
-				for (int k = 1; k <= ho; k++) {
-					if (i == 0) list[i][k] = k;
-					else {
-						int total = 0;
-
-						for (int j = 1; j <= k; j++) {
-							total += list[i - 1][j];
-						}
-
-						list[i][k] = total;
-					}
-				}
-			}
-
-			bw.write(String.valueOf(list[floor][ho]));
-			bw.newLine();
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        int[][] town = new int[15][15];
+        for (int i = 0; i < 15; i++) {
+            town[0][i] = i;
+        }
+        
+        for (int i = 1; i < 15; i++) {
+            for (int k = 1; k < 15; k++) {
+                town[i][k] = town[i - 1][k] + town[i][k - 1];
+            }
+        }
+        
+        for (int n = 0; n < N; n++) {
+            int floor = sc.nextInt();
+            int room = sc.nextInt();
+            
+            System.out.println(town[floor][room]);
+        }
+    }
 }
