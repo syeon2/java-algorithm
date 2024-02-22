664358564120d25b0924cd5ad2d13b0006476847
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 11:03:33 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 140 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -1,46 +1,43 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 1; i <= N; i++) {
-
-			for (int k = 1; k <= N - i; k++) {
-				sb.append(" ");
-			}
-
-			int repeat = i * 2 - 1;
-
-			for (int j = 0; j < repeat; j++) {
-				sb.append("*");
-			}
-
-			sb.append("\n");
-		}
-
-		for (int i = N - 1; i >= 1; i--) {
-
-			for (int k = 1; k <= N - i; k++) {
-				sb.append(" ");
-			}
-
-			int repeat = i * 2 - 1;
-
-			for (int j = 0; j < repeat; j++) {
-				sb.append("*");
-			}
-
-			sb.append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            
+            for (int k = 1; k <= N - i; k++) {
+                sb.append(" ");
+            }
+            
+            int cnt = (i * 2) - 1;    
+            for (int k = 0; k < cnt; k++) {
+                sb.append("*");
+            }
+            
+            sb.append("\n");
+        }
+        
+        for (int i = N - 1; i >= 1; i--) {
+            
+            for (int k = 1; k <= N - i; k++) {
+                sb.append(" ");
+            }
+            
+            int cnt = (i * 2) - 1;    
+            for (int k = 0; k < cnt; k++) {
+                sb.append("*");
+            }
+            
+            sb.append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
