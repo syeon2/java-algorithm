31b32c5d473cd81d3886fe0459ae53ae8ac746b0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 15:47:40 +0900

    [Silver IV] Title: 덱, Time: 204 ms, Memory: 19216 KB -BaekjoonHub

--- 백준/Silver/10866. 덱/덱.java
+++ 백준/Silver/10866. 덱/덱.java
@@ -1,57 +1,65 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-
-		Deque<Integer> queue = new LinkedList<>();
-
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-
-			String command = st.nextToken();
-
-			if (command.equals("push_front")) {
-				int num = Integer.parseInt(st.nextToken());
-
-				queue.addFirst(num);
-			} else if (command.equals("push_back")) {
-				int num = Integer.parseInt(st.nextToken());
-
-				queue.addLast(num);
-			} else if (command.equals("pop_front")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
-				else {
-					sb.append(queue.removeFirst()).append("\n");
-				}
-			} else if (command.equals("pop_back")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
-				else {
-					sb.append(queue.removeLast()).append("\n");
-				}
-			} else if (command.equals("size")) {
-				sb.append(queue.size()).append("\n");
-			} else if (command.equals("front")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(queue.getFirst()).append("\n");
-			} else if (command.equals("back")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
-				else sb.append(queue.getLast()).append("\n");
-			} else if (command.equals("empty")) {
-				if (queue.isEmpty()) sb.append(1).append("\n");
-				else sb.append(0).append("\n");
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] list = new int[10001];
+        int idx = -1;
+        
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            String cmd = st.nextToken();
+            
+            if (cmd.equals("push_front")) {
+                for (int i = idx; i >= 0; i--) {
+                    list[i + 1] = list[i];
+                }
+                
+                list[0] = Integer.parseInt(st.nextToken());
+                idx++;
+            } else if (cmd.equals("push_back")) {
+                list[++idx] = Integer.parseInt(st.nextToken());
+            } else if (cmd.equals("pop_front")) {
+                if (idx == -1) sb.append(-1).append("\n");
+                else {
+                    sb.append(list[0]).append("\n");
+                    
+                    for (int i = 0; i < idx; i++) {
+                        list[i] = list[i + 1];
+                    }
+                    
+                    list[idx--] = 0;
+                }
+            } else if (cmd.equals("pop_back")) {
+                if (idx == -1) sb.append(-1).append("\n");
+                else {
+                    sb.append(list[idx]).append("\n");
+                
+                    list[idx--] = 0;
+                }
+            } else if (cmd.equals("size")) {
+                sb.append(idx + 1).append("\n");
+            } else if (cmd.equals("empty")) {
+                if (idx == -1) sb.append(1).append("\n");
+                else sb.append(0).append("\n");
+            } else if (cmd.equals("front")) {
+                if (idx == -1) sb.append(-1).append("\n");
+                else sb.append(list[0]).append("\n");
+            } else {
+                if (idx == -1) sb.append(-1).append("\n");
+                else sb.append(list[idx]).append("\n");
+            }
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
