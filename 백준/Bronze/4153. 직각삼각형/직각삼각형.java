690aff9b9034d730298cf8a29e426b674c97932b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 11:22:48 +0900

    [Bronze III] Title: 직각삼각형, Time: 124 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,35 +1,24 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-
-		while (true) {
-			st = new StringTokenizer(br.readLine(), " ");
-
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-
-			if (a == 0 && b == 0 && c == 0) break;
-
-			int[] list = {a, b, c};
-
-			Arrays.sort(list);
-
-			if (list[2] * list[2] == (list[0] * list[0]) + (list[1] * list[1])) sb.append("right");
-			else sb.append("wrong");
-
-			sb.append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            Integer[] list = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+            Arrays.sort(list);
+            
+            if (list[0] == 0 && list[1] == 0 && list[2] == 0) break;
+            
+            if ((list[2] * list[2]) == (list[1] * list[1]) + (list[0] * list[0])) sb.append("right").append("\n");
+            else sb.append("wrong").append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
