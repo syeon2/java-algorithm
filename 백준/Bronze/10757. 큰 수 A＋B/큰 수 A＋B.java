dc6faf7e27eca2cdf7f552b642df6c0e004ad48e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 11:34:33 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 144 ms, Memory: 15100 KB -BaekjoonHub

--- 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -1,18 +1,49 @@
 import java.io.*;
-import java.util.*;
-import java.math.*;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
 
-		BigInteger A = new BigInteger(st.nextToken());
-		BigInteger B = new BigInteger(st.nextToken());
+		String[] list = br.readLine().split(" ");
+		String A = list[0];
+		String B = list[1];
 
-		BigInteger ans = A.add(B);
+		int aIdx = A.length() - 1;
+		int bIdx = B.length() - 1;
 
-		System.out.println(ans);
+		int up = 0;
+		while (aIdx >= 0 && bIdx >= 0) {
+			int a = A.charAt(aIdx--) - '0';
+			int b = B.charAt(bIdx--) - '0';
+
+			sb.insert(0, (a + b + up) % 10);
+
+			up = (a + b + up) / 10;
+		}
+
+		while (aIdx >= 0) {
+			int a = A.charAt(aIdx--) - '0';
+
+			sb.insert(0, (a + up) % 10);
+
+			up = (a + up) / 10;
+		}
+
+		while (bIdx >= 0) {
+			int b = B.charAt(bIdx--) - '0';
+
+			sb.insert(0, (b + up) % 10);
+
+			up = (b + up) / 10;
+		}
+
+		if (up > 0) sb.insert(0, up);
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
 	}
 }
