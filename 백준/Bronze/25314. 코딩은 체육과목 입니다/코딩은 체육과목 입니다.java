2e3351565e385ec2cbb2c66d54f7f651e814e149
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 11:12:17 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 124 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -1,22 +1,21 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
-
-		for (int i = 0; i < N / 4; i++) {
-			sb.append("long ");
-		}
-
-		sb.append("int");
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N / 4; i++) {
+            sb.append("long ");
+        }
+        
+        sb.append("int");
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
