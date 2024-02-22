26e5f716073269673727dbb6404827a344495951
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 16:03:03 +0900

    [Bronze I] Title: 2007년, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/1924. 2007년/2007년.java
+++ 백준/Bronze/1924. 2007년/2007년.java
@@ -1,33 +1,26 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
+
+	public static int[] month = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public static String[] dayOfWeek = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int M = Integer.parseInt(st.nextToken());
-		int D = Integer.parseInt(st.nextToken());
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int[] list = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
 
-		int total = 0;
-
-		for (int i = 1; i < M; i++) {
-			total += list[i];
+		int days = y;
+		for (int i = 1; i < x; i++) {
+			days += month[i];
 		}
 
-		total += D;
-
-		if (total % 7 == 1) bw.write("MON");
-		else if (total % 7 == 2) bw.write("TUE");
-		else if (total % 7 == 3) bw.write("WED");
-		else if (total % 7 == 4) bw.write("THU");
-		else if (total % 7 == 5) bw.write("FRI");
-		else if (total % 7 == 6) bw.write("SAT");
-		else bw.write("SUN");
-
+		bw.write(String.valueOf(dayOfWeek[days % 7]));
 		bw.flush();
 		bw.close();
 	}
