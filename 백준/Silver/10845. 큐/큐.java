a906d4316abf08c73226d586742e0013094323ee
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 10:13:41 +0900

    [Silver IV] Title: 큐, Time: 200 ms, Memory: 19608 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,44 +1,47 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
 		int N = Integer.parseInt(br.readLine());
 
-		Deque<Integer> queue = new LinkedList<>();
-		StringBuilder sb = new StringBuilder();
+		int[] list = new int[10001];
+		int idx = -1;
 
-		StringTokenizer st;
 		for (int n = 0; n < N; n++) {
-			st = new StringTokenizer(br.readLine(), " ");
+			st = new StringTokenizer(br.readLine());
 
-			String command = st.nextToken();
+			String cmd = st.nextToken();
 
-			if (command.equals("push")) {
-				int num = Integer.parseInt(st.nextToken());
-
-				queue.add(num);
-			} else if (command.equals("pop")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
+			if (cmd.equals("push")) {
+				list[++idx] = Integer.parseInt(st.nextToken());
+			} else if (cmd.equals("pop")) {
+				if (idx == -1) sb.append("-1\n");
 				else {
-					int num = queue.removeFirst();
+					sb.append(list[0]).append("\n");
 
-					sb.append(num).append("\n");
+					for (int i = 0; i <= idx; i++) {
+						list[i] = list[i + 1];
+					}
+
+					list[idx--] = 0;
 				}
-			} else if (command.equals("size")) {
-				sb.append(queue.size()).append("\n");
-			} else if (command.equals("empty")) {
-				if (queue.isEmpty()) sb.append(1).append("\n");
-                else sb.append(0).append("\n");
-			} else if (command.equals("front")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
-                else sb.append(queue.getFirst()).append("\n");
-			} else if (command.equals("back")) {
-				if (queue.isEmpty()) sb.append(-1).append("\n");
-                else sb.append(queue.getLast()).append("\n");
+			} else if (cmd.equals("size")) {
+				sb.append(idx + 1).append("\n");
+			} else if (cmd.equals("empty")) {
+				if (idx == -1) sb.append(1).append("\n");
+				else sb.append(0).append("\n");
+			} else if (cmd.equals("front")) {
+				if (idx == -1) sb.append(-1).append("\n");
+				else sb.append(list[0]).append("\n");
+			} else if (cmd.equals("back")) {
+				if (idx == -1) sb.append(-1).append("\n");
+				else sb.append(list[idx]).append("\n");
 			}
 		}
 
