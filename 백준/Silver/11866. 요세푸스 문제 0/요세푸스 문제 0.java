4a21fda9d3770688c4ab3513417a792a569700fa
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 18:10:34 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 168 ms, Memory: 23204 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,41 +1,39 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-
-		Queue<Integer> queue = new LinkedList<>();
-
-		for (int i = 1; i <= N; i++) {
-			queue.add(i);
-		}
-
-		int count = 1;
-		StringBuilder sb = new StringBuilder();
-		sb.append("<");
-
-		while (queue.size() != 1) {
-			if (count == K) {
-				int num = queue.remove();
-
-				sb.append(num).append(", ");
-				count = 1;
-			} else {
-				queue.add(queue.remove());
-				count++;
-			}
-		}
-
-		sb.append(queue.remove()).append(">");
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            queue.add(i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        
+        int cnt = 1;
+        while (queue.size() != 1) {
+            if (cnt == K) {
+                sb.append(queue.remove()).append(", ");
+                
+                cnt = 1;
+            } else {
+                queue.add(queue.remove());
+                cnt++;
+            }
+        }
+        
+        sb.append(queue.remove()).append(">");
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
