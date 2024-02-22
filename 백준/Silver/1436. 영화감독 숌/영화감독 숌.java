24bc6b3037feb22aaaee52b2e20cb02a83372ce2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 10:27:14 +0900

    [Silver V] Title: 영화감독 숌, Time: 164 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -1,54 +1,48 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		if (N == 1) bw.write("666");
-		else {
-			int count = 1;
-			int total = 666;
-
-			while (count != N) {
-				count += 1;
-
-				total = nextNum(total);
-			}
-
-			bw.write(String.valueOf(total));
-		}
-
-		bw.flush();
-		bw.close();
-	}
-
-	public static int nextNum(int num) {
-		int temp = num + 1;
-
-		while (true) {
-			int count = 0;
-			boolean flag = false;
-
-			int temp2 = temp;
-
-			while (temp2 != 0) {
-				if (temp2 % 10 == 6) {
-					count++;
-
-					if (count >= 3) flag = true;
-				}
-				else count = 0;
-
-				temp2 /= 10;
-			}
-
-			if (flag) break;
-			else temp++;
-		}
-
-		return temp;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int ans = 666;
+        for (int i = 1; i < N; i++) {
+            
+            int temp = ans + 1;
+            
+            while (!is666(temp)) {
+                temp++;
+            }
+            
+            ans = temp;
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+    
+    public static boolean is666(int num) {
+        boolean flag = true;
+        
+        int cnt = 0;
+        while (num > 0) {
+            int n = num % 10;
+            
+            if (n == 6) {
+                cnt++;
+                
+                if (cnt == 3) break;
+            } else {
+                cnt = 0;
+            }
+            
+            num /= 10;
+        }
+        
+        if (cnt == 3) return true;
+        else return false;
+    }
 }
