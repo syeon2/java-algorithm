6b36ce3c05d1fe60fec50416f6860a8f9f25134c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-22 11:49:49 +0900

    [Bronze III] Title: 약수 구하기, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,24 +1,31 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] list = br.readLine().split(" ");
-		int N = Integer.parseInt(list[0]);
-		int K = Integer.parseInt(list[1]);
-
-		for (int i = 1; i <= N; i++) {
-			if (N % i == 0) {
-				K--;
-			}
-
-			if (K == 0) {
-				System.out.println(i);
-				return;
-			}
-		}
-
-		System.out.println(0);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        int ans = 0;
+        int cnt = 0;
+        
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0) {
+                cnt++;
+            }
+            
+            if (cnt == b) {
+                ans = i;
+                break;
+            }
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
