6e869217189435dcb1a9b8125f79482ba78d1ff2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 15:56:18 +0900

    [Silver IV] Title: 문자열, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.java
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -1,29 +1,28 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		char[] aList = st.nextToken().toCharArray();
-		char[] bList = st.nextToken().toCharArray();
-
-		int min = 50;
-
-		for (int i = 0; i <= bList.length - aList.length; i++) {
-			int count = 0;
-
-			for (int k = i; k < i + aList.length; k++) {
-				if (bList[k] != aList[k - i]) count++;
-			}
-
-			if (min > count) min = count;
-		}
-
-		bw.write(String.valueOf(min));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] list = br.readLine().split(" ");
+        
+        String A = list[0];
+        String B = list[1];
+        
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i <= B.length() - A.length(); i++) {
+            
+            int cnt = 0;
+            for (int k = 0; k < A.length(); k++) {
+                if (A.charAt(k) != B.charAt(i + k)) cnt++;
+            }
+            
+            ans = Math.min(cnt, ans);
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
