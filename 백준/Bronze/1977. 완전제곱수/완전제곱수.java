f3609a937e3eafbe53997e1d049f5e7936589276
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 10:54:58 +0900

    [Bronze II] Title: 완전제곱수, Time: 136 ms, Memory: 14864 KB -BaekjoonHub

--- 백준/Bronze/1977. 완전제곱수/완전제곱수.java
+++ 백준/Bronze/1977. 완전제곱수/완전제곱수.java
@@ -1,35 +1,42 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int M = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-
-		int total = 0;
-		int min = -1;
-
-		int idx = 1;
-		while (true) {
-			int num = idx * idx;
-
-			if (num >= M && num <= N) {
-				total += num;
-
-				if (min == -1) min = num;
-			}
-
-			idx++;
-
-			if (idx * idx > N) break;
-		}
-
-		if (min == -1) {
-			System.out.println(-1);
-		} else {
-			System.out.println(total);
-			System.out.println(min);
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int M = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        
+        int sum = 0;
+        int min = -1;
+        
+        for (int i = M; i <= N; i++) {
+            int num = 1;
+            
+            while (true) {
+                int temp = num * num;
+                
+                if (temp < i) num++;
+                else if (temp == i) {
+                    sum += i;
+                    
+                    if (min == -1) min = i;
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+        
+        if (min == -1) bw.write("-1");
+        else {
+            bw.write(String.valueOf(sum));
+            bw.newLine();
+            bw.write(String.valueOf(min));
+        }
+        
+        bw.flush();
+        bw.close();
+    }
 }
