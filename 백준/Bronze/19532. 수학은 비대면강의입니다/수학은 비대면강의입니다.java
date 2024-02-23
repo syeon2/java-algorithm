bd140cd3b00ba16675b1c82a1520ac54e095321f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 11:23:12 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 196 ms, Memory: 18052 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -1,35 +1,30 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int d = Integer.parseInt(st.nextToken());
-		int e = Integer.parseInt(st.nextToken());
-		int f = Integer.parseInt(st.nextToken());
-
-		StringBuilder sb = new StringBuilder();
-		if (a == 0 || d == 0) {
-			int x = ((c * e) - (f * b)) / ((a * e) - (d * b));
-			int y = (c - (a * x)) / b;
-
-			sb.append(x).append(" ").append(y);
-		} else {
-			int y = ((c * d) - (f * a)) / ((b * d) - (e * a));
-			int x = ((c - (b * y)) / a);
-
-			sb.append(x).append(" ").append(y);
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int x = -999; x <= 999; x++) {
+            for (int y = -999; y <= 999; y++) {
+                if ((a * x) + (b * y) == c && (d * x) + (e * y) == f) {
+                    sb.append(x).append(" ").append(y);
+                }
+            }
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
