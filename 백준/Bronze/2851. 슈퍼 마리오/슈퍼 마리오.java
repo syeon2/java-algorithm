b268f2cae58315cd86b55760b958fca4ec538601
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 16:30:02 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -1,25 +1,26 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int total = 0;
-
-		for (int i = 0; i < 10; i++) {
-			int num = Integer.parseInt(br.readLine());
-
-			if (Math.abs(100 - total) > Math.abs(100 - (total + num))) {
-				total += num;
-			} else if (Math.abs(100 - total) == Math.abs(100 - (total + num))) {
-				total += num;
-				break;
-			} else break;
-		}
-
-		bw.write(String.valueOf(total));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int[] list = new int[11];
+        for (int i = 1; i <= 10; i++) {
+            list[i] = Integer.parseInt(br.readLine()) + list[i - 1];
+        }
+        
+        int ans = 0;
+        int sub = Integer.MAX_VALUE;
+        for (int i = 1; i <= 10; i++) {
+            if (Math.abs(list[i] - 100) <= sub) {
+                ans = list[i];
+                sub = Math.abs(list[i] - 100);
+            }
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
