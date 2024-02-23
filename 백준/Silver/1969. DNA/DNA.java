81e31e374ed1a21feb75b777f692924e430ed425
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 18:08:12 +0900

    [Silver IV] Title: DNA, Time: 144 ms, Memory: 15408 KB -BaekjoonHub

--- 백준/Silver/1969. DNA/DNA.java
+++ 백준/Silver/1969. DNA/DNA.java
@@ -1,57 +1,57 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		StringBuilder sb = new StringBuilder();
 
+		StringTokenizer st = new StringTokenizer(br.readLine());
 		int N = Integer.parseInt(st.nextToken());
 		int M = Integer.parseInt(st.nextToken());
 
 		String[] list = new String[N];
-		for (int i = 0; i < list.length; i++) {
+		for (int i = 0; i < N; i++) {
 			list[i] = br.readLine();
 		}
 
+		StringBuilder sb = new StringBuilder();
 		for (int i = 0; i < M; i++) {
-			int[] cache = new int[26];
+
+			int[] alp = new int[26];
 
 			for (int k = 0; k < N; k++) {
-				String str = list[k];
-				int index = str.charAt(i) - 'A';
+				char c = list[k].charAt(i);
 
-				cache[index]++;
+				alp[c - 'A']++;
 			}
 
-			int count = 0;
-			char c = 0;
+			int max = alp[0];
+			char s = 'A';
 
-			for (int k = 0; k < cache.length; k++) {
-				if (count < cache[k]) {
-					count = cache[k];
-					c = (char) (k + 'A');
+			for (int k = 1; k <= 25; k++) {
+				if (max < alp[k]) {
+					max = alp[k];
+					s = (char) (k + 'A');
 				}
 			}
 
-			sb.append(String.valueOf(c));
+			sb.append(s);
 		}
+        
+        String ansStr = sb.toString();
+        
+        int ansMin = 0;
+        for (int i = 0; i < M; i++) {
+            
+            for (int k = 0; k < N; k++) {
+                if (ansStr.charAt(i) != list[k].charAt(i)) ansMin++;
+            }
+        }
 
-		String answerStr = sb.toString();
-		int answerCount = 0;
-
-		for (int i = 0; i < N; i++) {
-
-			for (int k = 0; k < M; k++) {
-				if (answerStr.charAt(k) != list[i].charAt(k)) answerCount++;
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.newLine();
-		bw.write(String.valueOf(answerCount));
+		bw.write(ansStr);
+        bw.newLine();
+        bw.write(String.valueOf(ansMin));
 		bw.flush();
 		bw.close();
 	}
