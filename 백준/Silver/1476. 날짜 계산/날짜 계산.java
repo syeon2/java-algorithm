5c3ecdf6443232171eba44e2cd42a4579930bf28
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 10:27:17 +0900

    [Silver V] Title: 날짜 계산, Time: 120 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1476. 날짜 계산/날짜 계산.java
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.java
@@ -1,35 +1,41 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] list = br.readLine().split(" ");
-		int E = Integer.parseInt(list[0]);
-		int S = Integer.parseInt(list[1]);
-		int M = Integer.parseInt(list[2]);
-
-		int e = 1;
-		int s = 1;
-		int m = 1;
-
-		int cur = 1;
-
-		while (true) {
-			if (E == e && S == s && M == m) break;
-
-			if (e == 15) e = 1;
-			else e++;
-
-			if (s == 28) s = 1;
-			else s++;
-
-			if (m == 19) m = 1;
-			else m++;
-
-			cur++;
-		}
-
-		System.out.println(cur);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+        
+        int aCut = 15;
+        int bCut = 28;
+        int cCut = 19;
+        
+        
+        int aN = 1;
+        int bN = 1;
+        int cN = 1;
+        
+        int y = 1;
+        
+        while (!(aN == A && bN == B && cN == C)) {
+            y++;
+            aN++;
+            bN++;
+            cN++;
+            
+            if (aN > aCut) aN = 1;
+            if (bN > bCut) bN = 1;
+            if (cN > cCut) cN = 1;
+        }
+        
+        bw.write(String.valueOf(y));
+        bw.flush();
+        bw.close();
+    }
 }
