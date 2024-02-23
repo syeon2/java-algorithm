62964768001147bfc3454f6ab2d7d4922089b076
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 15:44:00 +0900

    [Silver IV] Title: 토너먼트, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/1057. 토너먼트/토너먼트.java
+++ 백준/Silver/1057. 토너먼트/토너먼트.java
@@ -1,46 +1,32 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-
-		int na = A;
-		if (na % 2 == 1) na += 1;
-
-		int nb = B;
-		if (nb % 2 == 1) nb += 1;
-
-		if (na == nb) bw.write(String.valueOf(1));
-		else {
-			int count = 1;
-
-			while (true) {
-				if (A % 2 == 1) {
-					A += 1;
-					A /= 2;
-				} else A /= 2;
-
-				if (B % 2 == 1) {
-					B += 1;
-					B /= 2;
-				} else B /= 2;
-
-				if (A == B) break;
-
-				count++;
-			}
-
-			bw.write(String.valueOf(count));
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        int cnt = 1;
+        while (true) {
+            if (a % 2 == 1 && a + 1 == b) break;
+            else if (b % 2 == 1 && a == b + 1) break;
+            
+            if (a % 2 == 1) a = (a + 1) / 2;
+            else a /= 2;
+            
+            if (b % 2 == 1) b = (b + 1) / 2;
+            else b /= 2;
+            
+            cnt++;
+        }
+        
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
 }
