c065747ab7c0d5b6caf604b7fe5b59a9e8d916fc
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 09:58:12 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.java
@@ -1,33 +1,24 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String str = br.readLine();
-
-		if (str.equals(reverse(str))) bw.write(String.valueOf(1));
-		else bw.write(String.valueOf(0));
-
-		bw.flush();
-		bw.close();
-	}
-
-	public static String reverse(String str) {
-		Stack<Character> stack = new Stack<>();
-
-		for (int i = 0; i < str.length(); i++) {
-			stack.add(str.charAt(i));
-		}
-
-		StringBuilder sb = new StringBuilder();
-
-		while (!stack.isEmpty()) {
-			sb.append(stack.pop());
-		}
-
-		return sb.toString();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        
+        boolean flag = true;
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                flag = false;
+                break;
+            }
+        }
+        
+        if (flag) bw.write("1");
+        else bw.write("0");
+        
+        bw.flush();
+        bw.close();
+    }
 }
