d5957be5ed23dd34120af1d3329a958cdd888dcf
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 15:50:13 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -1,36 +1,46 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		int total = 0;
-		int[] list = new int[9];
-		for (int i = 0; i < 9; i++) {
-			int num = Integer.parseInt(br.readLine());
-
-			list[i] = num;
-			total += num;
-		}
-
-		for (int i = 0; i < 9; i++) {
-			for (int k = i + 1; k < 9; k++) {
-				if (total - list[i] - list[k] == 100) {
-					for (int j = 0; j < 9; j++) {
-						if (j == i || j == k) continue;
-
-						sb.append(list[j]).append("\n");
-					}
-
-					bw.write(sb.toString());
-					bw.flush();
-					bw.close();
-
-					return;
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int[] list = new int[9];
+        for (int i = 0; i < 9; i++) {
+            list[i] = Integer.parseInt(br.readLine());
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+        for (int i = 0; i < 9; i++) {
+            
+            for (int k = i + 1; k < 9; k++) {
+                
+                int sum = 0;
+                for (int j = 0; j < 9; j++) {
+                    if (i == j || j == k) continue;
+                    
+                    sum += list[j];
+                }
+                
+                if (sum == 100) {
+                    for (int j = 0; j < 9; j++) {
+                        if (i == j || k == j) continue;
+                        
+                        sb.append(list[j]).append("\n");
+                    }
+                    
+                    flag = true;
+                }
+                
+                if (flag) break;
+            }
+            
+            if (flag) break;
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
