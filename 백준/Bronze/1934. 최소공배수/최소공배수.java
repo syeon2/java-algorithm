354a21f2ee5b3081bb418f85dc18ed1557e3ffc1
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 10:06:57 +0900

    [Bronze I] Title: 최소공배수, Time: 148 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -1,31 +1,35 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-
-		StringBuilder sb = new StringBuilder();
-		for (int n = 0; n < N; n++) {
-			st = new StringTokenizer(br.readLine(), " ");
-
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-
-			sb.append(A * B / gcd(Math.min(A, B), Math.max(A, B))).append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
-
-	public static int gcd(int a, int b) {
-		if (b == 0) return a;
-		else return gcd(b, a % b);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            
+            sb.append(lcm(A, B)).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+    
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        
+        return gcd(b, a % b);
+    }
 }
