1841bba2fa2ae103bfb9cc0672c9e6996c1e3c96
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-23 10:46:37 +0900

    [Silver IV] Title: 수들의 합 2, Time: 248 ms, Memory: 15524 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.java
@@ -1,36 +1,32 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		String[] info = br.readLine().split(" ");
-		int N = Integer.parseInt(info[0]);
-		int M = Integer.parseInt(info[1]);
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int[] list = new int[N];
-		for (int i = 0; i < list.length; i++) {
-			list[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int count = 0;
-		for (int i = 0; i < N; i++) {
-			int sum = 0;
-
-			for (int k = i; k < N; k++) {
-				sum += list[k];
-
-				if (sum == M) {
-					count++;
-					break;
-				}
-
-				if (sum > M) break;
-			}
-		}
-
-		System.out.println(count);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        long[] list = new long[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            list[i] = Integer.parseInt(st.nextToken()) + list[i - 1];
+        }
+        
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            
+            for (int k = i; k <= N; k++) {
+                
+                if (list[k] - list[i - 1] == M) ans++;
+            }
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
