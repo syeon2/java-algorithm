c15ac545b8fbd910cb829016fe05a5d296b27e76
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-24 16:50:31 +0900

    [Silver IV] Title: 연속부분최대곱, Time: 172 ms, Memory: 18520 KB -BaekjoonHub

--- 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
+++ 백준/Silver/2670. 연속부분최대곱/연속부분최대곱.java
@@ -1,29 +1,29 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int N = Integer.parseInt(br.readLine());
-
-		double[] list = new double[N];
-
-		for (int i = 0; i < N; i++) {
-			list[i] = Double.parseDouble(br.readLine());
-		}
-
-		double max = 0.0;
-
-		for (int i = 0; i < N; i++) {
-			double temp = 1.0;
-
-			for (int k = i; k >= 0; k--) {
-				temp *= list[k];
-
-				if (max < temp) max = temp;
-			}
-		}
-
-		System.out.printf("%.3f", max);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        double[] list = new double[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = Double.parseDouble(br.readLine());
+        }
+        
+        double ans = 0;
+        
+        for (int i = 0; i < N - 1; i++) {
+            if (list[i] * list[i + 1] > list[i + 1]) list[i + 1] = list[i] * list[i + 1];
+        }
+        
+        for (int i = 0; i < N; i++) {
+            if (ans < list[i]) ans = list[i];
+        }
+        
+        bw.write(String.format("%.3f", ans));
+        bw.flush();
+        bw.close();
+    }
 }
