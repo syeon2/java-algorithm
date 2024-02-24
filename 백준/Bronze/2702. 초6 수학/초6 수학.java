244a9cca84e0437fb7a359a2deebe1d87aa7b9f0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-24 16:55:15 +0900

    [Bronze II] Title: 초6 수학, Time: 136 ms, Memory: 14540 KB -BaekjoonHub

--- 백준/Bronze/2702. 초6 수학/초6 수학.java
+++ 백준/Bronze/2702. 초6 수학/초6 수학.java
@@ -1,36 +1,35 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st;
-
-		for (int n = 0; n < N; n++) {
-			st = new StringTokenizer(br.readLine(), " ");
-
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-
-			int min = A;
-			while (true) {
-				if (A % min == 0 && B % min == 0) break;
-				else {
-					min--;
-				}
-			}
-
-			int max = A * B / min;
-
-			sb.append(max).append(" ").append(min).append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int n = 0; n < N; n++) {
+            st = new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            sb.append(lcm(a, b)).append(" ").append(gcd(a, b)).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    
+    public static int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+    
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
 }
