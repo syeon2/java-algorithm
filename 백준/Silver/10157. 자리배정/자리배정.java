9a6a019cadbc7b4cae1743a9e1179321215f42e2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-24 16:39:58 +0900

    [Silver IV] Title: 자리배정, Time: 156 ms, Memory: 15580 KB -BaekjoonHub

--- 백준/Silver/10157. 자리배정/자리배정.java
+++ 백준/Silver/10157. 자리배정/자리배정.java
@@ -1,73 +1,63 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int C = Integer.parseInt(st.nextToken());
-		int R = Integer.parseInt(st.nextToken());
-
-		int N = Integer.parseInt(br.readLine());
-
-		if (C * R < N) bw.write(String.valueOf(0));
-		else {
-			int[][] board = new int[R + 2][C + 2];
-
-			Arrays.fill(board[0], -1);
-			Arrays.fill(board[board.length - 1], -1);
-
-			for (int i = 0; i < board.length; i++) {
-				board[i][0] = -1;
-				board[i][board[i].length - 1] = -1;
-			}
-
-			// 상우하좌 = 0, 1, 2, 3
-			int[] xList = {0, 1, 0, -1};
-			int[] yList = {1, 0, -1, 0};
-
-			int pos = 0;
-
-			int count = 1;
-			int x = 1;
-			int y = 1;
-
-			board[1][1] = 1;
-			while (count != N) {
-				int nx = xList[pos] + x;
-				int ny = yList[pos] + y;
-
-				if (board[ny][nx] == 0) {
-					board[ny][nx] = ++count;
-
-					x = nx;
-					y = ny;
-				} else {
-					pos = turnPos(pos);
-
-					nx = xList[pos] + x;
-					ny = yList[pos] + y;
-
-					board[ny][nx] = ++count;
-
-					x = nx;
-					y = ny;
-				}
-			}
-
-			bw.write(String.valueOf(x));
-			bw.write(" ");
-			bw.write(String.valueOf(y));
-		}
-		
-		bw.flush();
-		bw.close();
-	}
-
-	public static int turnPos(int pos) {
-		if (pos == 3) return 0;
-		else return pos + 1;
-	}
+    
+    public static int[] dx = {0, 1, 0, -1};
+    public static int[] dy = {-1, 0, 1, 0};
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        boolean[][] isVisit = new boolean[R][C];
+        isVisit[R - 1][0] = true;
+        
+        int curX = 0;
+        int curY = R - 1;
+        
+        int curDirec = 0;
+        
+        if (C * R < N) bw.write("0");
+        else {
+            int cnt = 1;
+            
+            while (cnt != N) {
+                int nx = curX + dx[curDirec];
+                int ny = curY + dy[curDirec];
+                
+                if (isRange(nx, ny, C, R) && !isVisit[ny][nx]) {
+                    curX = nx;
+                    curY = ny;
+                    
+                    isVisit[ny][nx] = true;
+                    cnt++;
+                } else curDirec = turnDirec(curDirec);
+            }
+            
+            int ansX = curX + 1;
+            int ansY = R - curY;
+            
+            bw.write(String.format("%d %d", ansX, ansY));
+        }
+        
+        bw.flush();
+        bw.close();
+    }
+    
+    public static boolean isRange(int x, int y, int C, int R) {
+        if (x >= 0 && x < C && y >= 0 && y < R) return true;
+        
+        return false;
+    }
+    
+    public static int turnDirec(int direc) {
+        return (direc + 1) % 4;
+    }
 }
