ba17321eb650a31a1046c4297833b7adfe0c8c7f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-24 17:53:33 +0900

    [Bronze I] Title: 가장 큰 금민수, Time: 144 ms, Memory: 14796 KB -BaekjoonHub

--- 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.java
+++ 백준/Bronze/1526. 가장 큰 금민수/가장 큰 금민수.java
@@ -1,40 +1,30 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int answer = getNum(N);
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
-	}
-
-	public static int getNum(int num) {
-		int temp = num;
-
-		while (true) {
-
-			boolean flag = true;
-			int temp2 = temp;
-
-			while (temp2 != 0) {
-				if (!(temp2 % 10 == 4 || temp2 % 10 == 7)) {
-					flag = false;
-					break;
-				}
-
-				temp2 /= 10;
-			}
-
-			if (flag) break;
-			else temp--;
-		}
-
-		return temp;
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int max = 0;
+        
+        for (int i = 4; i <= N; i++) {
+            if (isGum(i)) max = i;
+        }
+        
+        bw.write(String.valueOf(max));
+        bw.flush();
+        bw.close();
+    }
+    
+    public static boolean isGum(int n) {
+        while (n > 0) {
+            if (!(n % 10 == 4 || n % 10 == 7)) return false;
+            
+            n /= 10;
+        }
+        
+        return true;
+    }
 }
