a7bff4026b3d2b9c75cb48144c8cc7cde0992112
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-25 12:26:07 +0900

    [Bronze II] Title: 주사위, Time: 120 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/1233. 주사위/주사위.java
+++ 백준/Bronze/1233. 주사위/주사위.java
@@ -1,40 +1,40 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int s1 = Integer.parseInt(st.nextToken());
-		int s2 = Integer.parseInt(st.nextToken());
-		int s3 = Integer.parseInt(st.nextToken());
-
-		int[] list = new int[s1 + s2 + s3 + 1];
-
-		for (int i = 1; i <= s1; i++) {
-			for (int k = 1; k <= s2; k++) {
-				for (int j = 1; j <= s3; j++) {
-					int index = i + k + j;
-
-					list[index]++;
-				}
-			}
-		}
-
-		int max = 0;
-		int index = 0;
-
-		for (int i = 0; i < list.length; i++) {
-			if (list[i] > max) {
-				max = list[i];
-				index = i;
-			}
-		}
-
-		bw.write(String.valueOf(index));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        
+        int[] cnt = new int[a + b + c + 1];
+        
+        for (int i = 1; i <= a; i++) {
+            for (int k = 1; k <= b; k++) {
+                for (int j = 1; j <= c; j++) {
+                    int sum = i + k + j;
+                    
+                    cnt[sum]++;
+                }
+            }
+        }
+        
+        int max = cnt[1];
+        int ans = 1;
+        
+        for (int i = 1; i < cnt.length; i++) {
+            if (max < cnt[i]) {
+                max = cnt[i];
+                ans = i;
+            }
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
