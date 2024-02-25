72394b8a77a1723fa4bb35002baa127642e3f4ad
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-25 11:37:02 +0900

    [Bronze II] Title: 공약수, Time: 1496 ms, Memory: 17800 KB -BaekjoonHub

--- 백준/Bronze/5618. 공약수/공약수.java
+++ 백준/Bronze/5618. 공약수/공약수.java
@@ -1,42 +1,36 @@
 import java.io.*;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int[] list = new int[N];
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int min = Integer.MAX_VALUE;
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			list[i] = num;
-
-			if (min > num) min = num;
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= min; i++) {
-
-			boolean flag = false;
-			for (int k = 0; k < N; k++) {
-				if (list[k] % i != 0) {
-					flag = true;
-					break;
-				}
-			}
-
-			if (!flag) sb.append(i).append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-
-
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int min = Integer.MAX_VALUE;
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+            
+            min = Math.min(min, list[i]);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 1; i <= min; i++) {
+            boolean flag = true;
+            
+            for (int k = 0; k < list.length; k++) {
+                if (list[k] % i > 0) flag = false;
+            }
+            
+            if (flag) sb.append(i).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
