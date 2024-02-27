81e90f973c0b11baca20df56e4414219a6418d53
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-02-27 11:54:48 +0900

    [Bronze III] Title: 신기한 수, Time: 308 ms, Memory: 14868 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/17618. 신기한 수/신기한 수.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            int temp = i;
+            
+            int sum = 0;
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+            
+            if (i % sum == 0) ans++;
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+}
