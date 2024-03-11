e3b74a62e69da228427e2e4f8b833e302c2a33be
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-11 14:51:57 +0900

    [Bronze III] Title: 공 넣기, Time: 124 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -2,37 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+        int[] buckets = new int[N + 1];
 
-		int[] bucket = new int[N + 1];
+        for (int m = 0; m < M; m++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
 
-		for (int m = 0; m < M; m++) {
-			st = new StringTokenizer(br.readLine(), " ");
+            for (int x1 = i; x1 <= j; x1++) {
+                buckets[x1] = k;
+            }
+        }
 
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(buckets[i]).append(" ");
+        }
 
-			int num = Integer.parseInt(st.nextToken());
-
-			for (int i = start; i <= end; i++) {
-				bucket[i] = num;
-			}
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= N; i++) {
-			sb.append(bucket[i]).append(" ");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
