591db8209120544eea0798143f2897f56b94b1c2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-11 14:43:45 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -2,25 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = Integer.parseInt(br.readLine());
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		int min = Integer.MAX_VALUE;
-		int max = Integer.MIN_VALUE;
+        int[] list = new int[N];
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
 
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
+        Arrays.sort(list);
 
-			if (num > max) max = num;
-			if (num < min) min = num;
-		}
-
-		bw.write(String.valueOf(max * min));
-		bw.flush();
-		bw.close();
-	}
+        bw.write(String.valueOf(list[0] * list[list.length - 1]));
+        bw.flush();
+        bw.close();
+    }
 }
