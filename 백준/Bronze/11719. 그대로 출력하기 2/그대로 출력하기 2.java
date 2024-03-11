412e9d9b94c6144975fe6ac716ac467c58bf7ed9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-11 15:07:05 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -4,14 +4,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         
         String str;
         
         while ((str = br.readLine()) != null) {
-            bw.write(str);
-            bw.newLine();
+            sb.append(str).append("\n");
         }
         
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
     }
