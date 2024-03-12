75cf1e4dfd5d11aa239c9a0b8c927fcd01747b28
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-12 23:54:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.04 ms, Memory: 73.1 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,27 +1,31 @@
-import java.util.*;
-
 class Solution {
     public String solution(String number, int k) {
-        int len = number.length() - k;
-        
-        char[] ans = new char[len];
-        
-        Stack<Character> stack = new Stack<>();
-        
-        for (char num : number.toCharArray()) {
-            
-            while (!stack.empty() && stack.peek() < num && k > 0) {
-                stack.pop();
-                k--;
+        String answer = "";
+        StringBuilder answerBuilder = new StringBuilder();
+
+
+        char[] array = number.toCharArray();
+
+        int length = array.length - k; //k개 제거한 길이
+
+        //문자 비교를 시작하는 인덱스를 나타내는 start 변수
+        int start = 0;
+
+        for(int i = 0; i < length; i++){
+            char max = '0';
+            for(int j = start; j <= i + k; j++){
+                //가장 큰수를 골라서 그 다음 인덱스를 시작 인덱스로 설정하기
+                if(array[j] > max){
+                    max = array[j];
+                    start= j + 1;
+                }
             }
-            
-            stack.add(num);
+            //가장 큰 문자를 String에 넣어주기
+            answerBuilder.append(max);
         }
-        
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = stack.get(i);
-        }
-        
-        return String.valueOf(ans);
+
+        //k개의 수를 제거할 때 얻을 수 있는 가장 큰 숫자를 구하려 한다
+        answer = answerBuilder.toString();
+        return answer;
     }
 }
