22de26350ea2bbb3225343c232faa724b228865c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-13 23:44:21 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.04 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,21 +1,28 @@
 class Solution {
-    public static int solution(int storey) {
-        int answer = 0;
-        while (storey > 0) {
-            int digit = storey % 10; //1의 자리
-            storey = storey / 10; //1의 자리를 제거한 수
-            if (digit == 5) { //1의 자리가 5와 같을 때
-                if (storey % 10 >= 5) { //10의 자리가 5보다 크면
-                    answer += 10 - digit; //올라가는게 효율적
-                    storey++;
-                } else //5보다 작으면
-                    answer += digit; //내려가는게 효율적
-            } else if (digit > 5) { //1의자리가 5보다 작으면
-                answer += 10 - digit; //내려가는게 효율적
-                storey++;
-            } else //1의 자리가 5보다 크면
-                answer += digit; //올라가는게 효율적
+    public int solution(int storey) {
+        return recur(storey, 0);
+    }
+    
+    public int recur(int storey, int cnt) {
+		    // 남은 충수가 10이하라면 단순 계산으로 마무리
+        if (storey < 10) {
+            int down = storey;
+            int up = (10 - storey) + 1;
+            
+            return Math.min(down, up) + cnt;
         }
-        return answer;
+        
+        // 현재 움직일 자리수가 0이라면 아래로 내려가지도 올라가지도 못하기 때문에 그대로 0만 제거
+        if (storey % 10 == 0) return recur(storey / 10, cnt);
+        else {
+        // 내려갔을 경우, 올라갔을 경우에서 움직인 모든 값들 중 min값을 찾기 위해 재귀
+            int downStep = storey % 10;
+            int upStep = (10 - (storey % 10));
+            
+            int down = recur(storey - downStep, cnt + downStep);
+            int up = recur(storey + upStep, cnt + upStep);
+            
+            return Math.min(down, up);
+        }
     }
 }
