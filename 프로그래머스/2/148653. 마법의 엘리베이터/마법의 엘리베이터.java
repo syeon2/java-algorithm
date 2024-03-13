59e0ea4e12979bd7ce397afd2d84436dfd88f88b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-13 22:27:00 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.03 ms, Memory: 71.7 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int solution(int storey) {
-        return recur(storey, 0);
-    }
-    
-    public int recur(int storey, int cnt) {
-        if (storey < 10) {
-            int down = storey;
-            int up = (10 - storey) + 1;
-            
-            return Math.min(down, up) + cnt;
+    public static int solution(int storey) {
+        int answer = 0;
+        while (storey > 0) {
+            int digit = storey % 10; //1의 자리
+            storey = storey / 10; //1의 자리를 제거한 수
+            if (digit == 5) { //1의 자리가 5와 같을 때
+                if (storey % 10 >= 5) { //10의 자리가 5보다 크면
+                    answer += 10 - digit; //올라가는게 효율적
+                    storey++;
+                } else //5보다 작으면
+                    answer += digit; //내려가는게 효율적
+            } else if (digit > 5) { //1의자리가 5보다 작으면
+                answer += 10 - digit; //내려가는게 효율적
+                storey++;
+            } else //1의 자리가 5보다 크면
+                answer += digit; //올라가는게 효율적
         }
-        
-        if (storey % 10 == 0) return recur(storey / 10, cnt);
-        else {
-            int downStep = storey % 10;
-            int upStep = (10 - (storey % 10));
-            
-            int down = recur(storey - downStep, cnt + downStep);
-            int up = recur(storey + upStep, cnt + upStep);
-            
-            return Math.min(down, up);
-        }
+        return answer;
     }
 }
