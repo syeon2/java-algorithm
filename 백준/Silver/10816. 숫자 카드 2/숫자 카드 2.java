1225798935a8d7ba25ae2ce044b412f956346777
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-28 10:14:08 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1504 ms, Memory: 125064 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,35 +1,78 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		Map<Integer, Integer> map = new HashMap<>();
-
-		int N = Integer.parseInt(br.readLine());
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-
-			if (map.containsKey(num)) map.put(num, map.get(num) + 1);
-			else map.put(num, 1);
-		}
-
-		int M = Integer.parseInt(br.readLine());
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < M; i++) {
-			int num = Integer.parseInt(st.nextToken());
-
-			if (map.containsKey(num)) sb.append(map.get(num)).append(" ");
-			else sb.append(0).append(" ");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] aList = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            aList[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        int M = Integer.parseInt(br.readLine());
+        int[] bList = new int[M];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            bList[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(aList);
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < M; i++) {
+            int start = lowerBound(bList[i], aList);
+            int end = upperBound(bList[i], aList);
+            
+            sb.append(end - start).append(" ");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    
+    public static int upperBound(int value, int[] list) {
+        int start = 0;
+        int end = list.length - 1;
+        
+        int minIdx = list.length;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (list[mid] > value) {
+                end = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return minIdx;
+    }
+    
+    public static int lowerBound(int value, int[] list) {
+        int start = 0;
+        int end = list.length - 1;
+        
+        int minIdx = list.length;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (list[mid] >= value) {
+                end = mid - 1;
+                minIdx = Math.min(minIdx, mid);
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return minIdx;
+    }
 }
