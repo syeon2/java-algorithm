56c8dfcdb5a15d59f58d9a09511999192a1dce69
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-03-28 09:37:32 +0900

    [Silver IV] Title: 수 찾기, Time: 736 ms, Memory: 49296 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,5 +1,5 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,28 +7,40 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int N = Integer.parseInt(br.readLine());
-        
-        Set<Integer> set = new HashSet<>();
-        
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
-        }
+        Integer[] aList = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
         
         int M = Integer.parseInt(br.readLine());
+        Integer[] bList = Arrays.stream(br.readLine().split(" ")).map(Integer::parseInt).toArray(Integer[]::new);
+        
+        Arrays.sort(aList);
         
         StringBuilder sb = new StringBuilder();
         
-        st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < M; i++) {
-            int num = Integer.parseInt(st.nextToken());
+            int num = bList[i];
             
-            if (set.contains(num)) sb.append(1).append("\n");
+            if (binarySearch(num, aList)) sb.append(1).append("\n");
             else sb.append(0).append("\n");
         }
         
         bw.write(sb.toString());
         bw.flush();
         bw.close();
+    }
+    
+    public static boolean binarySearch(int n, Integer[] list) {
+        int start = 0;
+        int end = list.length - 1;
+        
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            
+            if (list[mid] == n) return true;
+            
+            if (list[mid] < n) start = mid + 1;
+            else end = mid - 1;
+        }
+        
+        return false;
     }
 }
