b7768c6b143c627c6d0370138790f56484c9822a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 11:06:05 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,16 +1,16 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		if (N % 4 == 0 && (N % 100 > 0 || N % 400 == 0)) bw.write("1");
-		else bw.write("0");
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int y = Integer.parseInt(br.readLine());
+        
+        if (y % 4 == 0 && (y % 100 > 0 || y % 400 == 0)) bw.write("1");
+        else bw.write("0");
+        
+        bw.flush();
+        bw.close();
+    }
 }
