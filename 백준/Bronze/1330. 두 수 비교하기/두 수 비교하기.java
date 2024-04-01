d4f5bae8db531e21651d740f736a1bed0144de3b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 10:51:24 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 124 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -1,20 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] list = br.readLine().split(" ");
-
-		int A = Integer.parseInt(list[0]);
-		int B = Integer.parseInt(list[1]);
-
-		if (A > B) bw.write(">");
-		else if (A < B) bw.write("<");
-		else bw.write("==");
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        String ans;
+        if (A > B) ans = ">";
+        else if (A < B) ans = "<";
+        else ans = "==";
+        
+        bw.write(ans);
+        bw.flush();
+        bw.close();
+    }
 }
