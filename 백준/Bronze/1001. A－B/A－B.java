27323bda9b1ec77b5734467b1d0d43652a9bc05c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 10:33:22 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/1001. A－B/A－B.java
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -1,17 +1,18 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] list = br.readLine().split(" ");
-
-		int a = Integer.parseInt(list[0]);
-		int b = Integer.parseInt(list[1]);
-
-		bw.write(String.valueOf(a - b));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        bw.write(String.valueOf(A - B));
+        bw.flush();
+        bw.close();
+        
+    }
 }
