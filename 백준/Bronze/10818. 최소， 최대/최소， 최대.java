e44309b489de63c1a3c43299f7bb03c1dd880dad
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 11:37:26 +0900

    [Bronze III] Title: 최소, 최대, Time: 488 ms, Memory: 105172 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,29 +1,29 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int max = -1000001;
-		int min = 1000001;
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-
-			if (max < num) max = num;
-			if (min > num) min = num;
-		}
-
-		bw.write(String.valueOf(min));
-		bw.write(" ");
-		bw.write(String.valueOf(max));
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        String[] list = br.readLine().split(" ");
+        
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(list[i]);
+            
+            if (min > num) min = num;
+            if (max < num) max = num;
+        }
+        
+        bw.write(String.valueOf(min));
+        bw.write(" ");
+        bw.write(String.valueOf(max));
+        
+        bw.flush();
+        bw.close();
+    }
 }
