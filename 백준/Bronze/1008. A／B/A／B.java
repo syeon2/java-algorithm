b8466bfdf0ab5e59d896d933d5d8777fb27d444e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 10:37:59 +0900

    [Bronze V] Title: A/B, Time: 124 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.java
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -1,19 +1,17 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String[] list = br.readLine().split(" ");
-
-		int A = Integer.parseInt(list[0]);
-		int B = Integer.parseInt(list[1]);
-
-		double answer = (double) A / (double) B;
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        bw.write(String.valueOf((double) A / B));
+        bw.flush();
+        bw.close();
+    }
 }
