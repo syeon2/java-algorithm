d69cc48dc873f6cfa12c8e42467ecb6a191a3479
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 10:31:41 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/1000. A＋B/A＋B.java
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -1,16 +1,16 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         
-        String[] list = br.readLine().split(" ");
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
         
-        int a = Integer.parseInt(list[0]);
-        int b = Integer.parseInt(list[1]);
-        
-        bw.write(String.valueOf(a + b));
+        bw.write(String.valueOf(A + B));
         bw.flush();
         bw.close();
     }
