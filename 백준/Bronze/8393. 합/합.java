a3015b87d1a92bf5fba6d5d9fc3726fc2102508d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 11:16:25 +0900

    [Bronze V] Title: 합, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -1,20 +1,19 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int ans = 0;
-
-		for (int i = 1; i <= N; i++) {
-			ans += i;
-		}
-
-		bw.write(String.valueOf(ans));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+        
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+    }
 }
