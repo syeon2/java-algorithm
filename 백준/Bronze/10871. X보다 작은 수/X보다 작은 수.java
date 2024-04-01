a172add5cc067244752a6dcc359be5e3b7b2e1ef
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 11:30:18 +0900

    [Bronze V] Title: X보다 작은 수, Time: 164 ms, Memory: 15292 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -1,28 +1,26 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int N = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
-
-		st = new StringTokenizer(br.readLine(), " ");
-
-		for (int i = 0; i < N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-
-			if (num < X) {
-				bw.write(String.valueOf(num));
-				bw.write(" ");
-			}
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        for (int n = 0; n < N; n++) {
+            int num = Integer.parseInt(st.nextToken());
+            
+            if (num < X) sb.append(num).append(" ");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
