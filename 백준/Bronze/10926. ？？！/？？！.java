53b36d510d7555c05dac3d78dacd1f7992aa630d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 12:02:23 +0900

    [Bronze V] Title: ??!, Time: 124 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/10926. ？？！/？？！.java
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -1,17 +1,14 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String ans = br.readLine();
-
-		StringBuilder sb = new StringBuilder();
-		sb.append(ans).append("??!");
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine().concat("??!");
+        
+        bw.write(str);
+        bw.flush();
+        bw.close();
+    }
 }
