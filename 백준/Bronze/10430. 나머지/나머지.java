2ee79799960a864c4a2df4d1a881afd169b855bf
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 10:56:34 +0900

    [Bronze V] Title: 나머지, Time: 120 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/10430. 나머지/나머지.java
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -1,29 +1,27 @@
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int A = Integer.parseInt(st.nextToken());
-		int B = Integer.parseInt(st.nextToken());
-		int C = Integer.parseInt(st.nextToken());
-
-		bw.write(String.valueOf((A + B) % C));
-		bw.newLine();
-
-		bw.write(String.valueOf(((A % C) + (B % C)) % C));
-		bw.newLine();
-
-		bw.write(String.valueOf((A * B) % C));
-		bw.newLine();
-
-		bw.write(String.valueOf(((A % C) * (B % C)) % C));
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String[] list = br.readLine().split(" ");
+        int a = Integer.parseInt(list[0]);
+        int b = Integer.parseInt(list[1]);
+        int c = Integer.parseInt(list[2]);
+        
+        bw.write(String.valueOf((a + b) % c));
+        bw.newLine();
+        
+        bw.write(String.valueOf(((a % c) + (b % c)) % c));
+        bw.newLine();
+        
+        bw.write(String.valueOf((a * b) % c));
+        bw.newLine();
+        
+        bw.write(String.valueOf(((a % c) * (b % c)) % c));
+        
+        bw.flush();
+        bw.close();
+    }
 }
