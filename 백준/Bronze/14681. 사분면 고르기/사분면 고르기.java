cdace99249734c1a441b4a210837bf30ba638d7b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-01 11:27:12 +0900

    [Bronze V] Title: 사분면 고르기, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -1,19 +1,22 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int x = Integer.parseInt(br.readLine());
-		int y = Integer.parseInt(br.readLine());
-
-		if (x > 0 && y > 0) bw.write("1");
-		else if (x < 0 && y > 0) bw.write("2");
-		else if (x < 0 && y < 0) bw.write("3");
-		else bw.write("4");
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        
+        int ans = 0;
+        
+        if (x > 0 && y > 0) ans = 1;
+        else if (x < 0 && y > 0) ans = 2;
+        else if (x < 0 && y < 0) ans = 3;
+        else ans = 4;
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
