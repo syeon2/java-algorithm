c6c5f8b02916912817ed0aa0c1eab964e6e36e4e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-02 16:02:43 +0900

    [Silver IV] Title: 괄호, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -7,25 +7,25 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         
         int N = Integer.parseInt(br.readLine());
-        
         for (int n = 0; n < N; n++) {
             char[] list = br.readLine().toCharArray();
+            boolean isNormal = true;
             
-            int cnt = 0;
-            boolean flag = true;
-            
-            for (int i = 0; i < list.length; i++) {
-                if (list[i] == '(') cnt++;
-                else cnt--;
-                
-                if (cnt < 0) {
-                    flag = false;
-                    break;
+            int stack = 0;
+            for (char c : list) {
+                if (c == '(') stack++;
+                else {
+                    if (stack == 0) isNormal = false;
+                    else stack--;
                 }
             }
             
-            if (cnt == 0 && flag) sb.append("YES\n");
-            else sb.append("NO\n");
+            if (stack > 0) isNormal = false;
+            
+            if (isNormal) sb.append("YES");
+            else sb.append("NO");
+            
+            sb.append("\n");
         }
         
         bw.write(sb.toString());
