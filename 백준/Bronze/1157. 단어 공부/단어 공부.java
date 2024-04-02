c6e5d3668dad6903f22789066b1bbf54e7e1b2be
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-02 15:03:46 +0900

    [Bronze I] Title: 단어 공부, Time: 272 ms, Memory: 22076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -4,37 +4,39 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         char[] list = br.readLine().toCharArray();
-        
+
         int[] alp = new int[26];
+
         for (int i = 0; i < list.length; i++) {
-            if (list[i] >= 'a' && list[i] <= 'z') {
-                int idx = list[i] - 'a';
-                
+            if (list[i] >= 'A' && list[i] <= 'Z') {
+                int idx = list[i] - 'A';
                 alp[idx]++;
             } else {
-                int idx = list[i] - 'A';
-                
+                int idx = list[i] - 'a';
+
                 alp[idx]++;
             }
         }
-        
-        int max = 0;
-        int idx = -1;
+
+        int idx = 0;
+        int maxCnt = 0;
         int cnt = 0;
-        
-        for (int i = 0; i < alp.length; i++) {
-            if (max < alp[i]) {
-                max = alp[i];
+
+        for (int i = 0; i < 26; i++) {
+            if (maxCnt < alp[i]) {
                 idx = i;
+                maxCnt = alp[i];
                 cnt = 1;
-            } else if (max == alp[i]) cnt++;
+            } else if (maxCnt == alp[i]) {
+                cnt++;
+            }
         }
-        
-        if (cnt >= 2) bw.write("?");
-        else bw.write(String.valueOf((char) (idx + 'A')));
-        
+
+        if (cnt == 1) bw.write(String.valueOf((char) (idx + 'A')));
+        else bw.write("?");
+
         bw.flush();
         bw.close();
     }
