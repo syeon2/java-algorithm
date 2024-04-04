a868acaac1b2602e31d96af088c6b4cbfe608eac
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 21:39:37 +0900

    [Silver V] Title: 나이순 정렬, Time: 612 ms, Memory: 44976 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,45 +1,48 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         
-        int N = sc.nextInt();
+        int N = Integer.parseInt(br.readLine());
         
-        Person[] list = new Person[N];
-        for (int n = 0; n < N; n++) {
-            int age = sc.nextInt();
-            String name = sc.next();
-            int idx = n;
+        Obj[] list = new Obj[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
             
-            list[n] = new Person(age, name, idx);
+            int age = Integer.parseInt(st.nextToken());
+            String name = st.nextToken();
+            
+            list[i] = new Obj(age, name);
         }
         
-        Arrays.sort(list, new Comparator<Person>() {
+        Arrays.sort(list, new Comparator<>() {
             @Override
-            public int compare(Person a, Person b) {
-                if (a.age == b.age) {
-                    return a.idx - b.idx;
-                }
-                
+            public int compare(Obj a, Obj b) {
                 return a.age - b.age;
             }
         });
         
-        for (int i = 0; i < list.length; i++) {
-            System.out.printf("%d %s\n", list[i].age, list[i].name);
+        for (int i = 0; i < N; i++) {
+            sb.append(list[i].age).append(" ").append(list[i].name).append("\n");
         }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
     
-    public static class Person {
+    public static class Obj {
         public int age;
         public String name;
-        public int idx;
         
-        public Person(int age, String name, int idx) {
+        public Obj(int age, String name) {
             this.age = age;
             this.name = name;
-            this.idx = idx;
         }
     }
 }
