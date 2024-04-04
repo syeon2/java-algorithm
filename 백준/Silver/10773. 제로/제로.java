64a42a5ed9196ac59f9b48e8d6e90b9939f0ae52
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 19:17:07 +0900

    [Silver IV] Title: 제로, Time: 268 ms, Memory: 24116 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,25 +1,28 @@
-import java.util.Scanner;
-import java.util.Stack;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
         Stack<Integer> stack = new Stack<>();
-
-        for (int n = 0; n < N; n++) {
-            int num = sc.nextInt();
-
+        
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            
             if (num == 0) stack.pop();
             else stack.push(num);
         }
-
-        int sum = 0;
+        
+        int ans = 0;
         while (!stack.isEmpty()) {
-            sum += stack.pop();
+            ans += stack.pop();
         }
-
-        System.out.print(sum);
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
     }
 }
