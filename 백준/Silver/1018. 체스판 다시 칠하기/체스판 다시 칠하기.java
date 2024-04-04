b17906a0be23bcb3729af4044f32dd8eeb2ef3e6
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 22:08:56 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 136 ms, Memory: 14484 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -1,51 +1,49 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-		int M = sc.nextInt();
-
-		char[][] board = new char[N][M];
-
-		for (int i = 0; i < N; i++) {
-			String str = sc.next();
-
-			for (int k = 0; k < M; k++) {
-				board[i][k] = str.charAt(k);
-			}
-		}
-
-		int ans = Integer.MAX_VALUE;
-
-		for (int i = 0; i <= N - 8; i++) {
-
-			for (int k = 0; k <= M - 8; k++) {
-				int stone = board[i][k];
-
-				int cnt1 = 0;
-				int cnt2 = 0;
-
-				for (int y1 = i; y1 < i + 8; y1++) {
-					for (int x1 = k; x1 < k + 8; x1++) {
-
-						if (i % 2 == y1 % 2) {
-							if (k % 2 == x1 % 2 && board[y1][x1] != stone) cnt1++;
-							else if (k % 2 != x1 % 2 && board[y1][x1] == stone) cnt1++;
-							else cnt2++;
-						} else if (i % 2 != y1 % 2) {
-							if (k % 2 == x1 % 2 && board[y1][x1] == stone) cnt1++;
-							else if (k % 2 != x1 % 2 && board[y1][x1] != stone) cnt1++;
-							else cnt2++;
-						}
-					}
-				}
-
-				ans = Math.min(ans, Math.min(cnt1, cnt2));
-			}
-		}
-
-		System.out.print(ans);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        char[][] board = new char[N][M];
+        for (int n = 0; n < N; n++) {
+            String str = br.readLine();
+            
+            for (int m = 0; m < M; m++) {
+                board[n][m] = str.charAt(m);
+            }
+        }
+        
+        int ans = Integer.MAX_VALUE;
+        
+        for (int r = 0; r <= N - 8; r++) {
+            for (int c = 0; c <= M - 8; c++) {
+                
+                int cnt1 = 0;
+                int cnt2 = 0;
+                
+                for (int i = r; i < r + 8; i++) {
+                    for (int k = c; k < c + 8; k++) {
+                        if ((i + k) % 2 == 0 && board[r][c] != board[i][k]) cnt1++;
+                        else if ((i + k) % 2 == 1 && board[r][c] == board[i][k]) cnt1++;
+                        
+                        if ((i + k) % 2 == 1 && board[r][c] != board[i][k]) cnt2++;
+                        else if ((i + k) % 2 == 0 && board[r][c] == board[i][k]) cnt2++;
+                    }
+                }
+                
+                ans = Math.min(ans, Math.min(cnt1, cnt2));
+            }
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
