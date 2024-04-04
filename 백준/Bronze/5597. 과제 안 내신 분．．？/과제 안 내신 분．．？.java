2103f06c9a2ad879177bbc05567e8dec98a2e96c
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 09:57:43 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 120 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,18 +1,24 @@
-import java.util.Scanner;
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int[] list = new int[31];
         for (int i = 0; i < 28; i++) {
-            int idx = sc.nextInt();
+            int num = Integer.parseInt(br.readLine());
             
-            list[idx]++;
+            list[num] = 1;
         }
         
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= 30; i++) {
-            if (list[i] == 0) System.out.println(i);
+            if (list[i] == 0) sb.append(i).append("\n");
         }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
