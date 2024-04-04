afacb669177837d1faf3fdd3eb6931f70368eb31
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-04 22:12:21 +0900

    [Silver IV] Title: 카드2, Time: 200 ms, Memory: 45668 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,22 +1,27 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		int N = sc.nextInt();
-
-		Queue<Integer> queue = new LinkedList<>();
-		for (int i = 1; i <= N; i++) {
-			queue.add(i);
-		}
-
-		while (queue.size() != 1) {
-			queue.remove();
-
-			queue.add(queue.remove());
-		}
-
-		System.out.print(queue.remove());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        Queue<Integer> que = new LinkedList<>();
+        
+        for (int i = 1; i <= N; i++) {
+            que.add(i);
+        }
+        
+        while (que.size() != 1) {
+            que.remove();
+            
+            que.add(que.remove());
+        }
+        
+        bw.write(String.valueOf(que.peek()));
+        bw.flush();
+        bw.close();
+    }
 }
