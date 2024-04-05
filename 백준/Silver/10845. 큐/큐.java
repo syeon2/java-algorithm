0023ebf78e8ba52aff01f52e6343f40081a3cfd4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-05 09:26:54 +0900

    [Silver IV] Title: 큐, Time: 192 ms, Memory: 19300 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -1,52 +1,51 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-
-		int N = Integer.parseInt(br.readLine());
-
-		int[] list = new int[10001];
-		int idx = -1;
-
-		for (int n = 0; n < N; n++) {
-			st = new StringTokenizer(br.readLine());
-
-			String cmd = st.nextToken();
-
-			if (cmd.equals("push")) {
-				list[++idx] = Integer.parseInt(st.nextToken());
-			} else if (cmd.equals("pop")) {
-				if (idx == -1) sb.append("-1\n");
-				else {
-					sb.append(list[0]).append("\n");
-
-					for (int i = 0; i <= idx; i++) {
-						list[i] = list[i + 1];
-					}
-
-					list[idx--] = 0;
-				}
-			} else if (cmd.equals("size")) {
-				sb.append(idx + 1).append("\n");
-			} else if (cmd.equals("empty")) {
-				if (idx == -1) sb.append(1).append("\n");
-				else sb.append(0).append("\n");
-			} else if (cmd.equals("front")) {
-				if (idx == -1) sb.append(-1).append("\n");
-				else sb.append(list[0]).append("\n");
-			} else if (cmd.equals("back")) {
-				if (idx == -1) sb.append(-1).append("\n");
-				else sb.append(list[idx]).append("\n");
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        Deque<Integer> que = new LinkedList<>();
+        
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            String cmd = st.nextToken();
+            
+            if (cmd.equals("push")) {
+                int X = Integer.parseInt(st.nextToken());
+                
+                que.add(X);
+            } else if (cmd.equals("pop")) {
+                if (que.isEmpty()) sb.append(-1);
+                else sb.append(que.pop());
+                
+                sb.append("\n");
+            } else if (cmd.equals("size")) {
+                sb.append(que.size()).append("\n");
+            } else if (cmd.equals("empty")) {
+                if (que.isEmpty()) sb.append(1);
+                else sb.append(0);
+                
+                sb.append("\n");
+            } else if (cmd.equals("front")) {
+                if (que.isEmpty()) sb.append(-1);
+                else sb.append(que.peek());
+                
+                sb.append("\n");
+            } else {
+                if (que.isEmpty()) sb.append(-1);
+                else sb.append(que.peekLast());
+                
+                sb.append("\n");
+            }
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
