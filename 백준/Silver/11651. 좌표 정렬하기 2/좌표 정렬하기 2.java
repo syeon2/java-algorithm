f8f613ee2197143fa8ce4841277018c258b4e95a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-05 09:49:01 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 672 ms, Memory: 53280 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -7,10 +7,11 @@ public class Main {
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         int N = Integer.parseInt(br.readLine());
-        
         int[][] list = new int[N][2];
+        
         for (int n = 0; n < N; n++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
+            
             int x = Integer.parseInt(st.nextToken());
             int y = Integer.parseInt(st.nextToken());
             
@@ -24,14 +25,13 @@ public class Main {
                 if (a[1] == b[1]) {
                     return a[0] - b[0];
                 }
-                
                 return a[1] - b[1];
             }
         });
         
         StringBuilder sb = new StringBuilder();
-        for (int n = 0; n < N; n++) {
-            sb.append(list[n][0]).append(" ").append(list[n][1]).append("\n");
+        for (int i = 0; i < N; i++) {
+            sb.append(list[i][0]).append(" ").append(list[i][1]).append("\n");
         }
         
         bw.write(sb.toString());
