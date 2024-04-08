f1d80100ffdd1e53f5da3f88f5bc5a8421d84656
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-08 18:58:14 +0900

    [Bronze I] Title: 이항 계수 1, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -2,35 +2,31 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-
-		int totalN = 1;
-
-		for (int i = 1; i <= N; i++) {
-			totalN *= i;
-		}
-
-		int totalK = 1;
-
-		for (int i = 1; i <= K; i++) {
-			totalK *= i;
-		}
-
-		int minusNum = 1;
-
-		for (int i = 1; i <= N - K; i++) {
-			minusNum *= i;
-		}
-
-		bw.write(String.valueOf(totalN / (totalK * minusNum)));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int up = 1;
+        for (int i = 1; i <= N; i++) {
+            up *= i;
+        }
+        
+        int down = 1;
+        for (int i = N - K; i >= 1; i--) {
+            down *= i;
+        }
+        
+        for (int i = 1; i <= K; i++) {
+            down *= i;
+        }
+        
+        bw.write(String.valueOf(up / down));
+        bw.flush();
+        bw.close();
+    }
 }
