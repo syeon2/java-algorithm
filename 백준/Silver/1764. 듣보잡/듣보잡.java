ff5157c60ea2402c161e74ee90111e8b1b5ae92e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-08 19:19:52 +0900

    [Silver IV] Title: 듣보잡, Time: 292 ms, Memory: 26216 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.java
+++ 백준/Silver/1764. 듣보잡/듣보잡.java
@@ -2,42 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		Set<String> set = new HashSet<>();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < N; i++) {
-			set.add(br.readLine());
-		}
+        Set<String> set = new HashSet<>();
 
-		int count = 0;
-		List<String> list = new ArrayList<>();
-		for (int i = 0; i < M; i++) {
-			String str = br.readLine();
+        for (int i = 0; i < N; i++) {
+            set.add(br.readLine());
+        }
 
-			if (set.contains(str)) {
-				count++;
-				list.add(str);
-			}
-		}
+        Set<String> ans = new TreeSet<>();
 
-		Collections.sort(list);
+        for (int i = 0; i < M; i++) {
+            String w = br.readLine();
 
-		bw.write(String.valueOf(count));
-		bw.newLine();
+            if (set.contains(w)) {
+                ans.add(w);
+            }
+        }
 
-		for (int i = 0; i < list.size(); i++) {
-			bw.write(list.get(i));
-			bw.newLine();
-		}
+        StringBuilder sb = new StringBuilder();
+        sb.append(ans.size()).append("\n");
+        
+        for (String s : ans) {
+            sb.append(s).append("\n");
+        }
 
-		bw.flush();
-		bw.close();
-	}
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
