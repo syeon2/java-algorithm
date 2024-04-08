5a69221d34b7bbc53666fdf2e00ff011a36bc590
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-08 18:53:02 +0900

    [Bronze III] Title: 네 번째 점, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Bronze/3009. 네 번째 점/네 번째 점.java
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -1,37 +1,32 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
-        int[] dx = new int[1001];
-        int[] dy = new int[1001];
+        int[] xList = new int[1001];
+        int[] yList = new int[1001];
         
         for (int i = 0; i < 3; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
+            int X = Integer.parseInt(st.nextToken());
+            int Y = Integer.parseInt(st.nextToken());
             
-            dx[x]++;
-            dy[y]++;
+            xList[X]++;
+            yList[Y]++;
         }
         
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 1001; i++) {
-            if (dx[i] == 1) {
-                sb.append(i).append(" ");
-                break;
-            }
+        
+        for (int i = 1; i <= 1000; i++) {
+            if (xList[i] == 1) sb.append(i).append(" ");
         }
         
-        for (int i = 0; i < 1001; i++) {
-            if (dy[i] == 1) {
-                sb.append(i);
-                break;
-            }
+        for (int i = 1; i <= 1000; i++) {
+            if (yList[i] == 1) sb.append(i);
         }
         
         bw.write(sb.toString());
