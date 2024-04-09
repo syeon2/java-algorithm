08a1f0b5d0a373bbc03a2390a64d75ad54c24038
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-09 09:49:57 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14360 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -7,15 +7,15 @@ public class Main {
         
         int N = Integer.parseInt(br.readLine());
         
-        long[] list = new long[N + 1];
-        list[1] = 1;
+        long[] memo = new long[N + 1];
+        if (N >= 1) memo[1] = 1;
         
         for (int i = 2; i <= N; i++) {
-            list[i] = list[i - 1] + list[i - 2];
+            memo[i] = memo[i - 1] + memo[i - 2];
         }
         
-        bw.write(String.valueOf(list[N]));
-        bw.flush();
+        bw.write(String.valueOf(memo[N]));
+        bw.flush();    
         bw.close();
     }
 }
