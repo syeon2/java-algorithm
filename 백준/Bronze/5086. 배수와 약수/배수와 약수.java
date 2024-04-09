03277a774256137a6b02454053ce08c31449b0f4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-09 11:40:30 +0900

    [Bronze III] Title: 배수와 약수, Time: 120 ms, Memory: 14104 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -2,32 +2,28 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		while (true) {
-			st = new StringTokenizer(br.readLine(), " ");
-
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-
-			if (A == 0 && B == 0) break;
-			else {
-				if (A > B) {
-					if (A % B == 0) sb.append("multiple").append("\n");
-					else sb.append("neither").append("\n");
-				} else {
-					if (B % A == 0) sb.append("factor").append("\n");
-					else sb.append("neither").append("\n");
-				}
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            if (a == 0 && b == 0) break;
+            
+            if (a < b && b % a == 0) sb.append("factor");
+            else if (b < a && a % b == 0) sb.append("multiple");
+            else sb.append("neither");
+            
+            sb.append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
