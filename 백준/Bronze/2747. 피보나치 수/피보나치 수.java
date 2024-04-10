c89d2ed2277db299f5bad5ffbd5e42730192134d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 12:51:58 +0900

    [Bronze II] Title: 피보나치 수, Time: 120 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,31 +1,21 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int prev = 0;
-		int cur = 1;
-
-		int count = 1;
-
-		if (N == 0) bw.write(String.valueOf(prev));
-		else {
-			while (count != N) {
-				count++;
-
-				int temp = cur;
-				cur += prev;
-				prev = temp;
-			}
-
-			bw.write(String.valueOf(cur));
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int[] memo = new int[N + 1];
+        memo[1] = 1;
+        
+        for (int i = 2; i <= N; i++) {
+            memo[i] = memo[i - 1] + memo[i - 2];
+        }
+        
+        bw.write(String.valueOf(memo[N]));
+        bw.flush();
+        bw.close();
+    }
 }
