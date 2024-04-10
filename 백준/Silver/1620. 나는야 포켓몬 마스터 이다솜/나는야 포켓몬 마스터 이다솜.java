49d2fd1cb52b388a9c3c4e81c03485a063d7c5b4
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 12:49:44 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 512 ms, Memory: 46940 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,40 +1,43 @@
 import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		Map<Integer, String> numToName = new HashMap<>();
-		Map<String, Integer> nameToNum = new HashMap<>();
-
-		for (int i = 1; i <= N; i++) {
-			String str = br.readLine();
-
-			numToName.put(i, str);
-			nameToNum.put(str, i);
-		}
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < M; i++) {
-			String str = br.readLine();
-
-			if (str.charAt(0) >= '0' && str.charAt(0) <= '9') {
-				sb.append(numToName.get(Integer.parseInt(str))).append("\n");
-			} else {
-				sb.append(nameToNum.get(str)).append("\n");
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        Map<String, Integer> map = new HashMap<>();
+        String[] list = new String[N + 1];
+        
+        for (int i = 1; i <= N; i++) {
+            String name = br.readLine();
+            
+            map.put(name, i);
+            list[i] = name;
+        }
+        
+        for (int m = 0; m < M; m++) {
+            String cmd = br.readLine();
+            
+            if (cmd.charAt(0) >= '0' && cmd.charAt(0) <= '9') {
+                int idx = Integer.parseInt(cmd);
+                
+                sb.append(list[idx]).append("\n");
+            } else {
+                int idx = map.get(cmd);
+                
+                sb.append(idx).append("\n");
+            }
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
