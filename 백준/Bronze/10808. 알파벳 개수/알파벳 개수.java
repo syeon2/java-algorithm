f8504a0daf09b0de2d1d96a4c8a971b92fcc5f33
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 13:10:48 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 128 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.java
@@ -1,26 +1,27 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		String str = br.readLine();
-
-		int[] cache = new int[26];
-
-		for (int i = 0; i < str.length(); i++) {
-			char c = str.charAt(i);
-
-			cache[c - 'a']++;
-		}
-
-		for (int i = 0; i < 26; i++) {
-			bw.write(String.valueOf(cache[i]));
-			bw.write(" ");
-		}
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String str = br.readLine();
+        
+        int[] list = new int[26];
+        
+        for (int i = 0; i < str.length(); i++) {
+            int idx = str.charAt(i) - 'a';
+            
+            list[idx]++;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            sb.append(list[i]).append(" ");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
