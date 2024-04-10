4045ceb93610355806253877bb97b876b4b825e2
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 12:15:24 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 132 ms, Memory: 14348 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.java
@@ -1,42 +1,40 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-
-			for (int k = 1; k <= i; k++) {
-				sb.append(" ");
-			}
-
-			for (int k = 1; k <= (N - i) * 2 - 1; k++) {
-				sb.append("*");
-			}
-
-			sb.append("\n");
-		}
-
-		for (int i = 2; i <= N; i++) {
-
-			for (int k = 1; k <= N - i; k++) {
-				sb.append(" ");
-			}
-
-			for (int k = 1; k <= (i * 2) - 1; k++) {
-				sb.append("*");
-			}
-
-			sb.append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for (int i = 0; i < N; i++) {
+            for (int k = 0; k < i; k++) {
+                sb.append(" ");
+            }
+            
+            for (int k = 0; k < (N - i) * 2 - 1; k++) {
+                sb.append("*");
+            }
+            
+            sb.append("\n");
+        }
+        
+        for (int i = 2; i <= N; i++) {
+            for (int k = 0; k < N - i; k++) {
+                sb.append(" ");
+            }
+            
+            for (int k = 0; k < (i * 2) - 1; k++) {
+                sb.append("*");
+            }
+            
+            sb.append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
