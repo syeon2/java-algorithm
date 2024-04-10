a22dcc59c486d78405f36d7864772846e751ca7d
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 12:19:55 +0900

    [Bronze III] Title: 최댓값, Time: 144 ms, Memory: 16104 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.java
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -2,37 +2,38 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st;
-
-		int max = -1;
-		int x = 0;
-		int y = 0;
-
-		for (int i = 0; i < 9; i++) {
-
-			st = new StringTokenizer(br.readLine(), " ");
-			for (int k = 0; k < 9; k++) {
-				int num = Integer.parseInt(st.nextToken());
-
-				if (max < num) {
-					max = num;
-					x = k + 1;
-					y = i + 1;
-				}
-			}
-		}
-
-		bw.write(String.valueOf(max));
-		bw.newLine();
-		bw.write(String.valueOf(y));
-		bw.write(" ");
-		bw.write(String.valueOf(x));
-
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int[][] board = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            for (int k = 0; k < 9; k++) {
+                board[i][k] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int ansMax = 0;
+        int ansX = 0;
+        int ansY = 0;
+        
+        for (int i = 0; i < 9; i++) {
+            for (int k = 0; k < 9; k++) {
+                if (ansMax < board[i][k]) {
+                    ansMax = board[i][k];
+                    ansX = k;
+                    ansY = i;
+                }
+            }
+        }
+        
+        bw.write(String.valueOf(ansMax));
+        bw.newLine();
+        bw.write((ansY + 1) + " " + (ansX + 1));
+        
+        bw.flush();
+        bw.close();
+    }
 }
