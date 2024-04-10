2eabae58792a7d3808bec0a71c421505d098f97a
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-11 08:59:11 +0900

    [Bronze III] Title: A+B - 6, Time: 128 ms, Memory: 14312 KB -BaekjoonHub

--- 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -2,24 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), ",");
-
-			int A = Integer.parseInt(st.nextToken());
-			int B = Integer.parseInt(st.nextToken());
-
-			sb.append(A + B).append("\n");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+            
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            
+            sb.append(A + B).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
