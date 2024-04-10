d27c99b6bb3463cc635b76142834daebf7bd5ceb
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-10 12:07:04 +0900

    [Bronze I] Title: 팰린드롬수, Time: 124 ms, Memory: 14100 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -1,32 +1,30 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringBuilder sb = new StringBuilder();
-		String str;
-		while (!(str = br.readLine()).equals("0")) {
-			int num = Integer.parseInt(str);
-
-			if (num < 10) sb.append("yes").append("\n");
-			else {
-				boolean flag = true;
-				for (int i = 0; i < str.length() / 2; i++) {
-					if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
-						flag = false;
-						sb.append("no").append("\n");
-						break;
-					}
-				}
-
-				if (flag) sb.append("yes").append("\n");
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        String str;
+        
+        while (!(str = br.readLine()).equals("0")) {
+            if (isFal(str)) sb.append("yes");
+            else sb.append("no");
+            
+            sb.append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    
+    public static boolean isFal(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) return false;
+        }
+        
+        return true;
+    }
 }
