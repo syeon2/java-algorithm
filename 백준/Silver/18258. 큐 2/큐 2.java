63bddf31e96b317a05ff17a722332907a23c37c0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-11 09:41:13 +0900

    [Silver IV] Title: 큐 2, Time: 1396 ms, Memory: 350372 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -2,53 +2,51 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		Deque<Integer> queue = new LinkedList<>();
-
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine(), " ");
-
-			String command = st.nextToken();
-
-			if (command.equals("push")) {
-				int num = Integer.parseInt(st.nextToken());
-
-				queue.add(num);
-			} else if (command.equals("pop")) {
-				if (queue.isEmpty()) {
-					sb.append(-1).append("\n");
-				} else {
-					sb.append(queue.pop()).append("\n");
-				}
-			} else if (command.equals("size")) {
-				sb.append(queue.size()).append("\n");
-			} else if (command.equals("empty")) {
-				if (queue.isEmpty()) sb.append(1).append("\n");
-				else sb.append(0).append("\n");
-			} else if (command.equals("front")) {
-				if (queue.isEmpty()) {
-					sb.append(-1).append("\n");
-				} else {
-					sb.append(queue.getFirst()).append("\n");
-				}
-			} else if (command.equals("back")) {
-				if (queue.isEmpty()) {
-					sb.append(-1).append("\n");
-				} else {
-					sb.append(queue.getLast()).append("\n");
-				}
-			}
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        Deque<Integer> que = new LinkedList<>();
+        
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            String cmd = st.nextToken();
+            
+            if (cmd.equals("push")) {
+                int num = Integer.parseInt(st.nextToken());
+                
+                que.addLast(num);
+            } else if (cmd.equals("pop")) {
+                if (que.isEmpty()) sb.append(-1);
+                else sb.append(que.removeFirst());
+                
+                sb.append("\n");
+            } else if (cmd.equals("size")) {
+                sb.append(que.size()).append("\n");
+            } else if (cmd.equals("empty")) {
+                if (que.isEmpty()) sb.append(1);
+                else sb.append(0);
+                
+                sb.append("\n");
+            } else if (cmd.equals("front")) {
+                if (que.isEmpty()) sb.append(-1);
+                else sb.append(que.peekFirst());
+                
+                sb.append("\n");
+            } else {
+                if (que.isEmpty()) sb.append(-1);
+                else sb.append(que.peekLast());
+                
+                sb.append("\n");
+            }
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
 }
