bcaf92d0f3e0299b193f30baa7fdbe10576b1e77
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-11 09:11:57 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 128 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.java
@@ -2,41 +2,23 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		int[][] cache = new int[N + 1][M + 1];
-
-		for (int i = 1; i <= N; i++) {
-			cache[i][1] = i - 1;
-		}
-
-		for (int i = 1; i <= M; i++) {
-			cache[1][i] = i - 1;
-		}
-
-		for (int i = 2; i <= N; i++) {
-
-			for (int k = 2; k <= M; k++) {
-				if (i % 2 == 0 && k % 2 == 0) {
-					cache[i][k] = cache[i / 2][k] + cache[i / 2][k] + 1;
-				} else if (i % 2 == 1 && k % 2 == 0) {
-					cache[i][k] = cache[i][k / 2] + cache[i][k / 2] + 1;
-				} else if (i % 2 == 0 && k % 2 == 1) {
-					cache[i][k] = cache[i / 2][k] + cache[i / 2][k] + 1;
-				} else {
-					cache[i][k] = cache[1][k] + cache[(i - 1) / 2][k] + cache[(i - 1) / 2][k] + 2;
-				}
-			}
-		}
-
-		bw.write(String.valueOf(cache[N][M]));
-		bw.flush();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken()) - 1;
+        int M = Integer.parseInt(st.nextToken()) - 1;
+        
+        int cnt = M;
+        while (N-- > 0) {
+            cnt++;
+            cnt += M;
+        }
+        
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+    }
 }
