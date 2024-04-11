9f874fff040dcc29679b47fab8ec0ee337c251a8
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-11 09:53:47 +0900

    [Bronze II] Title: 거스름돈, Time: 120 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.java
+++ 백준/Bronze/5585. 거스름돈/거스름돈.java
@@ -1,49 +1,22 @@
 import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static int[] list = {500, 100, 50, 10, 5, 1};
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		int N = 1000 - Integer.parseInt(br.readLine());
+        int N = 1000 - Integer.parseInt(br.readLine());
 
-		int count = 0;
-		if (N >= 500) {
-			count += (N / 500);
+        int ans = 0;
+        for (int i = 0; i < 6; i++) {
+            ans += N / list[i];
+            N = N % list[i];
+        }
 
-			N = N % 500;
-		}
-
-		if (N >= 100) {
-			count += (N / 100);
-
-			N = N % 100;
-		}
-
-		if (N >= 50) {
-			count += (N / 50);
-
-			N = N % 50;
-		}
-
-		if (N >= 10) {
-			count += (N / 10);
-
-			N = N % 10;
-		}
-
-		if (N >= 5) {
-			count += (N / 5);
-
-			N = N % 5;
-		}
-
-		if (N >= 1) {
-			count += N;
-		}
-
-		bw.write(String.valueOf(count));
-		bw.flush();
-		bw.close();
-	}
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
 }
