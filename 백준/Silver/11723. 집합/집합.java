edd5f582b87f79a6a780ae8678c7994bc81f13be
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-11 11:45:21 +0900

    [Silver V] Title: 집합, Time: 1088 ms, Memory: 312884 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,10 +1,5 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -15,32 +10,38 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         boolean[] list = new boolean[21];
 
-        for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int n = 0; n < N; n++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-            String operator = st.nextToken();
+            String cmd = st.nextToken();
 
-            if (operator.equals("add")) {
+            if (cmd.equals("add")) {
                 int num = Integer.parseInt(st.nextToken());
 
                 list[num] = true;
-            } else if (operator.equals("remove")) {
+            } else if (cmd.equals("remove")) {
                 int num = Integer.parseInt(st.nextToken());
 
                 list[num] = false;
-            } else if (operator.equals("check")) {
+            } else if (cmd.equals("check")) {
                 int num = Integer.parseInt(st.nextToken());
 
-                if (list[num]) sb.append(1).append("\n");
-                else sb.append(0).append("\n");
-            } else if (operator.equals("toggle")) {
+                if (list[num]) sb.append(1);
+                else sb.append(0);
+
+                sb.append("\n");
+            } else if (cmd.equals("toggle")) {
                 int num = Integer.parseInt(st.nextToken());
 
                 list[num] = !list[num];
-            } else if (operator.equals("all")) {
-                Arrays.fill(list, true);
-            } else if (operator.equals("empty")) {
-                Arrays.fill(list, false);
+            } else if (cmd.equals("all")) {
+                for (int i = 1; i <= 20; i++) {
+                    list[i] = true;
+                }
+            } else {
+                for (int i = 1; i <= 20; i++) {
+                    list[i] = false;
+                }
             }
         }
 
