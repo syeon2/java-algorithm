798d9ad6f161ea71d57a8662fe79121550883934
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-12 08:25:10 +0900

    [Bronze II] Title: 커트라인, Time: 132 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] list = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        Arrays.sort(list);
+        
+        bw.write(String.valueOf(list[list.length - K]));
+        bw.flush();
+        bw.close();
+    }
+}
