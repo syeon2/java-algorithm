889bf29b29119027f2441b1335c8f9fd3d1e314b
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-14 09:40:28 +0900

    [Bronze V] Title: 고려대는 사랑입니다, Time: 108 ms, Memory: 13832 KB -BaekjoonHub

---
+++ 백준/Bronze/11942. 고려대는 사랑입니다/고려대는 사랑입니다.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("고려대학교");
+        bw.flush();
+        bw.close();
+    }
+}
