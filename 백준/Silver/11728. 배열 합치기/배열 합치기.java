d4c121994fe8e3dcff3d96d2c7dc395d0f8fce23
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-16 11:45:46 +0900

    [Silver V] Title: 배열 합치기, Time: 1472 ms, Memory: 309520 KB -BaekjoonHub

--- 백준/Silver/11728. 배열 합치기/배열 합치기.java
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.java
@@ -1,54 +1,41 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		StringBuilder sb = new StringBuilder();
-
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-
-		int[] nList = new int[N];
-		int[] mList = new int[M];
-
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < N; i++) {
-			nList[i] = Integer.parseInt(st.nextToken());
-		}
-
-		st = new StringTokenizer(br.readLine(), " ");
-		for (int i = 0; i < M; i++) {
-			mList[i] = Integer.parseInt(st.nextToken());
-		}
-
-		int n = 0;
-		int m = 0;
-
-		while (n < N && m < M) {
-			if (nList[n] < mList[m]) {
-				sb.append(nList[n++]).append(" ");
-			} else {
-				sb.append(mList[m++]).append(" ");
-			}
-		}
-
-		while (n < N) {
-			sb.append(nList[n++]).append(" ");
-		}
-
-		while (m < M) {
-			sb.append(mList[m++]).append(" ");
-		}
-
-		bw.write(sb.toString());
-		bw.flush();
-		bw.close();
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        
+        int[] totalList = new int[A + B];
+        
+        int idx = 0;
+        for (int i = 0; i < 2; i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            if (i == 0) {
+                for (int k = 0; k < A; k++) {
+                    totalList[idx++] = Integer.parseInt(st.nextToken());
+                }
+            } else {
+                for (int k = 0; k < B; k++) {
+                    totalList[idx++] = Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+        
+        Arrays.sort(totalList);
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < totalList.length; i++) {
+            sb.append(totalList[i]).append(" ");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
 }
