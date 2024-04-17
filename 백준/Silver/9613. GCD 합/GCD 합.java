542aa770d7225029b4210d828ed9405a8720ad06
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-17 09:40:13 +0900

    [Silver IV] Title: GCD 합, Time: 116 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Silver/9613. GCD 합/GCD 합.java
+++ 백준/Silver/9613. GCD 합/GCD 합.java
@@ -1,44 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.BufferedWriter;
-import java.io.OutputStreamWriter;
-import java.io.IOException;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
+        StringBuilder sb = new StringBuilder();
+        
         int T = Integer.parseInt(br.readLine());
-
-		for (int t = 0; t < T; t++) {
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int N = Integer.parseInt(st.nextToken());
-
-			long total = 0;
-			int[] list = new int[N];
-			for (int i = 0; i < N; i++) {
-				list[i] = Integer.parseInt(st.nextToken());
-			}
-
-			for (int i = 0; i < N; i++) {
-
-				for (int k = i + 1; k < N; k++) {
-					total += gcd(list[i], list[k]);
-				}
-			}
-
-			bw.write(String.valueOf(total));
-			bw.newLine();
-		}
-
-		bw.flush();
-		bw.close();
+        
+        while (T-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int N = Integer.parseInt(st.nextToken());
+            
+            int[] list = new int[N];
+            for (int i = 0; i < N; i++) {
+                list[i] = Integer.parseInt(st.nextToken());
+            }
+            
+            long sum = 0;
+            
+            for (int i = 0; i < N; i++) {
+                for (int k = i + 1; k < N; k++) {
+                    sum += gcd(list[i], list[k]);
+                }
+            }
+            
+            sb.append(sum).append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
     }
-
-	public static long gcd(int a, int b) {
-		if (a % b == 0) return b;
-		else return gcd(b, a % b);
-	}
+    
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
 }
