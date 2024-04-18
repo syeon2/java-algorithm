a8830d67243fc9eeae85c0888da4cab4a84e17da
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-18 10:09:04 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 336 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -1,37 +1,32 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-		int N = Integer.parseInt(br.readLine());
-
-		int answer = 0;
-
-		for (int i = 1; i <= N; i++) {
-			int pos = 0;
-
-			if (i >= 10) {
-				int index = i;
-
-				while (index > 0) {
-					pos++;
-					index /= 10;
-				}
-			} else {
-				pos++;
-			}
-
-			answer += pos;
-		}
-
-		bw.write(String.valueOf(answer));
-		bw.flush();
-		bw.close();
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        long sum = 0;
+        
+        for (int i = 1; i <= N; i++) {
+            sum += getPos(i);
+        }
+        
+        bw.write(String.valueOf(sum));
+        bw.flush();
+        bw.close();
+    }
+    
+    public static int getPos(int num) {
+        if (num < 10) return 1;
+        else if (num < 100) return 2;
+        else if (num < 1000) return 3;
+        else if (num < 10000) return 4;
+        else if (num < 100000) return 5;
+        else if (num < 1000000) return 6;
+        else if (num < 10000000) return 7;
+        else if (num < 100000000) return 8;
+        else return 9;
     }
 }
