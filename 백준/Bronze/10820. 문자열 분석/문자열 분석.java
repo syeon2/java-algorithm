daa7a18ba7175d8df5f9907cbb2daf24879e0de7
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-19 10:42:23 +0900

    [Bronze II] Title: 문자열 분석, Time: 128 ms, Memory: 14260 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.java
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.java
@@ -1,28 +1,38 @@
-import java.util.*;
 import java.io.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String s = br.readLine();
-
-		while(s != null) {
-			int sch = 0;
-			int bch = 0;
-			int num = 0;
-			int blank = 0;
-
-			for(int i = 0; i < s.length(); i++) {
-				if(s.charAt(i) >= 'a' && s.charAt(i) <= 'z') sch++;
-				else if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') bch++;
-				else if(s.charAt(i) >= '0' && s.charAt(i) <= '9') num++;
-				else blank++;
-			}
-
-			System.out.printf("%d %d %d %d\n", sch, bch, num, blank);
-			s = br.readLine();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringBuilder sb = new StringBuilder();
+        
+        String str;
+        
+        while ((str = br.readLine()) != null) {
+            int[] list = new int[4];
+            for (int i = 0; i < str.length(); i++) {
+                int idx = getIdx(str.charAt(i));
+                
+                list[idx]++;
+            }
+            
+            for (int i = 0; i < 4; i++) {
+                sb.append(list[i]).append(" ");
+            }
+            
+            sb.append("\n");
+        }
+        
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+    
+    public static int getIdx(char c) {
+        if (c >= 'a' && c <= 'z') return 0;
+        else if (c >= 'A' && c <= 'Z') return 1;
+        else if (c >= '0' && c <= '9') return 2;
+        else return 3;
+    }
 }
