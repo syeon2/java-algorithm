585fe5fee365d6352706cd0e3982c1ba7d4cde6f
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-19 10:53:17 +0900

    [Bronze V] Title: 팩토리얼 2, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/27433. 팩토리얼 2/팩토리얼 2.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        long prod = 1;
+        for (int i = 1; i <= N; i++) {
+            prod *= i;
+        }
+        
+        bw.write(String.valueOf(prod));
+        bw.flush();
+        bw.close();
+    }
+}
