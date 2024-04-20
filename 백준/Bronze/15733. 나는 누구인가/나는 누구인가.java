d641c0ced01d5c9405dc9daefb2116101820abb3
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-20 15:01:06 +0900

    [Bronze V] Title: 나는 누구인가, Time: 116 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        bw.write("I'm Sexy");
+        bw.flush();
+        bw.close();
+    }
+}
