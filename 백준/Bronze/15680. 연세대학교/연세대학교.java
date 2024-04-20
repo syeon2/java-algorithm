4df428e840032cabadbe2c4677bf48a03ae65d49
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-20 14:24:10 +0900

    [Bronze V] Title: 연세대학교, Time: 128 ms, Memory: 14056 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        if (N == 0) bw.write("YONSEI");
+        else bw.write("Leading the Way to the Future");
+        
+        bw.flush();
+        bw.close();
+    }
+}
