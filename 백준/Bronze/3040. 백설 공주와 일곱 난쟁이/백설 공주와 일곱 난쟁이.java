ac24973e3eb330a46498313908ea728006790a17
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-22 00:22:17 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -4,41 +4,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+        StringBuilder sb = new StringBuilder();
+
+        int total = 0;
         int[] list = new int[9];
         for (int i = 0; i < 9; i++) {
             list[i] = Integer.parseInt(br.readLine());
+            total += list[i];
         }
-        
-        StringBuilder sb = new StringBuilder();
-        boolean flag = false;
+
+        boolean isEnd = false;
+
         for (int i = 0; i < 9; i++) {
-            
             for (int k = i + 1; k < 9; k++) {
-                
-                int sum = 0;
-                for (int j = 0; j < 9; j++) {
-                    if (i == j || j == k) continue;
-                    
-                    sum += list[j];
-                }
-                
-                if (sum == 100) {
+                if (total - list[i] - list[k] == 100) {
+
                     for (int j = 0; j < 9; j++) {
-                        if (i == j || k == j) continue;
-                        
+                        if (j == i || j == k) continue;
+
                         sb.append(list[j]).append("\n");
                     }
-                    
-                    flag = true;
+
+                    isEnd = true;
+                    break;
                 }
-                
-                if (flag) break;
             }
-            
-            if (flag) break;
+
+            if (isEnd) break;
         }
-        
+
         bw.write(sb.toString());
         bw.flush();
         bw.close();
