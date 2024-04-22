ff20fe8a8c490e7b5ba05724e1a3f7adf25a67f0
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-22 10:21:11 +0900

    [Bronze III] Title: 사과, Time: 128 ms, Memory: 14348 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10833. 사과/사과.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int ans = 0;
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            
+            ans += (b % a);
+        }
+        
+        bw.write(String.valueOf(ans));
+        bw.flush();
+        bw.close();
+    }
+}
