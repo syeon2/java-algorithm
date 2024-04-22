b523c9b0b5846b541030488388a05a897efd3678
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-22 09:26:13 +0900

    [Silver IV] Title: 문자열, Time: 124 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/1120. 문자열/문자열.java
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -1,26 +1,27 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        String[] list = br.readLine().split(" ");
-        
-        String A = list[0];
-        String B = list[1];
-        
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+
         int ans = Integer.MAX_VALUE;
         for (int i = 0; i <= B.length() - A.length(); i++) {
-            
             int cnt = 0;
+
             for (int k = 0; k < A.length(); k++) {
                 if (A.charAt(k) != B.charAt(i + k)) cnt++;
             }
-            
-            ans = Math.min(cnt, ans);
+
+            ans = Math.min(ans, cnt);
         }
-        
+
         bw.write(String.valueOf(ans));
         bw.flush();
         bw.close();
