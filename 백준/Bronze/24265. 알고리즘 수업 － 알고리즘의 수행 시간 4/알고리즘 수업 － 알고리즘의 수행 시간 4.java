cdd5ccfa9d64ef706e051b21e0eebb387b71962e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-22 09:38:50 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 148 ms, Memory: 15864 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long sum = 0;
+        for (int i = 1; i < N; i++) {
+            sum += i;
+        }
+
+        bw.write(sum + "\n" + 2);
+        bw.flush();
+        bw.close();
+    }
+}
