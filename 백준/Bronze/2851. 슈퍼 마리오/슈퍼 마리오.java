bdd035e8a1ad7f3256e7b9d88a264b93d086f977
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-25 14:34:06 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 124 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -4,21 +4,28 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int[] list = new int[11];
-        for (int i = 1; i <= 10; i++) {
-            list[i] = Integer.parseInt(br.readLine()) + list[i - 1];
+
+        int[] list = new int[10];
+        for (int n = 0; n < 10; n++) {
+            list[n] = Integer.parseInt(br.readLine());
         }
-        
+
         int ans = 0;
-        int sub = Integer.MAX_VALUE;
-        for (int i = 1; i <= 10; i++) {
-            if (Math.abs(list[i] - 100) <= sub) {
-                ans = list[i];
-                sub = Math.abs(list[i] - 100);
+        int value = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int temp = 0;
+
+            for (int k = 0; k <= i; k++) {
+                temp += list[k];
+            }
+
+            if (Math.abs(100 - value) >= Math.abs(100 - temp) && value < temp) {
+                ans = temp;
+                value = temp;
             }
         }
-        
+
         bw.write(String.valueOf(ans));
         bw.flush();
         bw.close();
