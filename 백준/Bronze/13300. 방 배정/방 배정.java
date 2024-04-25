1a628cbb93aa315d36c8d01fd75b1b5a2cce91e9
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-04-25 13:50:46 +0900

    [Bronze II] Title: 방 배정, Time: 140 ms, Memory: 14628 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/13300. 방 배정/방 배정.java
+++ 백준/Bronze/13300. 방 배정/방 배정.java
@@ -5,34 +5,34 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-        
+
         int[] boys = new int[7];
         int[] girls = new int[7];
-        
+
         for (int n = 0; n < N; n++) {
             st = new StringTokenizer(br.readLine());
-            
+
             int S = Integer.parseInt(st.nextToken());
             int Y = Integer.parseInt(st.nextToken());
-            
+
             if (S == 0) girls[Y]++;
             else boys[Y]++;
         }
-        
+
         int ans = 0;
         for (int i = 1; i <= 6; i++) {
-            ans += girls[i] / 2;
-            ans += boys[i] / 2;
-            
-            ans += girls[i] % 2;
-            ans += boys[i] % 2;
+            ans += girls[i] / K;
+            ans += boys[i] / K;
+
+            if (girls[i] % K > 0) ans++;
+            if (boys[i] % K > 0) ans++;
         }
-        
+
         bw.write(String.valueOf(ans));
         bw.flush();
         bw.close();
