7c0512aaf468a898b89fc24f4c252b747d8bbe5e
SUYEON KIM <71717303+syeon2@users.noreply.github.com>
2024-05-11 09:19:03 +0900

    [Bronze III] Title: 별 찍기 - 15, Time: 128 ms, Memory: 14416 KB -BaekjoonHub

---
+++ 백준/Bronze/10990. 별 찍기 － 15/별 찍기 － 15.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            for (int k = 1; k <= N - i; k++) {
+                sb.append(" ");
+            }
+
+            sb.append("*");
+
+            for (int k = 1; k <= (i - 1) * 2 - 1; k++) {
+                sb.append(" ");
+            }
+
+            if (i != 1) sb.append("*");
+
+            sb.append("\n");
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+    }
+}
